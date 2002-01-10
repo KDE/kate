f9e410b1910e28136927042ecce81ee4857a0416
Christoph Cullmann <cullmann@kde.org>
2002-01-10 21:28:53 +0000

    tests for the highlighting, just some byte of text files
    svn path=/trunk/kdelibs/kate/; revision=131052

---
+++ part/tests/highlight.php
@@ -0,0 +1,16 @@
+<?
+/* This is a pseudo PHP file to test Kate's PHP syntax highlighting. */
+# TODO: this is incomplete, add more syntax examples!
+# this is also a comment.
+// Even this is a comment
+function test($varname) {
+	return "bla";	# this is also a comment
+}
+
+?>
+
+<?php echo("hello test"); ?>
+
+<html>
+	<? print "<title>test</title>"; ?>
+</html>

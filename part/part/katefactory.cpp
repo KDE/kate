5094031f101b93ce1a1b65d68142f02f431c1bcd
Christoph Cullmann <cullmann@kde.org>
2002-01-10 22:03:46 +0000

    added some files about license, readme, ...
    svn path=/trunk/kdelibs/kate/; revision=131060

--- part/part/katefactory.cpp
+++ part/part/katefactory.cpp
@@ -79,7 +79,7 @@ KInstance *KateFactory::instance()
 const KAboutData *KateFactory::aboutData()
 {
   KAboutData *data = new KAboutData  ("kate", I18N_NOOP("Kate"), "2.0",
-                                                           I18N_NOOP( "Kate - KDE Advanced Text Editor" ), KAboutData::License_GPL,
+                                                           I18N_NOOP( "Kate - KDE Advanced Text Editor" ), KAboutData::License_LGPL_V2,
                                                            I18N_NOOP( "(c) 2000-2001 The Kate Authors" ), 0, "http://kate.sourceforge.net");
 
   data->addAuthor ("Christoph Cullmann", I18N_NOOP("Project Manager and Core Developer"), "cullmann@kde.org", "http://www.babylon2k.de");

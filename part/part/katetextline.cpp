4d9f2c4640128d9b46db2f778841a7f7bd4b4344
Christoph Cullmann <cullmann@kde.org>
2002-01-10 21:59:27 +0000

    added some files about license, readme, ...
    svn path=/trunk/kdelibs/kate/; revision=131058

--- part/part/katetextline.cpp
+++ part/part/katetextline.cpp
@@ -1,31 +1,39 @@
 /***************************************************************************
-                          katetextline.cpp  -  description       
-                             -------------------       
-    begin                : Mon Feb 5 2001       
-    copyright            : (C) 2001 by Christoph Cullmann       
-    email                : cullmann@kde.org       
- ***************************************************************************/       
-       
-/***************************************************************************       
- *                                                                         *       
- *   This program is free software; you can redistribute it and/or modify  *       
- *   it under the terms of the GNU General Public License as published by  *       
- *   the Free Software Foundation; either version 2 of the License, or     *       
- *   (at your option) any later version.                                   *       
- *                                                                         *       
- ***************************************************************************/ 
-          
-#include "katetextline.h"          
-#include <kdebug.h> 
- 
-TextLine::TextLine() 
+                          katetextline.cpp  -  description
+                             -------------------
+    begin                : Mon Feb 5 2001
+    copyright            : (C) 2001 by Christoph Cullmann
+    email                : cullmann@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+ ***************************************************************************/
+
+#include "katetextline.h"
+#include <kdebug.h>
+
+TextLine::TextLine()
   : text(0L), attributes(0L), textLen(0), ctx(0L), ctxLen(0), hlContinue(false)
-{ 
+{
   attr = 0;
-} 
- 
-TextLine::~TextLine() 
-{ 
+}
+
+TextLine::~TextLine()
+{
   if (text != 0L)
     free (text);
 

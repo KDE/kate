75bb8b729e10b50d823b250318626d229f8588a9
John Firebaugh <jfirebaugh@kde.org>
2002-05-05 01:29:56 +0000

    Use most recent version from kdeaddons. Naming convention: ktexteditor_<pluginname>.
    svn path=/trunk/kdelibs/kate/; revision=153874

---
+++ part/plugins/isearch/ISearchPlugin.cpp
@@ -0,0 +1,464 @@
+ /***************************************************************************
+                          ISearchPlugin.cpp  -  description
+                             -------------------
+    begin                : Mon Apr 1 2002
+    copyright            : (C) 2002 by John Firebaugh
+    email                : jfirebaugh@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qtoolbutton.h>
+#include <qregexp.h>
+#include <qstyle.h>
+#include <kgenericfactory.h>
+#include <klocale.h>
+#include <kaction.h>
+#include <kcombobox.h>
+#include <kconfig.h>
+#include <kdebug.h>
+
+#include "ISearchPlugin.h"
+#include "ISearchPlugin.moc"
+
+K_EXPORT_COMPONENT_FACTORY( ktexteditor_isearch, KGenericFactory<ISearchPlugin>( "ktexteditor_isearch" ) );
+
+// Copied from kdebase/konqueror/konq_misc.cc with modifications.
+// Yay code duplication. Hacking widgets to support a specific
+// style is bad bad bad. Why oh why doesn't the toolbar draw the
+// darn gradient once, and then have buttons paint themselves with
+// a mask? (John)
+// Use a toolbutton instead of a label so it is styled correctly. (gallium)
+class KToolBarLabel : public QToolButton
+{
+public:
+	KToolBarLabel( const QString & text, QWidget* parent, const char* name = 0 )
+		: QToolButton( parent, name )
+	{ setText( text ); }
+
+protected:
+	QSize sizeHint() const { return QSize( fontMetrics().width( text() ), 
+	                                       fontMetrics().height() ); }
+	void drawButton( QPainter * p )
+	{
+		// Draw the background
+		style().drawComplexControl( QStyle::CC_ToolButton, p, this, rect(), colorGroup(),
+		    QStyle::Style_Enabled, QStyle::SC_ToolButton );
+		// Draw the label
+		style().drawControl( QStyle::CE_ToolButtonLabel, p, this, rect(), colorGroup(),
+		     QStyle::Style_Enabled );
+}
+	void enterEvent( QEvent* ) {};
+	void leaveEvent( QEvent* ) {};
+};
+
+ISearchPlugin::ISearchPlugin( QObject* parent, const char* name, const QStringList& )
+	: KTextEditor::ViewPlugin ( parent, name )
+	, m_view( 0L )
+	, m_doc( 0L )
+	, m_searchIF( 0L )
+	, m_cursorIF( 0L )
+	, m_selectIF( 0L )
+	, m_toolBarAction( 0L )
+	, m_searchForwardAction( 0L )
+	, m_searchBackwardAction( 0L )
+	, m_label( 0L )
+	, m_combo( 0L )
+	, m_searchBackward( false )
+	, m_caseSensitive( false )
+	, m_fromBeginning( false )
+	, m_regExp( false )
+	, m_autoWrap( false )
+	, m_wrapped( false )
+	, m_startLine( 0 )
+	, m_startCol( 0 )
+	, m_searchLine( 0 )
+	, m_searchCol( 0 )
+	, m_foundLine( 0 )
+	, m_foundCol( 0 )
+	, m_matchLen( 0 )
+	, m_toolBarWasHidden( false )
+{
+	m_searchForwardAction = new KAction(
+		i18n("Search Incrementally"), CTRL+ALT+Key_F,
+		this, SLOT(slotSearchForwardAction()),
+		actionCollection(), "edit_isearch" );
+	m_searchBackwardAction = new KAction(
+		i18n("Search Incrementally Backwards"), CTRL+ALT+SHIFT+Key_F,
+		this, SLOT(slotSearchBackwardAction()),
+		actionCollection(), "edit_isearch_reverse" );
+	
+	m_toolBarAction = new KToggleToolBarAction(
+		"isearchToolBar", i18n("Show Search Toolbar"),
+		actionCollection(), "settings_show_search_toolbar" );
+	
+	m_label = new KToolBarLabel( i18n("I-Search:"), 0L );
+	KWidgetAction* labelAction = new KWidgetAction(
+		m_label,
+		i18n("I-Search:"), 0, 0, 0,
+		actionCollection(), "isearch_label" );
+	labelAction->setShortcutConfigurable( false );
+	
+	m_combo = new KHistoryCombo();
+	m_combo->setDuplicatesEnabled( false );
+	m_combo->setMaximumWidth( 300 );
+	m_combo->lineEdit()->installEventFilter( this );
+	connect( m_combo, SIGNAL(textChanged(const QString&)),
+	         this, SLOT(slotTextChanged(const QString&)) );
+	connect( m_combo, SIGNAL(returnPressed(const QString&)),
+	         this, SLOT(slotReturnPressed(const QString&)) );
+	KWidgetAction* comboAction = new KWidgetAction(
+		m_combo,
+		i18n("Search"), 0, 0, 0,
+		actionCollection(), "isearch_combo" );
+	comboAction->setAutoSized( true );
+	comboAction->setShortcutConfigurable( false );
+	
+	KActionMenu* optionMenu = new KActionMenu(
+		i18n("Search Options"), "configure",
+		actionCollection(), "isearch_options" );
+	optionMenu->setDelayed( false );
+	
+	KToggleAction* action = new KToggleAction(
+		i18n("Case Sensitive"), KShortcut(),
+		actionCollection(), "isearch_case_sensitive" );
+	connect( action, SIGNAL(toggled(bool)),
+	         this, SLOT(setCaseSensitive(bool)) );
+	action->setChecked( m_caseSensitive );
+	optionMenu->insert( action );
+	
+	action = new KToggleAction(
+		i18n("From Beginning"), KShortcut(),
+		actionCollection(), "isearch_from_beginning" );
+	connect( action, SIGNAL(toggled(bool)),
+	         this, SLOT(setFromBeginning(bool)) );
+	action->setChecked( m_fromBeginning );
+	optionMenu->insert( action );
+	
+	action = new KToggleAction(
+		i18n("Regular Expression"), KShortcut(),
+		actionCollection(), "isearch_reg_exp" );
+	connect( action, SIGNAL(toggled(bool)),
+	         this, SLOT(setRegExp(bool)) );
+	action->setChecked( m_regExp );
+	optionMenu->insert( action );
+	
+// 	optionMenu->insert( new KActionSeparator() );
+// 	
+// 	action = new KToggleAction(
+// 		i18n("Auto-Wrap Search"), KShortcut(),
+// 		actionCollection(), "isearch_auto_wrap" );
+// 	connect( action, SIGNAL(toggled(bool)),
+// 	         this, SLOT(setAutoWrap(bool)) );
+// 	action->setChecked( m_autoWrap );
+// 	optionMenu->insert( action );
+	
+	setXMLFile( "ktexteditor_isearch/ktexteditor_isearchui.rc" );
+	
+	kdDebug() << "ISearchPlugin created" << endl;
+}
+
+ISearchPlugin::~ISearchPlugin()
+{
+	writeConfig();
+}
+
+void ISearchPlugin::setView( KTextEditor::View* view )
+{
+	m_view = view;
+	m_doc  = m_view->document();
+	m_searchIF = dynamic_cast<KTextEditor::SearchInterface*>( m_doc );
+	m_cursorIF = dynamic_cast<KTextEditor::ViewCursorInterface*>( m_view );
+	m_selectIF = dynamic_cast<KTextEditor::SelectionInterface*>( m_doc );
+	if( !m_doc || !m_cursorIF || !m_selectIF ) {
+		m_view = 0L;
+		m_doc = 0L;
+		m_searchIF = 0L;
+		m_cursorIF = 0L;
+		m_selectIF = 0L;
+	}
+	
+	readConfig();
+}
+
+void ISearchPlugin::readConfig()
+{
+//	KConfig config( "ktexteditor_isearchrc" );
+//	m_toolBarAction->setChecked( config.readBoolEntry( "Show Toolbar" , false ) );
+}
+
+void ISearchPlugin::writeConfig()
+{
+//	KConfig config( "ktexteditor_isearchrc" );
+//	config.writeEntry( "Show Toolbar", m_toolBarAction->isChecked() );
+}
+
+void ISearchPlugin::setCaseSensitive( bool caseSensitive )
+{
+	m_caseSensitive = caseSensitive;
+}
+
+void ISearchPlugin::setFromBeginning( bool fromBeginning )
+{
+	m_fromBeginning = fromBeginning;
+	
+	if( m_fromBeginning ) {
+		m_searchLine = m_searchCol = 0;
+	} 
+}
+
+void ISearchPlugin::setRegExp( bool regExp )
+{
+	m_regExp = regExp;
+}
+
+void ISearchPlugin::setAutoWrap( bool autoWrap )
+{
+	m_autoWrap = autoWrap;
+}
+
+bool ISearchPlugin::eventFilter( QObject* o, QEvent* e )
+{
+	if( o != m_combo->lineEdit() )
+		return false;
+	
+	if( e->type() == QEvent::FocusIn ) {
+		QFocusEvent* focusEvent = (QFocusEvent*)e;
+		if( focusEvent->reason() == QFocusEvent::ActiveWindow ||
+		    focusEvent->reason() == QFocusEvent::Popup )
+			return false;
+		startSearch();
+	}
+	
+	if( e->type() == QEvent::FocusOut ) {
+		QFocusEvent* focusEvent = (QFocusEvent*)e;
+		if( focusEvent->reason() == QFocusEvent::ActiveWindow ||
+		    focusEvent->reason() == QFocusEvent::Popup )
+			return false;
+		endSearch();
+	}
+	
+	return false;
+}
+
+// Sigh... i18n hell.
+void ISearchPlugin::updateLabelText(
+	bool failing /* = false */, bool reverse /* = false */,
+	bool wrapped /* = false */, bool overwrapped /* = false */ )
+{
+	QString text;
+	// Reverse binary:
+	// 0000
+	if( !failing && !reverse && !wrapped && !overwrapped ) {
+		text = i18n("I-Search:");
+	// 1000
+	} else if ( failing && !reverse && !wrapped && !overwrapped ) {
+		text = i18n("Failing I-Search:");
+	// 0100
+	} else if ( !failing && reverse && !wrapped && !overwrapped ) {
+		text = i18n("I-Search Backward:");
+	// 1100
+	} else if ( failing && reverse && !wrapped && !overwrapped ) {
+		text = i18n("Failing I-Search Backward:");
+	// 0010
+	} else if ( !failing && !reverse && wrapped  && !overwrapped ) {
+		text = i18n("Wrapped I-Search:");
+	// 1010
+	} else if ( failing && !reverse && wrapped && !overwrapped ) {
+		text = i18n("Failing Wrapped I-Search:");
+	// 0110
+	} else if ( !failing && reverse && wrapped && !overwrapped ) {
+		text = i18n("Wrapped I-Search Backward:");
+	// 1110
+	} else if ( failing && reverse && wrapped && !overwrapped ) {
+		text = i18n("Failing Wrapped I-Search Backward:");
+	// 0011
+	} else if ( !failing && !reverse && overwrapped ) {
+		text = i18n("Overwrapped I-Search:");
+	// 1011
+	} else if ( failing && !reverse && overwrapped ) {
+		text = i18n("Failing Overwrapped I-Search:");
+	// 0111
+	} else if ( !failing && reverse && overwrapped ) {
+		text = i18n("Overwrapped I-Search Backwards:");	
+	// 1111
+	} else if ( failing && reverse && overwrapped ) {
+		text = i18n("Failing Overwrapped I-Search Backward:");
+	} else {
+		text = "Error: unknown i-search state!";
+	}
+	m_label->setText( text );
+}
+
+void ISearchPlugin::slotSearchForwardAction()
+{
+	slotSearchAction( false );
+}
+
+void ISearchPlugin::slotSearchBackwardAction()
+{
+	slotSearchAction( true );
+}
+
+void ISearchPlugin::slotSearchAction( bool reverse )
+{
+	if( !m_combo->hasFocus() ) {
+		if( !m_toolBarAction->isChecked() ) {
+			m_toolBarWasHidden = true;
+			m_toolBarAction->setChecked( true );
+		} else {
+			m_toolBarWasHidden = false;
+		}
+		m_combo->setFocus(); // Will call startSearch()
+	} else {
+		nextMatch( reverse );
+	}
+}
+
+void ISearchPlugin::nextMatch( bool reverse )
+{
+	QString text = m_combo->currentText();
+	if( text.isEmpty() )
+		return;
+	if( state != MatchSearch ) {
+		// Last search was performed by typing, start from that match.
+		if( !reverse ) {
+			m_searchLine = m_foundLine;
+			m_searchCol = m_foundCol + m_matchLen;
+		} else {
+			m_searchLine = m_foundLine;
+			m_searchCol = m_foundCol;
+		}
+		state = MatchSearch;
+	}
+	bool found = false;
+	if( !reverse ) {
+		found = iSearch( m_searchLine, m_searchCol, text, reverse, m_autoWrap );
+	} else {
+		found = iSearch( m_searchLine, m_searchCol, text, reverse, m_autoWrap );
+	}
+	if( found ) {
+		m_searchLine = m_foundLine;
+		m_searchCol = m_foundCol + m_matchLen;
+	} else {
+		m_wrapped = true;
+		m_searchLine = m_searchCol = 0;
+	}
+}
+
+void ISearchPlugin::startSearch()
+{
+	if( !m_view ) return;
+	
+	m_searchForwardAction->setText( i18n("Next Incremental Search Match") );
+	m_searchBackwardAction->setText( i18n("Previous Incremental Search Match") );
+	
+	m_wrapped = false;
+	
+	if( m_fromBeginning ) {
+		m_startLine = m_startCol = 0;
+	} else {
+		m_cursorIF->cursorPositionReal( &m_startLine, &m_startCol );
+	}
+	m_searchLine = m_startLine;
+	m_searchCol = m_startCol;
+	
+	updateLabelText( false, m_searchBackward );
+	
+	m_combo->blockSignals( true );
+	m_combo->setCurrentText( m_selectIF->selection() );
+	m_combo->blockSignals( false );
+	m_combo->lineEdit()->selectAll();
+	
+	kdDebug() << "Starting search at " << m_startLine << ", " << m_startCol << endl;
+}
+
+void ISearchPlugin::endSearch()
+{
+	m_searchForwardAction->setText( i18n("Search Incrementally") );
+	m_searchBackwardAction->setText( i18n("Search Incrementally Backwards") );
+	
+	updateLabelText();
+	
+	if( m_toolBarWasHidden ) {
+		m_toolBarAction->setChecked( false );
+	}
+}
+
+void ISearchPlugin::slotTextChanged( const QString& text )
+{
+	state = TextSearch;
+	
+	if( text.isEmpty() )
+		return;
+	
+	iSearch( m_searchLine, m_searchCol, text, m_searchBackward, m_autoWrap );
+}
+
+void ISearchPlugin::slotReturnPressed( const QString& text )
+{
+	if( !text.isEmpty() ) {
+		m_combo->addToHistory( text );
+		m_combo->insertItem( text );
+	}
+	
+	m_combo->blockSignals( true );
+	m_combo->clear();
+	m_combo->blockSignals( false );
+	
+	if( m_view ) {
+		m_view->setFocus(); // Will call endSearch()
+	}
+}
+
+bool ISearchPlugin::iSearch(
+	uint startLine, uint startCol,
+	const QString& text, bool reverse,
+	bool autoWrap )
+{
+	if( !m_view ) return false;
+	
+	kdDebug() << "Searching for " << text << " at " << startLine << ", " << startCol << endl;
+	bool found = false;
+	if( !m_regExp ) {
+		found = m_searchIF->searchText( startLine,
+			           startCol,
+			           text,
+			           &m_foundLine,
+			           &m_foundCol,
+			           &m_matchLen,
+			           m_caseSensitive,
+			           reverse );
+	} else {
+		found = m_searchIF->searchText( startLine,
+			           startCol,
+			           QRegExp( text ),
+			           &m_foundLine,
+			           &m_foundCol,
+			           &m_matchLen,
+			           reverse );
+	}
+	if( found ) {
+		kdDebug() << "Found '" << text << "' at " << m_foundLine << ", " << m_foundCol << endl;
+//		v->gotoLineNumber( m_foundLine );
+		m_cursorIF->setCursorPositionReal( m_foundLine, m_foundCol + m_matchLen );
+		m_selectIF->setSelection( m_foundLine, m_foundCol, m_foundLine, m_foundCol + m_matchLen );
+	} else if ( autoWrap ) {
+		m_wrapped = true;
+		found = iSearch( 0, 0, text, reverse, false );
+	}
+	// FIXME
+	bool overwrapped = ( m_wrapped && 
+		((m_foundLine > m_startLine ) ||
+		 (m_foundLine == m_startLine && m_foundCol >= m_startCol)) );
+	kdDebug() << "Overwrap = " << overwrapped << ". Start was " << m_startLine << ", " << m_startCol << endl;
+	updateLabelText( !found, reverse, m_wrapped, overwrapped );
+	return found;
+}
---
+++ part/plugins/isearch/ISearchPlugin.h
@@ -0,0 +1,91 @@
+ /***************************************************************************
+                          ISearchPlugin.h  -  description
+                             -------------------
+    begin                : Mon Apr 1 2002
+    copyright            : (C) 2002 by John Firebaugh
+    email                : jfirebaugh@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _ISearchPlugin_H_
+#define _ISearchPlugin_H_
+
+#include <ktexteditor/plugin.h>
+#include <ktexteditor/view.h>
+#include <ktexteditor/document.h>
+#include <ktexteditor/searchinterface.h>
+#include <ktexteditor/viewcursorinterface.h>
+#include <ktexteditor/selectioninterface.h>
+
+class QLabel;
+class KToolBarLabel;
+
+class ISearchPlugin : public KTextEditor::ViewPlugin
+{
+	Q_OBJECT
+	
+public:
+	ISearchPlugin( QObject* parent = 0, const char* name = 0, const QStringList &args = QStringList() );
+	virtual ~ISearchPlugin();
+	
+	void setView( KTextEditor::View* view );
+	
+	virtual bool eventFilter( QObject*, QEvent* );
+
+public slots:
+	void setCaseSensitive( bool );
+	void setFromBeginning( bool );
+	void setRegExp( bool );
+	void setAutoWrap( bool );
+
+private slots:
+	void slotSearchForwardAction();
+	void slotSearchBackwardAction();
+	void slotSearchAction( bool reverse );
+	void slotTextChanged( const QString& text );
+	void slotReturnPressed( const QString& text );
+
+private:
+	void readConfig();
+	void writeConfig();
+	
+	void updateLabelText( bool failing = false, bool reverse = false,
+	                      bool wrapped = false, bool overwrapped = false );
+	void startSearch();
+	void endSearch();
+	void nextMatch( bool reverse );
+	bool iSearch( uint startLine, uint startCol,
+	              const QString& text, bool reverse, bool autoWrap );
+
+	KTextEditor::View*     m_view;
+	KTextEditor::Document* m_doc;
+	KTextEditor::SearchInterface* m_searchIF;
+	KTextEditor::ViewCursorInterface* m_cursorIF;
+	KTextEditor::SelectionInterface* m_selectIF;
+	KToggleToolBarAction*  m_toolBarAction;
+	KAction*               m_searchForwardAction;
+	KAction*               m_searchBackwardAction;
+	KToolBarLabel* m_label;
+	KHistoryCombo* m_combo;
+	bool           m_searchBackward;
+	bool           m_caseSensitive;
+	bool           m_fromBeginning;
+	bool           m_regExp;
+	bool           m_autoWrap;
+	bool           m_wrapped;
+	uint           m_startLine, m_startCol;
+	uint           m_searchLine, m_searchCol;
+	uint           m_foundLine, m_foundCol, m_matchLen;
+	bool           m_toolBarWasHidden;
+	enum { NoSearch, TextSearch, MatchSearch } state;
+};
+
+#endif // _ISearchPlugin_H_

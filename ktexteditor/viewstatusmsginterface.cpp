772edcc544b1686f3172d8923bcd8450274464d1
Christoph Cullmann <cullmann@kde.org>
2002-05-23 19:06:35 +0000

    update
    svn path=/trunk/kdelibs/interfaces/ktexteditor/; revision=157202

---
+++ ktexteditor/viewstatusmsginterface.cpp
@@ -0,0 +1,55 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2001 Christoph Cullmann <cullmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+// $Id$
+
+#include "viewstatusmsginterface.h"
+
+namespace KTextEditor
+{
+
+class PrivateViewStatusMsgInterface
+{
+  public:
+    PrivateViewStatusMsgInterface() {}
+    ~PrivateViewStatusMsgInterface() {}
+};
+
+};
+
+using namespace KTextEditor;
+
+unsigned int ViewStatusMsgInterface::globalViewStatusMsgInterfaceNumber = 0;
+
+ViewStatusMsgInterface::ViewStatusMsgInterface()
+{
+  globalViewStatusMsgInterfaceNumber++;
+  myViewStatusMsgInterfaceNumber = globalViewStatusMsgInterfaceNumber++;
+
+  d = new PrivateViewStatusMsgInterface();
+}
+
+ViewStatusMsgInterface::~ViewStatusMsgInterface()
+{
+  delete d;
+}
+
+unsigned int ViewStatusMsgInterface::viewStatusMsgInterfaceNumber () const
+{
+  return myViewStatusMsgInterfaceNumber;
+}
---
+++ ktexteditor/viewstatusmsginterface.h
@@ -0,0 +1,53 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2001 Christoph Cullmann <cullmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __ktexteditor_viewstatusmsginterface_h__
+#define __ktexteditor_viewstatusmsginterface_h__
+
+namespace KTextEditor
+{
+
+/*
+*  This is an interface for the KTextEditor::Document class !!!
+*/
+class ViewStatusMsgInterface
+{
+  friend class PrivateViewStatusMsgInterface;
+
+  public:
+    ViewStatusMsgInterface();
+    virtual ~ViewStatusMsgInterface();
+
+    unsigned int viewStatusMsgInterfaceNumber () const;
+
+  //
+  // signals !!!
+  //  
+  public:
+    virtual void viewStatusMsg (const class QString &msg) = 0;
+    
+
+  private:
+    class PrivateViewStatusMsgInterface *d;
+    static unsigned int globalViewStatusMsgInterfaceNumber;
+    unsigned int myViewStatusMsgInterfaceNumber;
+};
+
+};
+
+#endif

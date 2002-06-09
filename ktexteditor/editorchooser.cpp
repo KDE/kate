c8cbb3bc8afaac7ff2261f13b90537a5ab3cb3d6
Joseph Wenninger <kde@jowenn.at>
2002-06-09 09:57:48 +0000

    except really creatingan object everything should work now
    svn path=/trunk/kdelibs/interfaces/ktexteditor/; revision=159967

--- ktexteditor/editorchooser.cpp
+++ ktexteditor/editorchooser.cpp
@@ -9,72 +9,68 @@
 using namespace KTextEditor;
 
 namespace KTextEditor {
-EditorChooser::EditorChooser(QWidget *parent,const char *name,const ChooserType type) :
+EditorChooser::EditorChooser(QWidget *parent,const char *name) :
 	EditorChooser_UI(parent,name){
 
-	if (type==AppSetting)
-	{
-		TextLabel1->setText(i18n("Choose the default editing widget this application should use"));
-		TextLabel2->setText(i18n(""));
-	}
 	KTrader::OfferList offers = KTrader::self()->query("text/plain", "'KTextEditor/Document' in ServiceTypes");
-	KConfig *config=new KConfig("default_texteditor_component");
-  	config->setGroup("Editor");
-  	QString editor = config->readEntry("EmbeddedEditor", "");
+	KConfig *config=new KConfig("default_components");
+  	config->setGroup("KTextEditor");
+  	QString editor = config->readEntry("embeddedEditor", "");
+	if (editor.isEmpty()) editor="katepart";
 
-	KTrader::OfferList::Iterator it;
-	int index=-1, current=0;
-	int katepartIndex=-1;
-  	for (it = offers.begin(); it != offers.end(); ++it)
+	for (KTrader::OfferList::Iterator it = offers.begin(); it != offers.end(); ++it)
+	{
+    		if ((*it)->desktopEntryName().contains("editor"))
+		{
+			editorCombo->insertItem(i18n("System default (%1)").arg((*it)->name()));
+			break;
+		}
+  	}
+	
+  	for (KTrader::OfferList::Iterator it = offers.begin(); it != offers.end(); ++it)
   	{
     		editorCombo->insertItem((*it)->name());
-    		if ( (*it)->name() == editor )
-      			index = current;
-		if ( (*it)->library().contains("katepart")) katepartIndex=current;
-    		++current;
+		elements.append((*it)->desktopEntryName());
   	}
-
-	if (index >=0)
-    	editorCombo->setCurrentItem(index);
-	else editorCombo->setCurrentItem(katepartIndex);
-	delete config;
+    	editorCombo->setCurrentItem(0);
 }
 
 EditorChooser:: ~EditorChooser(){
 ;
 }
 
-void EditorChooser::writeSysDefault()
-{
-	KConfig *config=new KConfig("default_texteditor_component");
-	config->setGroup("Editor");
-	config->writeEntry("EmbeddedEditor",editorCombo->currentText());
-	config->sync();
-	delete config;
-}
-
-void EditorChooser::readAppSetting(KConfig *cfg, const QString& postfix){
+void EditorChooser::readAppSetting(const QString& postfix){
+	KConfig *cfg=kapp->config();
 	QString previousGroup=cfg->group();
 	cfg->setGroup("KTEXTEDITOR:"+postfix);
 	QString editor=cfg->readEntry("editor","");
-	for (int i=0;i<editorCombo->count();i++)
-		if (editorCombo->text(i)==editor)
-		{
-			editorCombo->setCurrentItem(i);
-			break;
-		}
-
+	if (editor.isEmpty()) editorCombo->setCurrentItem(0);
+	else
+	{
+		int idx=elements.findIndex(editor);
+		idx=idx+1;
+		editorCombo->setCurrentItem(idx);
+	}
 	cfg->setGroup(previousGroup);
 }
 
-void EditorChooser::writeAppSetting(KConfig *cfg, const QString& postfix){
+void EditorChooser::writeAppSetting(const QString& postfix){
+	KConfig *cfg=kapp->config();
 	QString previousGroup=cfg->group();
 	cfg->setGroup("KTEXTEDITOR:"+postfix);
 	cfg->writeEntry("DEVELOPER_INFO","NEVER TRY TO USE VALUES FROM THAT GROUP, THEY ARE SUBJECT TO CHANGES");
-	cfg->writeEntry("editor",editorCombo->currentText());
+	cfg->writeEntry("editor",editorCombo->currentItem()==0?"":(*elements.at(editorCombo->currentItem())));
 	cfg->sync();
 	cfg->setGroup(previousGroup);
 
+}
+
+KTextEditor::Document *EditorChooser::createDocument(const QString& postfix=QString::null,bool fallBackToKatePart=true){
+;
+}
+
+KTextEditor::Editor *EditorChooser::createEditor(const QString& postfix=QString::null,bool fallBackToKatePart=true){
+;
 }
 
 }
--- ktexteditor/editorchooser.h
+++ ktexteditor/editorchooser.h
@@ -2,7 +2,8 @@
 #define  _EDITOR_CHOOSER_H_
 
 #include <editorchooser_ui.h>
-
+#include <ktexteditor/editor.h>
+#include <ktexteditor/document.h>
 class KConfig;
 class QString;
 
@@ -14,12 +15,41 @@ class EditorChooser: public EditorChooser_UI {
 Q_OBJECT
 
 public:
-	enum ChooserType {AppSetting=0,SysSetting};
-	EditorChooser(QWidget *parent=0,const char *name=0,const ChooserType type=AppSetting);
+	EditorChooser(QWidget *parent=0,const char *name=0);
 	virtual ~EditorChooser();
 	void writeSysDefault();
+
+	void readAppSetting(const QString& postfix);
+	void writeAppSetting(const QString& postfix);
+
+	static KTextEditor::Document *createDocument(const QString& postfix=QString::null, bool fallBackToKatePart=true);
+	static KTextEditor::Editor *createEditor(const QString& postfix=QString::null, bool fallBackToKatePart=true);
+ 
+private:
+	QStringList ElementNames;
+	QStringList elements;
+};
+
+/*
+class EditorChooserBackEnd: public ComponentChooserPlugin {
+
+Q_OBJECT
+public:
+	EditorChooserBackEnd(QObject *parent=0, const char *name=0);
+	virtual ~EditorChooserBackEnd();
+
+	virtual QWidget *widget(QWidget *);
+	virtual const QStringList &choices();
+	virtual void saveSettings();
+
 	void readAppSetting(KConfig *cfg,const QString& postfix);
 	void writeAppSetting(KConfig *cfg,const QString& postfix);
+
+public slots:
+	virtual void madeChoice(int pos,const QString &choice);
+
 };
+*/
+
 };
 #endif

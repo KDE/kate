8168445c8500d5a0fa9fa2e10749acaedd8e1aec
Joseph Wenninger <kde@jowenn.at>
2002-06-19 05:53:16 +0000

    License changed from GPL to LGPL
    svn path=/trunk/kdelibs/kate/; revision=161938

--- part/part/katecodecompletion.h
+++ part/part/katecodecompletion.h
@@ -1,25 +1,28 @@
-/***************************************************************************
-                          katecodecompletion.h  -  description
-                             -------------------
-  begin      : Sun Nov 18 20:00 CET 2001
+/* This file is part of the KDE libraries
+
   copyright  : (C) 2001 Joseph Wenninger <jowenn@kde.org>
                (C) 2002 John Firebaugh <jfirebaugh@kde.org>
-               
-  taken from KDEVELOP:
-  begin   : Sam Jul 14 18:20:00 CEST 2001
-  copyright : (C) 2001 by Victor Röder <Victor_Roeder@GMX.de>
- ***************************************************************************/
+ 	       (C) 2001 by Victor Röder <Victor_Roeder@GMX.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 /******** Partly based on the ArgHintWidget of Qt3 by Trolltech AS *********/
+/* Trolltech doesn't mind, if we license that piece of code as LGPL, because there isn't much
+ * left from the desigener code */
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 
 #ifndef __KateCodeCompletion_H__
 #define __KateCodeCompletion_H__
--- part/part/katecodecompletion_arghint.h
+++ part/part/katecodecompletion_arghint.h
@@ -1,25 +1,26 @@
-/***************************************************************************
-                          katecodecompletion_arghint.h  -  description
-                             -------------------
-	begin		: Sun Nov 18 20:00 CET 2001
-	copyright	: (C) 2001 by Joseph Wenninger
-	email		: jowenn@kde.org
-	taken from KDEVELOP:
-	begin		: Sam Jul 14 18:20:00 CEST 2001
-	copyright	: (C) 2001 by Victor Röder
-	email		: Victor_Roeder@GMX.de
- ***************************************************************************/
+/* This file is part of the KDE libraries
+   copyright	: (C) 2001 by Joseph Wenninger <jowenn@kde.org>
+   copyright	: (C) 2001 by Victor Röder <Victor_Roeder@GMX.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 /******** Partly based on the ArgHintWidget of Qt3 by Trolltech AS *********/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/* Trolltech doesn't mind, if we license that piece of code as LGPL, because there isn't much
+ * left from the desigener code */
 
 #ifndef __CODECOMPLETION_ARGHINT_H__
 #define __CODECOMPLETION_ARGHINT_H__

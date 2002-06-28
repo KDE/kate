c7c6ecaed4d75c02cfe1ecf510c9d41a05327324
Daniel Naber <daniel.naber@t-online.de>
2002-06-28 01:03:27 +0000

    use word under cursor; add LGP copyright; fix whitespace
    svn path=/trunk/kdelibs/kate/; revision=163573

--- part/plugins/kdatatool/kate_kdatatool.cpp
+++ part/plugins/kdatatool/kate_kdatatool.cpp
@@ -1,3 +1,23 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2002 Joseph Wenninger <jowenn@jowenn.at> and Daniel Naber <daniel.naber@t-online.de>
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+// $Id: $
+
 #include "kate_kdatatool.h"
 #include "kate_kdatatool.moc"
 #include <kgenericfactory.h>
@@ -16,136 +36,177 @@ K_EXPORT_COMPONENT_FACTORY( ktexteditor_kdatatool, KGenericFactory<KTextEditor::
 namespace KTextEditor {
 
 KDataToolPlugin::KDataToolPlugin( QObject *parent, const char* name, const QStringList& )
-        : KTextEditor::Plugin ( (KTextEditor::Document*) parent, name ) {
+	: KTextEditor::Plugin ( (KTextEditor::Document*) parent, name )
+{
 }
 
 
-KDataToolPlugin::~KDataToolPlugin () {
-
+KDataToolPlugin::~KDataToolPlugin ()
+{
 }
 
 void KDataToolPlugin::addView(KTextEditor::View *view)
 {
-  KDataToolPluginView *nview = new KDataToolPluginView (view);
-  nview->setView (view);
-//  m_views.append (nview);
+	KDataToolPluginView *nview = new KDataToolPluginView (view);
+	nview->setView (view);
+	//m_views.append (nview);
 }
 
 
 KDataToolPluginView::KDataToolPluginView( KTextEditor::View *view )
-	:m_menu(0){
+	:m_menu(0)
+{
 
 	view->insertChildClient (this);
-        setInstance( KGenericFactory<KDataToolPlugin>::instance() );
+	setInstance( KGenericFactory<KDataToolPlugin>::instance() );
 
+	m_menu = new KActionMenu(i18n("Datatools"), actionCollection(), "popup_dataTool");
+	connect(m_menu->popupMenu(), SIGNAL(aboutToShow()), this, SLOT(aboutToShow()));
+	setXMLFile("ktexteditor_kdatatoolui.rc");
 
-	m_menu= new KActionMenu(i18n("Datatools"),actionCollection(),"popup_dataTool");
-	connect(m_menu->popupMenu(),SIGNAL(aboutToShow()),this,SLOT(aboutToShow()));
-        setXMLFile( "ktexteditor_kdatatoolui.rc" );
-
-	m_view=view;
+	m_view = view;
 }
 
-KDataToolPluginView::~KDataToolPluginView(){
+KDataToolPluginView::~KDataToolPluginView()
+{
 	delete m_menu;
 }
 
 void KDataToolPluginView::aboutToShow()
 {
-  kdDebug()<<"KTextEditor::KDataToolPluginView::aboutToShow"<<endl;
-  QString word;
-  m_singleWord = false;
-  m_wordUnderCursor = QString::null;
+	kdDebug()<<"KTextEditor::KDataToolPluginView::aboutToShow"<<endl;
+	QString word;
+	m_singleWord = false;
+	m_wordUnderCursor = QString::null;
 
-  if ( selectionInterface(m_view->document())->hasSelection() )
-  {
-    word = selectionInterface(m_view->document())->selection();
-    if ( word.find(' ') == -1 && word.find('\t') == -1 && word.find('\n') == -1 ) {
-        m_singleWord = true;
-    } else {
-        m_singleWord = false;
-    }
-  } else {
-    return;
-    // fixme:
-    // No selection -> use word under cursor
-    //m_singleWord = true;
-    //m_wordUnderCursor = word;
-  }
+	// unplug old actions, if any:
+	KAction *ac;
+	for ( ac = m_actionList.first(); ac; ac = m_actionList.next() ) {
+		m_menu->remove(ac);
+	}
 
-  KInstance *inst=instance();
+	if ( selectionInterface(m_view->document())->hasSelection() )
+	{
+		word = selectionInterface(m_view->document())->selection();
+		if ( word.find(' ') == -1 && word.find('\t') == -1 && word.find('\n') == -1 )
+			m_singleWord = true;
+		else
+			m_singleWord = false;
+	} else {
+		// No selection -> use word under cursor
+		KTextEditor::EditInterface *ei;
+		KTextEditor::ViewCursorInterface *ci;
+		KTextEditor::View *v = (KTextEditor::View*)m_view; 
+		ei = KTextEditor::editInterface(v->document());
+		ci = KTextEditor::viewCursorInterface(v);
+		uint line, col;
+		ci->cursorPositionReal(&line, &col);
+		QString tmp_line = ei->textLine(line);
+		m_wordUnderCursor = "";
+		// find begin of word:
+		for(uint i = col; i > 0; i--) {
+			QChar ch = tmp_line.at(i);
+			if( ch.isSpace() )
+			{
+				m_singleWord_start = i+1;
+				break;
+			}
+			m_wordUnderCursor = ch + m_wordUnderCursor;
+		}
+		// find end of word:
+		for(uint i = col+1; i < tmp_line.length(); i++) {
+			QChar ch = tmp_line.at(i);
+			if( ch.isSpace() )
+			{
+				m_singleWord_end = i;
+				break;
+			}
+			m_wordUnderCursor += ch;
+		}
+		if( ! m_wordUnderCursor.isEmpty() )
+		{
+			m_singleWord = true;
+			m_singleWord_line = line;
+		} else {
+			return;
+		}
+	}
 
-  QValueList<KDataToolInfo> tools;
-  tools += KDataToolInfo::query( "QString", "text/plain", inst );
-  if( m_singleWord )
-    tools += KDataToolInfo::query( "QString", "application/x-singleword", inst );
+	KInstance *inst=instance();
 
-  // unplug old actions, if any:
-  KAction *ac;
-  for ( ac = m_actionList.first(); ac; ac = m_actionList.next() ) {
-	m_menu->remove(ac);
-  }
+	QValueList<KDataToolInfo> tools;
+	tools += KDataToolInfo::query( "QString", "text/plain", inst );
+	if( m_singleWord )
+		tools += KDataToolInfo::query( "QString", "application/x-singleword", inst );
 
-  m_actionList = KDataToolAction::dataToolActionList( tools, this,
-    SLOT( slotToolActivated( const KDataToolInfo &, const QString & ) ) );
+	m_actionList = KDataToolAction::dataToolActionList( tools, this,
+		SLOT( slotToolActivated( const KDataToolInfo &, const QString & ) ) );
 
-  for ( ac = m_actionList.first(); ac; ac = m_actionList.next() ) {
-	m_menu->insert(ac);
-  }
+	for ( ac = m_actionList.first(); ac; ac = m_actionList.next() ) {
+		m_menu->insert(ac);
+	}
+	QString note(i18n("(not available)"));
+	/* fixme: don't show an empty submenu
+	if( m_actionList.isEmpty() ) {
+	KAction *empty = new KAction(i18n("(not available)"), QString::null, 0, this, SLOT(slotNotAvailable()));
+	m_menu->insert(empty);
+	} */
 }
 
 void KDataToolPluginView::slotToolActivated( const KDataToolInfo &info, const QString &command )
 {
 
-  KDataTool* tool = info.createTool( );
-  if ( !tool )
-  {
-      kdWarning() << "Could not create Tool !" << endl;
-      return;
-  }
+	KDataTool* tool = info.createTool( );
+	if ( !tool )
+	{
+		kdWarning() << "Could not create Tool !" << endl;
+		return;
+	}
 
-  QString text;
-  if ( selectionInterface(m_view->document())->hasSelection() )
-    text = selectionInterface(m_view->document())->selection();
-  else
-    text = m_wordUnderCursor;
+	QString text;
+	if ( selectionInterface(m_view->document())->hasSelection() )
+		text = selectionInterface(m_view->document())->selection();
+	else
+		text = m_wordUnderCursor;
 
-  QString mimetype = "text/plain";
-  QString datatype = "QString";
+	QString mimetype = "text/plain";
+	QString datatype = "QString";
 
-  // If unsupported (and if we have a single word indeed), try application/x-singleword
-  if ( !info.mimeTypes().contains( mimetype ) && m_singleWord )
-    mimetype = "application/x-singleword";
+	// If unsupported (and if we have a single word indeed), try application/x-singleword
+	if ( !info.mimeTypes().contains( mimetype ) && m_singleWord )
+		mimetype = "application/x-singleword";
 
-  kdDebug() << "Running tool with datatype=" << datatype << " mimetype=" << mimetype << endl;
+	kdDebug() << "Running tool with datatype=" << datatype << " mimetype=" << mimetype << endl;
 
-  QString origText = text;
-  if ( tool->run( command, &text, datatype, mimetype) )
-  {
-    kdDebug() << "Tool ran. Text is now " << text << endl;
-    if ( origText != text )
-    {
-       uint line, col;
-       viewCursorInterface(m_view)->cursorPositionReal(&line, &col);
-       if ( selectionInterface(m_view->document())->hasSelection() )
-       {
-         // replace selection with 'text'
-         selectionInterface(m_view->document())->removeSelectedText();
-	
-         viewCursorInterface(m_view)->cursorPositionReal(&line, &col);
-         editInterface(m_view->document())->insertText(line,col,text);
-         // place cursor at the end:
-/* No idea yet (Joseph Wenninger)
-         for ( uint i = 0; i < text.length(); i++ ) {
-           viewCursorInterface(m_view)->cursorRight();
-         }
-*/
-       }
-          // fixme: else case: use word under cursor
-    }
-  }
+	QString origText = text;
 
-  delete tool;
+	if ( tool->run( command, &text, datatype, mimetype) )
+	{
+		kdDebug() << "Tool ran. Text is now " << text << endl;
+		if ( origText != text )
+		{
+			uint line, col;
+			viewCursorInterface(m_view)->cursorPositionReal(&line, &col);
+			if ( ! selectionInterface(m_view->document())->hasSelection() )
+			{
+				KTextEditor::SelectionInterface *si;
+				si = KTextEditor::selectionInterface(m_view->document());
+				si->setSelection(m_singleWord_line, m_singleWord_start, m_singleWord_line, m_singleWord_end);
+			}
+		
+			// replace selection with 'text'
+			selectionInterface(m_view->document())->removeSelectedText();
+			viewCursorInterface(m_view)->cursorPositionReal(&line, &col);
+			editInterface(m_view->document())->insertText(line, col, text);
+			 // fixme: place cursor at the end:
+			 /* No idea yet (Joseph Wenninger)
+			 for ( uint i = 0; i < text.length(); i++ ) {
+				viewCursorInterface(m_view)->cursorRight();
+			 } */
+		}
+	}
+
+	delete tool;
 }
 
 
--- part/plugins/kdatatool/kate_kdatatool.h
+++ part/plugins/kdatatool/kate_kdatatool.h
@@ -1,3 +1,23 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2002 Joseph Wenninger <jowenn@jowenn.at> and Daniel Naber <daniel.naber@t-online.de>
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+// $Id: $
+
 #ifndef _KATE_KDATATOOL_
 #define _KATE_KDATATOOL_
 
@@ -16,32 +36,31 @@ class View;
 
 class KDataToolPlugin : public KTextEditor::Plugin, public KTextEditor::PluginViewInterface
 {
-  Q_OBJECT
+	Q_OBJECT
 
-  public:
-          KDataToolPlugin( QObject *parent = 0, const char* name = 0, const QStringList &args = QStringList() );
-          virtual ~KDataToolPlugin();
-
-    void addView (KTextEditor::View *view);
-    void removeView (KTextEditor::View *view){;}
+public:
+	KDataToolPlugin( QObject *parent = 0, const char* name = 0, const QStringList &args = QStringList() );
+	virtual ~KDataToolPlugin();
+	void addView (KTextEditor::View *view);
+	void removeView (KTextEditor::View *){;}
 
 //  private:
-//    QPtrList<class ISearchPluginView> m_views;
+//	QPtrList<class ISearchPluginView> m_views;
 };
 
 
 class KDataToolPluginView : public QObject, public KXMLGUIClient
 {
-        Q_OBJECT
+	Q_OBJECT
 
 public:
-        KDataToolPluginView( KTextEditor::View *view );
-        virtual ~KDataToolPluginView();
-
-  	void setView( KTextEditor::View* view ){;}
+	KDataToolPluginView( KTextEditor::View *view );
+	virtual ~KDataToolPluginView();
+	void setView( KTextEditor::View* ){;}
 private:
 	View *m_view;
 	bool m_singleWord;
+	uint m_singleWord_line, m_singleWord_start, m_singleWord_end;
 	QString m_wordUnderCursor;
 	QPtrList<KAction> m_actionList;
 	QGuardedPtr<KActionMenu> m_menu;

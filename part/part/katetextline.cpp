3b6b5d40409509b033020405e054badfc6b0f233
David Faure <faure@kde.org>
2002-10-31 18:22:07 +0000

    Another crash (Q_ASSERT for others) fixed: when deleting text from a line, the loop that moves the chars was going too far. In essence: -    for (int z = pos; z < (int) oldTextLen; z++) +  for (uint z = pos; z < oldTextLen - delLen; z++) (but I also got rid of the useless oldTextLen, moving the -= below that loop). Heavily tested.
    svn path=/trunk/kdelibs/kate/; revision=186693

--- part/part/katetextline.cpp
+++ part/part/katetextline.cpp
@@ -1,10 +1,10 @@
 /* This file is part of the KDE libraries
-   Copyright (C) 2001-2002 Christoph Cullmann <cullmann@kde.org> 
+   Copyright (C) 2001-2002 Christoph Cullmann <cullmann@kde.org>
    Copyright (C) 2002 Joseph Wenninger <jowenn@kde.org>
-   
+
    Based on:
      TextLine : Copyright (C) 1999 Jochen Wilhelmy <digisnap@cs.tu-berlin.de>
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.
@@ -44,16 +44,16 @@ void TextLine::insertText (uint pos, uint insLen, const QChar *insText, uchar *i
   // calc new textLen, store old
   uint textLen = m_text.size();
   uint oldTextLen = textLen;
-  
+
   if (pos <= textLen)
     textLen += insLen;
   else if (pos > textLen)
     textLen = pos + insLen;
-  
+
   // resize the arrays
   m_text.resize (textLen);
   m_attributes.resize (textLen);
-  
+
   // HA, insert behind text end, fill with spaces
   if (pos >= oldTextLen)
   {
@@ -65,19 +65,19 @@ void TextLine::insertText (uint pos, uint insLen, const QChar *insText, uchar *i
   }
   // HA, insert in text, move the old text behind pos
   else if (oldTextLen > 0)
-  {      
+  {
     for (int z = oldTextLen -1; z >= (int) pos; z--)
     {
       m_text[z+insLen] = m_text[z];
       m_attributes[z+insLen] = m_attributes[z];
     }
   }
-  
+
   // BUH, actually insert the new text
   for (uint z = 0; z < insLen; z++)
   {
     m_text[z+pos] = insText[z];
-    
+
     if (insAttribs == 0)
       m_attributes[z+pos] = 0;
     else
@@ -90,37 +90,31 @@ void TextLine::removeText (uint pos, uint delLen)
   // nothing to do
   if (delLen == 0)
     return;
-  
-  // calc textLen, store old one, calc delLen
+
   uint textLen = m_text.size();
-  
+
   if (textLen == 0)
     return; // uh, again nothing real to do ;)
-  
-  uint oldTextLen = textLen;
-  
+
   if (pos >= textLen)
     return;
-    
+
   if ((pos + delLen) > textLen)
     delLen = textLen - pos;
-    
-  if (delLen < textLen)
-    textLen -= delLen;
-  else
-    textLen = 0;
-  
+
   // BU, MOVE THE OLD TEXT AROUND
-  if (textLen > 0)
+  for (uint z = pos; z < textLen - delLen; z++)
   {
-    for (int z = pos; z < (int) oldTextLen; z++)
-    {
       m_text[z] = m_text[z+delLen];
       m_attributes[z] = m_attributes[z+delLen];
-    }
   }
-  
+
   // resize the stuff
+  if (delLen < textLen)
+    textLen -= delLen;
+  else // seems to happen when hitting Backspace at the beginning of a line...
+    textLen = 0;
+
   m_text.resize (textLen);
   m_attributes.resize (textLen);
 }
@@ -177,12 +171,12 @@ int TextLine::previousNonSpaceChar(uint pos) const
     }
     return -1;
 }
- 
+
 int TextLine::firstChar() const
 {
-  return nextNonSpaceChar(0); 
+  return nextNonSpaceChar(0);
 }
- 
+
 int TextLine::lastChar() const
 {
     return previousNonSpaceChar(m_text.size() - 1);
@@ -192,10 +186,10 @@ QString TextLine::string (uint startCol, uint length) const
 {
   if ( startCol >= m_text.size() )
     return QString ();
-    
+
   if ( (startCol + length) > m_text.size())
     length = m_text.size() - startCol;
-    
+
   return QString (m_text.data() + startCol, length);
 }
 
@@ -262,23 +256,23 @@ bool TextLine::searchText (uint startCol, const QString &text, uint *foundAtCol,
   }
 
   return false;
-} 
- 
-bool TextLine::searchText (uint startCol, const QRegExp &regexp, uint *foundAtCol, uint *matchLen, bool backwards) 
-{ 
-  int index; 
- 
+}
+
+bool TextLine::searchText (uint startCol, const QRegExp &regexp, uint *foundAtCol, uint *matchLen, bool backwards)
+{
+  int index;
+
   if (backwards)
     index = regexp.searchRev (QConstString (this->m_text.data(), this->m_text.size()).string(), startCol);
   else
     index = regexp.search (QConstString (this->m_text.data(), this->m_text.size()).string(), startCol);
- 
-   if (index > -1) 
+
+   if (index > -1)
 	{
-	  (*foundAtCol) = index; 
-		(*matchLen)=regexp.matchedLength(); 
-		return true; 
-  } 
+	  (*foundAtCol) = index;
+		(*matchLen)=regexp.matchedLength();
+		return true;
+  }
 
   return false;
 }
@@ -290,9 +284,9 @@ uint TextLine::dumpSize () const
   if ( ! m_attributes.isEmpty())
   {
     attributesLen = 1;
-  
+
     uint lastAttrib = m_attributes[0];
-    
+
     for (uint z=0; z < m_attributes.size(); z++)
     {
       if (m_attributes[z] != lastAttrib)
@@ -301,8 +295,8 @@ uint TextLine::dumpSize () const
         lastAttrib = m_attributes[z];
       }
     }
-  }  
-  
+  }
+
   return (4*sizeof(uint)) + (m_text.size()*sizeof(QChar)) + (attributesLen * sizeof(uchar)) + (attributesLen * sizeof(uint)) + 1 + (m_ctx.size() * sizeof(uint)) + (m_foldingList.size() * sizeof(signed char));
 }
 
@@ -314,16 +308,16 @@ char *TextLine::dump (char *buf) const
 
   memcpy(buf, &l, sizeof(uint));
   buf += sizeof(uint);
-  
+
   memcpy(buf, (char *) m_text.data(), sizeof(QChar)*l);
   buf += sizeof(QChar)*l;
 
   memcpy(buf, (char *) &m_flags, 1);
   buf += 1;
-  
+
   char *attribLenPos = buf;
   buf += sizeof(uint);
- 
+
   memcpy(buf, &lctx, sizeof(uint));
   buf += sizeof(uint);
 
@@ -333,11 +327,11 @@ char *TextLine::dump (char *buf) const
   // hl size runlength encoding START
 
   uint attributesLen = 0;
-  
+
   if ( ! m_attributes.isEmpty() )
   {
     attributesLen = 1;
-  
+
     uchar lastAttrib = m_attributes[0];
     uint lastStart = 0;
     uint length = 0;
@@ -347,7 +341,7 @@ char *TextLine::dump (char *buf) const
       if (m_attributes[z] != lastAttrib)
       {
         length = z - lastStart;
- 
+
         memcpy(buf, &lastAttrib, sizeof(uchar));
         buf += sizeof(uchar);
 
@@ -367,9 +361,9 @@ char *TextLine::dump (char *buf) const
     buf += sizeof(uchar);
 
     memcpy(buf, &length, sizeof(uint));
-    buf += sizeof(uint);    
+    buf += sizeof(uint);
   }
-  
+
   memcpy(attribLenPos, &attributesLen, sizeof(uint));
 
   // hl size runlength encoding STOP
@@ -393,22 +387,22 @@ char *TextLine::restore (char *buf)
   // text + context length read
   memcpy((char *) &l, buf, sizeof(uint));
   buf += sizeof(uint);
-  
+
   // text + attributes
   m_text.duplicate ((QChar *) buf, l);
   buf += sizeof(QChar)*l;
-  
+
   m_attributes.resize (l);
-  
+
   memcpy((char *) &m_flags, buf, 1);
   buf += 1;
-    
+
   // we just restore a TextLine from a buffer first time
   if (m_flags == TextLine::flagNoOtherData)
   {
     m_flags = TextLine::flagVisible;
     m_attributes.fill (0);
-    
+
     return buf;
   }
 
@@ -428,12 +422,12 @@ char *TextLine::restore (char *buf)
   uchar attrib = 0;
   uint length = 0;
   uint pos = 0;
-  
+
   for (uint z=0; z < lattrib; z++)
   {
     if (pos >= m_attributes.size())
-      break;  
-  
+      break;
+
     memcpy((char *) &attrib, buf, sizeof(uchar));
     buf += sizeof(uchar);
 
@@ -442,21 +436,21 @@ char *TextLine::restore (char *buf)
 
     if ((pos+length) > m_attributes.size())
       length = m_attributes.size() - pos;
-    
+
     memset (attr, attrib, length);
 
     pos += length;
     attr += length;
   }
-  
+
   // hl size runlength encoding STOP
-  
+
   m_ctx.duplicate ((uint *) buf, lctx);
   buf += sizeof(uint) * lctx;
 
   m_foldingList.duplicate ((signed char *) buf, lfold);
-  buf += lfold;           
-  
+  buf += lfold;
+
   return buf;
 }
 

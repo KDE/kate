28a268394de0d6f833299d844606f49a6b397bae
Hamish Rodda <rodda@kde.org>
2003-02-26 13:18:14 +0000

    Nicer fix, thanks for the tip.
    svn path=/trunk/kdelibs/kate/; revision=209641

--- part/part/kateviewinternal.cpp
+++ part/part/kateviewinternal.cpp
@@ -2655,7 +2655,7 @@ void KateViewInternal::editRemoveLine(int line)
 
 void KateViewInternal::wheelEvent(QWheelEvent* e)
 {
-  if (m_lineScroll->minValue() != m_lineScroll->maxValue() && !(e->state() & Qt::AltButton)) {
+  if (m_lineScroll->minValue() != m_lineScroll->maxValue() && e->orientation() != Qt::Horizontal) {
     // React to this as a vertical event
     if ( ( e->state() & ControlButton ) || ( e->state() & ShiftButton ) ) {
       if (e->delta() > 0)

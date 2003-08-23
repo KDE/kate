5e13f184cfb57a57d5a816d53496be643bde132d
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2003-08-23 00:25:00 +0000

    CVS_SILENT Typographical corrections and changes
    svn path=/trunk/kdelibs/kate/; revision=244151

--- part/part/kateviewdialog.cpp
+++ part/part/kateviewdialog.cpp
@@ -223,10 +223,10 @@ IndentConfigTab::IndentConfigTab(QWidget *parent, KateDocument *view)
   layout->addStretch();
 
   // What is this? help
-  QWhatsThis::add(opt[0], i18n("When <b>Automatically indent</b> is on, KateView will indent new lines to equal the indent on the previous line.<p>If the previous line is blank, the nearest line above with text is used."));
+  QWhatsThis::add(opt[0], i18n("When <b>Automatically indent</b> is on, KateView will indent new lines to equal the indentation on the previous line.<p>If the previous line is blank, the nearest line above with text is used."));
   QWhatsThis::add(opt[1], i18n("Check this if you want to indent with spaces rather than tabs."));
-  QWhatsThis::add(opt[2], i18n("This allows the <b>Backspace</b> key to be used to decrease the indent level."));
-  QWhatsThis::add(opt[3], i18n("This allows the <b>Tab</b> key to be used to increase the indent level."));
+  QWhatsThis::add(opt[2], i18n("This allows the <b>Backspace</b> key to be used to decrease the indentation level."));
+  QWhatsThis::add(opt[3], i18n("This allows the <b>Tab</b> key to be used to increase the indentation level."));
   QWhatsThis::add(opt[4], i18n("This retains current indentation settings for future documents."));
   QWhatsThis::add(opt[5], i18n("Indentations of more than the selected number of spaces will not be shortened."));
   QWhatsThis::add(indentationWidth, i18n("The number of spaces to indent with."));

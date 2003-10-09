63579d5f6718660beed8b8e41e473ebea5c024f0
Christoph Cullmann <cullmann@kde.org>
2003-10-09 20:39:58 +0000

    add anders and my copyright to some files missing it ;)
    svn path=/trunk/kdelibs/kate/; revision=257551

--- part/part/katedialogs.h
+++ part/part/katedialogs.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE libraries
    Copyright (C) 2002, 2003 Anders Lund <anders.lund@lund.tdcadsl.dk>
+   Copyright (C) 2003 Christoph Cullmann <cullmann@kde.org>
    Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
 
    Based on work of:
--- part/part/kateschema.cpp
+++ part/part/kateschema.cpp
@@ -1,6 +1,7 @@
 /* This file is part of the KDE libraries
    Copyright (C) 2001-2003 Christoph Cullmann <cullmann@kde.org>
-
+   Copyright (C) 2002, 2003 Anders Lund <anders.lund@lund.tdcadsl.dk>
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.

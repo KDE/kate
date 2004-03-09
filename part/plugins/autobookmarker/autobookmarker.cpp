d380138bfded2e92ec4593416a6393953dd46e34
Anders Lund <anders@alweb.dk>
2004-03-09 07:25:58 +0000

    fix licence header
    svn path=/trunk/kdelibs/kate/; revision=294605

--- part/plugins/autobookmarker/autobookmarker.cpp
+++ part/plugins/autobookmarker/autobookmarker.cpp
@@ -1,4 +1,8 @@
 /*
+    This library is free software you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License.
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
--- part/plugins/autobookmarker/autobookmarker.h
+++ part/plugins/autobookmarker/autobookmarker.h
@@ -1,4 +1,7 @@
 /*
+    This library is free software you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License.
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of

54485848640d77a9d133c7113edbe80605442238
Stephan Binner <binner@kde.org>
2004-04-11 17:37:59 +0000

    More KStdGuiItems: less strings, more icons
    svn path=/trunk/kdelibs/kate/; revision=302860

--- part/part/katesearch.cpp
+++ part/part/katesearch.cpp
@@ -423,7 +423,7 @@ bool KateSearch::askContinue()
 
   return KMessageBox::Yes == KMessageBox::questionYesNo(
      view(), text, s.flags.replace ? i18n("Replace") : i18n("Find"),
-     i18n("&Continue"), i18n("&Stop") );
+     KStdGuiItem::cont(), i18n("&Stop") );
 }
 
 QString KateSearch::getSearchText()

f3b58c928053049c2ac2a271c0e42b23913934d3
Anders Lund <anders@alweb.dk>
2004-05-30 00:37:38 +0000

    CVS_SILENT fix the licence headers
    svn path=/trunk/kdelibs/kate/; revision=315992

--- part/plugins/wordcompletion/docwordcompletion.cpp
+++ part/plugins/wordcompletion/docwordcompletion.cpp
@@ -1,4 +1,8 @@
 /*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU

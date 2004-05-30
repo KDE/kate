8b42355b21066150bfabcaca599d210a8d2e4355
Anders Lund <anders@alweb.dk>
2004-05-30 00:09:11 +0000

    Adding a document variable interface
    svn path=/trunk/kdelibs/interfaces/ktexteditor/; revision=315987

---
+++ ktexteditor/variableinterface.h
@@ -0,0 +1,70 @@
+/*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+
+    ---
+    Copyright (C) 2004, Anders Lund <anders@alweb.dk>
+*/
+#ifndef _KTEXTEDITOR_VARIABLE_INTERFACE_H_
+#define _KTEXTEDITOR_VARIABLE_INTERFACE_H_
+
+class QString;
+
+namespace KTextEditor {
+
+/**
+ * This interface is designed to provide access to "document variables",
+ * for example variables defined in files like "kate: variable value;"
+ * or the emacs style "-*- variable: value -*-".
+ *
+ * The purpose is to allow KTE plugins to use variables.
+ * A document implementing this interface should return values for variable
+ * that it does not otherwise know how to use, since they could be of interrest
+ * to plugins. A document implementing this interface must emit the variableChanged()
+ * signal whenever a variable is set that it will return a value for.
+ *
+ * @short KTextEditor interface to Document Variables
+ */
+class VariableInterface
+{
+  public:
+    VariableInterface();
+    virtual ~VariableInterface();
+
+    unsigned int variableInterfaceNumber();
+
+    /**
+    * @return the value of the variable @p name, or an empty string if the
+    * variable is not set or has no value.
+    */
+    virtual QString variable( const QString &name ) const = 0;
+
+    //
+    // signals!!
+    //
+  public:
+    /**
+    * Signal: emitted when a variable is set
+    */
+    virtual void variableChanged( const QString &variable, const QString &value ) = 0;
+
+  private:
+    static unsigned int globalVariableInterfaceNumber;
+    unsigned int myVariableInterfaceNumber;
+};
+
+
+VariableInterface *variableInterface( class Document * );
+} // namespace KTextEditor
+#endif //_KTEXTEDITOR_VARIABLE_INTERFACE_H_

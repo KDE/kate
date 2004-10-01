660ed38f4c04f743359be4521afd001205cf69ed
Anders Lund <anders@alweb.dk>
2004-10-01 12:07:26 +0000

    try to at least make the docs readable
    svn path=/trunk/kdelibs/interfaces/ktexteditor/; revision=350777

--- ktexteditor/templateinterface.h
+++ ktexteditor/templateinterface.h
@@ -31,8 +31,9 @@ namespace KTextEditor
 class Document;
 
 /**
-* This is an interface for inserting template strings with user editable fields into a document 
-*/
+ * This is an interface for inserting template strings with user editable
+ * fields into a document.
+ */
 class KTEXTEDITOR_EXPORT TemplateInterface //should be named AbstractTemplateInterface, but for consistency with the other classes it is not (for the 3.x release series)
 {
   friend class PrivateTemplateInterface;
@@ -43,41 +44,52 @@ class KTEXTEDITOR_EXPORT TemplateInterface //should be named AbstractTemplateInt
 
     uint templateInterfaceNumber () const;
 
-  protected:  
-    void setTemplateInterfaceDCOPSuffix (const QCString &suffix);  
-    
+  protected:
+    void setTemplateInterfaceDCOPSuffix (const QCString &suffix);
+
   public:
 
     /**
-     *  Inserts an interactive ediable template text at line "line", column "col". parentWindow is used if dialogs have to be shown
-     *  returns true if success
-     *  Use insertTemplateText(numLines(), ...) to append text at end of document
-     *  Template  strings look like "for( int ${index}=0;${index}<10;${index}++) { ${cursor} };
-     *  This syntax is similiar to the one found in the Eclipse editor
-     *  There are certain common placeholders (variables), which get assigned a default initialValue, If the second parameter does not a given value. for all others the initial value is the name of the placeholder.
+     * Inserts an interactive ediable template text at line "line", column "col".
+     * @p parentWindow is used if dialogs have to be shown
+     * @return true if inserting the string succeeded
+     *
+     * Use insertTemplateText(numLines(), ...) to append text at end of document
+     * Template  strings look like
+     * "for( int ${index}=0;${index}<10;${index}++) { ${cursor} };"
+     * This syntax is similiar to the one found in the Eclipse editor
+     * There are certain common placeholders (variables), which get assigned a
+     * default initialValue, If the second parameter does not a given value.
+     * For all others the initial value is the name of the placeholder.
+     *
      * Placeholder names may only consist of a-zA-Z0-9_
-     *  Common placeholders | values
-     *  index | i
-     * loginname | The current users's loginname
-     * firstname | The current user's first name retrieved from kabc
-     * lastname | The current user's last name retrieved from kabc
-     * fullname | The current user's first and last name retrieved from kabc
-     * email | The current user's primary email adress  retrieved from kabc
-     * date | current date
-     * time | current time
-     * year | current year
-     * month | current month
-     * day | current day
-     * hostname | hostname of the computer
-     * cursor | at this position the cursor will be after editing of the template has finished, this has to be taken care of by the actual implementation. The placeholder gets a value of "|" assigned.
-     * 
-     * If the editor supports some kind of smart indentation, the inserted code should be layouted by the indenter.
+     * Common placeholders and values are
+     *
+     * - index: "i"
+     * - loginname: The current users's loginname
+     * - firstname: The current user's first name retrieved from kabc
+     * - lastname: The current user's last name retrieved from kabc
+     * - fullname: The current user's first and last name retrieved from kabc
+     * - email: The current user's primary email adress  retrieved from kabc
+     * - date: current date
+     * - time: current time
+     * - year: current year
+     * - month: current month
+     * - day: current day
+     * - hostname: hostname of the computer
+     * - cursor: at this position the cursor will be after editing of the
+     *   template has finished, this has to be taken care of by the actual
+     *   implementation. The placeholder gets a value of "|" assigned.
+     *
+     * If the editor supports some kind of smart indentation, the inserted code
+     * should be layouted by the indenter.
      */
     bool insertTemplateText ( uint line, uint column, const QString &templateString, const QMap<QString,QString> &initialValues, QWidget *parentWindow=0);
 
 protected:
     /**
-     * Implement this, it is called by insertTemplateText, after all default values are inserted
+     * You must implement this, it is called by insertTemplateText, after all
+     * default values are inserted.
      */
     virtual bool insertTemplateTextImplementation ( uint line, uint column, const QString &templateString, const QMap<QString,QString> &initialValues, QWidget *parentWindow=0 )=0;
 

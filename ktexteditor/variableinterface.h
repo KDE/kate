661768efc6a47a6572844c4121c77eb96ac78f5e
Dirk Mueller <mueller@kde.org>
2005-07-04 18:46:45 +0000

    update FSF address
    svn path=/trunk/KDE/kdelibs/interfaces/ktexteditor/; revision=431574

--- ktexteditor/variableinterface.h
+++ ktexteditor/variableinterface.h
@@ -10,8 +10,8 @@
 
     You should have received a copy of the GNU Library General Public License
     along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-    Boston, MA 02111-1307, USA.
+    the Free Software Foundation, Inc., 51 Franklin Steet, Fifth Floor,
+    Boston, MA 02110-1301, USA.
 
     ---
     Copyright (C) 2004, Anders Lund <anders@alweb.dk>

595be16b1ecd5b51e5117cd1d279b424ec98cab7
Hamish Rodda <rodda@kde.org>
2005-10-15 12:06:51 +0000

    Oops, forgot to add these
    svn path=/trunk/KDE/kdelibs/kate/; revision=470833

---
+++ part/tests/arbitraryhighlighttest.cpp
@@ -0,0 +1,94 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2005 Hamish Rodda <rodda@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "arbitraryhighlighttest.h"
+
+#include <ktexteditor/document.h>
+#include <ktexteditor/smartinterface.h>
+#include <ktexteditor/attribute.h>
+
+using namespace KTextEditor;
+
+ArbitraryHighlightTest::ArbitraryHighlightTest(Document* parent)
+  : QObject(parent)
+  , m_topRange(smart()->newSmartRange(parent->documentRange()))
+{
+  smart()->addHighlightToDocument(m_topRange);
+  m_topRange->setInsertBehaviour(SmartRange::ExpandRight);
+  connect(m_topRange->notifier(), SIGNAL(contentsChanged(KTextEditor::SmartRange*, KTextEditor::SmartRange*)), SLOT(slotRangeChanged(KTextEditor::SmartRange*, KTextEditor::SmartRange*)));
+}
+
+ArbitraryHighlightTest::~ArbitraryHighlightTest()
+{
+}
+
+Document * ArbitraryHighlightTest::doc( ) const
+{
+  return qobject_cast<Document*>(const_cast<QObject*>(parent()));
+}
+
+KTextEditor::SmartInterface * ArbitraryHighlightTest::smart( ) const
+{
+  return dynamic_cast<SmartInterface*>(doc());
+}
+
+void ArbitraryHighlightTest::slotRangeChanged(SmartRange* range, SmartRange* mostSpecificChild)
+{
+  static Attribute* ranges[10] = {0,0,0,0,0,0,0,0,0,0};
+  static const QChar openBrace = QChar('{');
+  static const QChar closeBrace = QChar('}');
+
+  if (!ranges[0]) {
+    for (int i = 0; i < 10; ++i) {
+      ranges[i] = new Attribute();
+      ranges[i]->setBGColor(QColor(0xFF - i * 8,0xFF,0xFF));
+    }
+  }
+
+  SmartRange* currentRange = mostSpecificChild;
+  currentRange->clearChildRanges();
+  QStringList text = currentRange->text();
+  Cursor current = range->start();
+  foreach (QString string, text) {
+    for (int i = 0; i < string.length(); ++i) {
+      if (string.at(i) == openBrace) {
+        currentRange = smart()->newSmartRange(current, mostSpecificChild->end(), currentRange);
+        if (currentRange->depth() < 10)
+          currentRange->setAttribute(ranges[currentRange->depth()]);
+
+      } else if (string.at(i) == closeBrace && currentRange->parentRange()) {
+        currentRange->end() = current;
+        currentRange = currentRange->parentRange();
+      }
+      current.setColumn(current.column() + 1);
+    }
+    current.setPosition(current.line() + 1, 0);
+  }
+
+  outputRange(range, mostSpecificChild);
+}
+
+void ArbitraryHighlightTest::outputRange( KTextEditor::SmartRange * range, KTextEditor::SmartRange * mostSpecific )
+{
+  kdDebug() << (mostSpecific == range ? "==> " : "       ") << QString(range->depth(), ' ') << *range << endl;
+  foreach (SmartRange* child, range->childRanges())
+    outputRange(child, mostSpecific);
+}
+
+
+#include "arbitraryhighlighttest.moc"
---
+++ part/tests/arbitraryhighlighttest.h
@@ -0,0 +1,55 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2005 Hamish Rodda <rodda@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef ARBITRARYHIGHLIGHTTEST_H
+#define ARBITRARYHIGHLIGHTTEST_H
+
+#include <QObject>
+
+namespace KTextEditor {
+  class Document;
+  class SmartInterface;
+  class SmartRange;
+}
+
+/**
+  Uses the arbitrary highlight interface to show off some capabilities.
+
+  @author Hamish Rodda <rodda@kde.org>
+*/
+class ArbitraryHighlightTest : public QObject
+{
+  Q_OBJECT
+
+  public:
+    ArbitraryHighlightTest(KTextEditor::Document* parent = 0L);
+    virtual ~ArbitraryHighlightTest();
+
+    KTextEditor::Document* doc() const;
+    KTextEditor::SmartInterface* smart() const;
+
+  private slots:
+    void slotRangeChanged(KTextEditor::SmartRange* range, KTextEditor::SmartRange* mostSpecificChild);
+
+  private:
+    void outputRange(KTextEditor::SmartRange* range, KTextEditor::SmartRange * mostSpecific);
+
+    KTextEditor::SmartRange* m_topRange;
+};
+
+#endif

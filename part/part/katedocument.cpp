52f7575699e8c64c5aca03032c5879797c6ae9d7
Laurent Montel <montel@kde.org>
2006-12-14 13:39:28 +0000

    Don't save in session manager a temporary file
    svn path=/trunk/KDE/kdelibs/kate/; revision=613644

--- part/part/katedocument.cpp
+++ part/part/katedocument.cpp
@@ -71,6 +71,7 @@
 #include <ktemporaryfile.h>
 #include <kcodecs.h>
 #include <kstaticdeleter.h>
+#include <kstandarddirs.h>
 
 #include <QtDBus/QtDBus>
 #include <qtimer.h>
@@ -2028,6 +2029,8 @@ void KateDocument::readSessionConfig(KConfig *kconfig)
 
 void KateDocument::writeSessionConfig(KConfig *kconfig)
 {
+  if ( m_url.isLocalFile() && !KGlobal::dirs()->relativeLocation("tmp", m_url.path()).startsWith("/"))
+       return;	
   // save url
   kconfig->writeEntry("URL", m_url.prettyUrl() );
 

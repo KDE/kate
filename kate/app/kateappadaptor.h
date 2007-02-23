2b4d53ad3d2da2002a2febae8a3396568a943880
Christoph Cullmann <cullmann@kde.org>
2007-02-23 19:41:43 +0000

    adjust naming
    svn path=/trunk/KDE/kdesdk/kate/; revision=636668

--- kate/app/kateappadaptor.h
+++ kate/app/kateappadaptor.h
@@ -1,15 +1,15 @@
 /* This file is part of the KDE project
    Copyright (C) 2001 Christoph Cullmann <cullmann@kde.org>
- 
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.
- 
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
- 
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
@@ -25,7 +25,7 @@ class KateApp;
 class KateAppAdaptor : public QDBusAbstractAdaptor
 {
     Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", "org.kde.kate.App")
+    Q_CLASSINFO("D-Bus Interface", "org.kde.Kate.Application")
 
   public:
     KateAppAdaptor (KateApp *app);

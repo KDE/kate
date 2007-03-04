babbb0395427ff3716593efbf642a826161a36dd
Laurent Montel <montel@kde.org>
2007-03-04 11:02:52 +0000

    Move kicker menuext here. Not sense to keep it into kdebase/, kate was moved into kdesdk => if we compiled menuext/kate into kdebase without kate it's not usefull.
    svn path=/trunk/KDE/kdesdk/kate/; revision=639081

---
+++ kate/kicker/menuext/kate/katesessionmenu.h
@@ -0,0 +1,46 @@
+/*
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA  02110-1301, USA.
+
+    ---
+    Copyright (C) 2005-2006, Anders Lund <anders@alweb.dk>
+ */
+
+#ifndef _KateSessionMenu_h_
+#define _KateSessionMenu_h_
+
+#include <plasma/kpanelmenu.h>
+#include <QObject>
+
+class KateSessionMenu : public KPanelMenu {
+  Q_OBJECT
+  public:
+    KateSessionMenu( QWidget *parent=0, const QStringList& args=QStringList() );
+    ~KateSessionMenu();
+
+  public slots:
+    virtual void initialize();
+
+  protected slots:
+    virtual void slotExec( int id );
+
+  private:
+    QStringList m_sessions;
+    QWidget *m_parent;
+};
+
+#endif // _KateSessionMenu_h_
+
+// kate: space-indent on; indent-width 2; replace-tabs on;

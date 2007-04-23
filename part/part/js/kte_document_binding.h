f43f6df2ee3b0da7a6807b830f0a8e62f2f4d33b
Alex Merry <kde@randomguy3.me.uk>
2007-04-23 18:55:12 +0000

    Add copyright notice and license.
    svn path=/trunk/KDE/kdelibs/kate/; revision=657295

--- part/part/js/kte_document_binding.h
+++ part/part/js/kte_document_binding.h
@@ -1,3 +1,21 @@
+/* This file is part of the KDE libraries
+ * Copyright (C) 2007 Matt Broadstone <mbroadst@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #ifndef KTE_DOCUMENT_BINDING_H
 #define KTE_DOCUMENT_BINDING_H
 

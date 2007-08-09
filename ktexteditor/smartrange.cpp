ced3cc5fc6b948653fa5cdca3dad2c466cca116d
David Nolden <david.nolden.kde@art-master.de>
2007-08-09 00:19:28 +0000

    Give a warning when a smart-range is added as a child to another, and another child-range is trimmed/moved so they don't intersect. This is extremely hard to debug, and this at least gives the smart-range user a clue what is going wrong. The alternative would be an assertion, but that would not be appropriate because the interface does not document that this is not allowed.
    svn path=/trunk/KDE/kdelibs/interfaces/ktexteditor/; revision=698070

--- ktexteditor/smartrange.cpp
+++ ktexteditor/smartrange.cpp
@@ -133,7 +133,11 @@ void SmartRange::insertChildRange( SmartRange * newChild )
     if (it.peekPrevious()->end() <= newChild->start()) {
       it.insert(newChild);
       if (it.hasNext() && it.peekNext()->start() < newChild->end())
+      {
+          //Give a warning here, because this most probably results in unwanted behavior, and is extremely hard to debug. This at least gives a clue what'is going wrong. The alternative would be an assertion.
+          kDebug() << "SmartRange warning: " << this << ": Added child-range " << newChild << "(" << *newChild << ") intersects child-range " << it.peekNext() << "(" << *it.peekNext() << "), the second one will be trimmed" << endl;
           it.peekNext()->start() = newChild->end();
+      }
 
       done = true;
       break;
@@ -144,7 +148,10 @@ void SmartRange::insertChildRange( SmartRange * newChild )
 
   if (!done) {
     if (m_childRanges.count() && m_childRanges.first()->start() < newChild->end())
+    {
+        kDebug() << "SmartRange warning: " << this << ": Added child-range " << newChild << "(" << *newChild << ") intersects child-range " << m_childRanges.first() << "(" << *m_childRanges.first() << "), the second one will be trimmed" << endl;
         m_childRanges.first()->start() = newChild->end();
+    }
     m_childRanges.prepend(newChild);
   }
 

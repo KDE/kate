5a9bea233c7ee8b6c1579c3e7ffda9109481648c
Laurent Montel <montel@kde.org>
2007-08-09 20:24:04 +0000

    don't search into plugins. plugins string is always extract into own po file => don't duplicate workd
    svn path=/trunk/KDE/kdelibs/kate/; revision=698312

--- part/Messages.sh
+++ part/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" syntax/data/*.xml >> rc.cpp || exit 12
-$XGETTEXT `find . -name "*.cpp"` -o $podir/katepart4.pot
+$XGETTEXT `find . -name "*.cpp"  | egrep -v "/plugins" ` -o $podir/katepart4.pot

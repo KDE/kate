dbba2034a4ce2f368d0b753bb622c5379d56dc65
Hoàng Đức Hiếu <hieu.d.hoang@gmail.com>
2007-11-07 16:06:52 +0000

    gcc suggests a pair of paren here
    svn path=/trunk/KDE/kdesdk/kate/; revision=733931

--- kate/app/katepluginmanager.cpp
+++ kate/app/katepluginmanager.cpp
@@ -161,7 +161,7 @@ void KatePluginManager::loadPlugin (KatePluginInfo *item)
   if (pluginName.isEmpty())
     pluginName = item->service->library();
 
-  item->load=item->plugin = Kate::createPlugin (QFile::encodeName(item->service->library()), Kate::application(), QStringList(pluginName));
+  item->load = (item->plugin = Kate::createPlugin (QFile::encodeName(item->service->library()), Kate::application(), QStringList(pluginName)));
 }
 
 void KatePluginManager::unloadPlugin (KatePluginInfo *item)

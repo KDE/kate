f9596d33d21909e8238692204f73eca811e4faee
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-11-10 14:33:42 +0000

    fix saving config file on windows
    BUG: 150375
    
    svn path=/trunk/KDE/kdelibs/kate/; revision=734930

--- part/document/katedocument.cpp
+++ part/document/katedocument.cpp
@@ -3092,7 +3092,7 @@ void KateDocument::readSessionConfig(const KConfigGroup &kconfig)
 
 void KateDocument::writeSessionConfig(KConfigGroup &kconfig)
 {
-  if ( this->url().isLocalFile() && !KGlobal::dirs()->relativeLocation("tmp", this->url().path()).startsWith('/'))
+  if ( this->url().isLocalFile() && !QDir(KGlobal::dirs()->relativeLocation("tmp", this->url().path())).isRoot())
        return;
   // save url
   kconfig.writeEntry("URL", this->url().prettyUrl() );

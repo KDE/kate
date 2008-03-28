280bedb77a3726d8fe5912730fce9e546c951c91
Robin Pedersen <robinpeder@gmail.com>
2008-03-28 17:00:22 +0000

    SVN_SILENT Changed indent-width to 2 spaces
    svn path=/trunk/KDE/kdelibs/kate/; revision=791186

--- part/jscript/data/ruby.js
+++ part/jscript/data/ruby.js
@@ -41,127 +41,127 @@ var rxUnindent = /^\s*((end|when|else|elsif|rescue|ensure)\b|[\]\}])(.*)$/;
 // of Kate's other debug output.
 function dbg(s)
 {
-    debug("\u001B[44m" + s + "\u001B[0m");
+  debug("\u001B[44m" + s + "\u001B[0m");
 }
 
 function isCommentAttr(line, column)
 {
-    var attr = document.attribute(line, column);
-    return (attr == 30 || attr == 31);
+  var attr = document.attribute(line, column);
+  return (attr == 30 || attr == 31);
 }
 
 // Return the closest non-empty line, ignoring comments
 // (result <= line). Return -1 if the document
 function findPrevNonCommentLine(line)
 {
-    line = document.prevNonEmptyLine(line);
-    while (line >= 0 && isCommentAttr(line, document.firstColumn(line))) {
-        line = document.prevNonEmptyLine(line - 1);
-    }
-    return line;
+  line = document.prevNonEmptyLine(line);
+  while (line >= 0 && isCommentAttr(line, document.firstColumn(line))) {
+    line = document.prevNonEmptyLine(line - 1);
+  }
+  return line;
 }
 
 function isStmtContinuing(line)
 {
-    // Check for operators at end of line
-    var rx = /(\+|\-|\*|\/|\=|&&|\|\||and|or|,|\\)\s*$/;
+  // Check for operators at end of line
+  var rx = /(\+|\-|\*|\/|\=|&&|\|\||and|or|,|\\)\s*$/;
 
-    return rx.test(document.line(line));
+  return rx.test(document.line(line));
 }
 
 // Return the first line that is not preceeded by a "continuing" line.
 // Return currLine if currLine <= 0
 function findStmtStart(currLine)
 {
-    var p, l = currLine;
-    do {
-        if (l <= 0) return l;
-        p = l;
-        l = findPrevNonCommentLine(l - 1);
-    } while (isStmtContinuing(l));
-    return p;
+  var p, l = currLine;
+  do {
+    if (l <= 0) return l;
+    p = l;
+    l = findPrevNonCommentLine(l - 1);
+  } while (isStmtContinuing(l));
+  return p;
 }
 
 // Returns a tuple that contains the first and last line of the
 // previous statement before line.
 function findPrevStmt(line)
 {
-    var stmtEnd = findPrevNonCommentLine(line);
-    var stmtStart = findStmtStart(stmtEnd);
+  var stmtEnd = findPrevNonCommentLine(line);
+  var stmtStart = findStmtStart(stmtEnd);
 
-    return {start: stmtStart, end: stmtEnd};
+  return {start: stmtStart, end: stmtEnd};
 }
 
 function isBlockStart(currLine)
 {
-    var rx0 = rxIndent;
-    var rx1 = /(do|\{)(\s+\|.*\||\s*)$/;
-    var currStr = document.line(currLine);
+  var rx0 = rxIndent;
+  var rx1 = /(do|\{)(\s+\|.*\||\s*)$/;
+  var currStr = document.line(currLine);
 
-    return (rx0.test(currStr) || rx1.test(currStr))
+  return (rx0.test(currStr) || rx1.test(currStr))
 }
 
 function isBlockEnd(currLine)
 {
-    var currStr = document.line(currLine);
+  var currStr = document.line(currLine);
 
-    return rxUnindent.test(currStr);
+  return rxUnindent.test(currStr);
 }
 
 function findBlockStart(currLine)
 {
-    var nested = 0;
-    var l = currLine;
-    while (true) {
-        if (l < 0) return -1;
+  var nested = 0;
+  var l = currLine;
+  while (true) {
+    if (l < 0) return -1;
 
-        l = findPrevNonCommentLine(l - 1);
-        if (isBlockEnd(l)) {
-            nested++;
-        }
-        if (isBlockStart(l)) {
-            if (nested == 0)
-                return l;
-            else
-                nested--;
-        }
+    l = findPrevNonCommentLine(l - 1);
+    if (isBlockEnd(l)) {
+      nested++;
     }
+    if (isBlockStart(l)) {
+      if (nested == 0)
+        return l;
+      else
+        nested--;
+    }
+  }
 }
 
 // Return the string representing a possible multi-line statement
 // defined by stmt.start and stmt.end
 function getStmtStr(stmt)
 {
-    var str = "";
-    for (var l = stmt.start; l <= stmt.end; ++l) {
-        str += document.line(l).replace(/\\$/, ' ');
-        if (l < stmt.end)
-            str += "\n"
-    }
-    return str;
+  var str = "";
+  for (var l = stmt.start; l <= stmt.end; ++l) {
+    str += document.line(l).replace(/\\$/, ' ');
+    if (l < stmt.end)
+      str += "\n"
+  }
+  return str;
 }
 
 // Return document.attribute at the given offset in a statement
 function getStmtAttr(stmt, offset)
 {
-    var line = stmt.start;
-    while (line < stmt.end && document.lineLength(line) < offset) {
-        offset -= document.lineLength(line++) + 1;
-    }
-    return document.attribute(line, offset);
+  var line = stmt.start;
+  while (line < stmt.end && document.lineLength(line) < offset) {
+    offset -= document.lineLength(line++) + 1;
+  }
+  return document.attribute(line, offset);
 }
 
 // check if the trigger characters are in the right context,
 // otherwise running the indenter might be annoying to the user
 function isValidTrigger(line, ch)
 {
-    if (ch == "" || ch == "\n")
-        return true; // Explicit align or new line
-    if (rxUnindent.test(document.line(line))) {
-        if (RegExp.$3 == "")
-            return true; // Exact match
-    }
-    return false;
+  if (ch == "" || ch == "\n")
+    return true; // Explicit align or new line
+  if (rxUnindent.test(document.line(line))) {
+    if (RegExp.$3 == "")
+      return true; // Exact match
+  }
+  return false;
 }
 
 // TODO: Make an API where multiline statements are passed around like this:
@@ -181,55 +181,57 @@ function isValidTrigger(line, ch)
 // typed character indent
 function indent(line, indentWidth, ch)
 {
-    if (!isValidTrigger(line, ch))
-        return -2;
+  if (!isValidTrigger(line, ch))
+    return -2;
 
-    var prevStmt = findPrevStmt(line - 1);
-    dbg("start: " + prevStmt.start + ", end: " + prevStmt.end);
+  var prevStmt = findPrevStmt(line - 1);
+  dbg("start: " + prevStmt.start + ", end: " + prevStmt.end);
 
-    if (prevStmt.end < 0)
-        return -2; // Can't indent the first line
+  if (prevStmt.end < 0)
+    return -2; // Can't indent the first line
 
-    var prevStmtStr = getStmtStr(prevStmt);
-    dbg("foo"); //FIXME Removing this caused testkateregression to crash the same place as when running indent/csmart
+  var prevStmtStr = getStmtStr(prevStmt);
+  dbg("foo"); //FIXME Removing this caused testkateregression to crash the same place as when running indent/csmart
 
-    var prevStmtInd = document.firstVirtualColumn(prevStmt.start);
+  var prevStmtInd = document.firstVirtualColumn(prevStmt.start);
 
-    // Handle indenting of multiline statements.
-    // Manually indenting to be able to force spaces.
-    if (isStmtContinuing(prevStmt.end) && (prevStmt.end == line-1 || RegExp.$1 != "\\")) {
-        var len = document.firstColumn(prevStmt.end);
-        var str = document.line(prevStmt.end).substr(0, len);
-        if (!document.startsWith(line, str)) {
-            document.removeText(line, 0, line, document.firstColumn(line));
-            if (prevStmt.start == prevStmt.end)
-                str += "    ";
-            document.insertText(line, 0, str);
-        }
-        return -2;
+  // Handle indenting of multiline statements.
+  // Manually indenting to be able to force spaces.
+  if (isStmtContinuing(prevStmt.end) && (prevStmt.end == line-1 || RegExp.$1 != "\\")) {
+    var len = document.firstColumn(prevStmt.end);
+    var str = document.line(prevStmt.end).substr(0, len);
+    if (!document.startsWith(line, str)) {
+      document.removeText(line, 0, line, document.firstColumn(line));
+      if (prevStmt.start == prevStmt.end)
+        str += "    ";
+      document.insertText(line, 0, str);
     }
+    return -2;
+  }
 
-    if (rxUnindent.test(document.line(line))) {
-        var startLine = findBlockStart(line);
-        dbg("currLine: " + line);
-        dbg("StartLine: " + startLine);
-        if (startLine >= 0)
-            return document.firstVirtualColumn(startLine);
-        else
-            return -2;
+  if (rxUnindent.test(document.line(line))) {
+    var startLine = findBlockStart(line);
+    dbg("currLine: " + line);
+    dbg("StartLine: " + startLine);
+    if (startLine >= 0)
+      return document.firstVirtualColumn(startLine);
+    else
+      return -2;
+  }
+
+  if (rxIndent.test(prevStmtStr)) {
+    return prevStmtInd + indentWidth;
+  } else {
+    var p = prevStmtStr.search(/(do|\{)(\s+\|.*\||\s*)$/);
+    if (p != -1) {
+      return prevStmtInd + indentWidth;
+    } else if (prevStmtStr.search(/[\[\{]\s*$/) != -1) {
+      return prevStmtInd + indentWidth;
     }
+  }
 
-    if (rxIndent.test(prevStmtStr)) {
-        return prevStmtInd + indentWidth;
-    } else {
-        var p = prevStmtStr.search(/(do|\{)(\s+\|.*\||\s*)$/);
-        if (p != -1) {
-            return prevStmtInd + indentWidth;
-        } else if (prevStmtStr.search(/[\[\{]\s*$/) != -1) {
-            return prevStmtInd + indentWidth;
-        }
-    }
-
-    // Keep current
-    return prevStmtInd;
+  // Keep current
+  return prevStmtInd;
 }
+
+// kate: indent-width 2; indent-spaces on;

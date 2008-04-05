62e6310d9501bef87b91d1204add2790bbb83505
Paul Giannaros <ceruleanblaze@gmail.com>
2008-04-05 12:53:49 +0000

    Automatic unindenting in python.js for ] or } introduces more problems than it solves. It is comented out for now.
    svn path=/trunk/KDE/kdelibs/kate/; revision=793847

--- part/jscript/data/python.js
+++ part/jscript/data/python.js
@@ -43,8 +43,8 @@ function dbg(s) {
     debug("\u001B[34m" + s + "\u001B[0m");
 }
 
-
-var triggerCharacters = ":}]";
+// XX forget for now
+// var triggerCharacters = "}]";
 
 // General notes:
 // indent() returns the amount of characters (in spaces) to be indented.
@@ -72,7 +72,9 @@ function indent(line, indentWidth, character) {
     }
     // otherwise, check the line contents
     // for :, we simply indent
+    dbg('line without white space: ' + currentLine.sansWhiteSpace().length);
     if(lastLine.endsWith(':')) {
+        dbg('indenting line for :');
         return document.firstVirtualColumn(line - 1) + indentWidth;
     }
     // generally, when a brace is on its own at the end of a regular line
@@ -83,16 +85,21 @@ function indent(line, indentWidth, character) {
     // }
     // etc..
     else if(lastCharacter == '{' || lastCharacter == '[') {
+        dbg('indenting for { or [');
         return document.firstVirtualColumn(line - 1) + indentWidth;
     }
-    // relatively simplistic heuristic for determining whether or not to unindent:
-    // if a }]) has been typed and it's the sole letter on the line, unindent
-    else if((character == '}' || character == ']') && (currentLine.sansWhiteSpace().length == 1)) {
-        return Math.max(0, document.firstVirtualColumn(line - 1) - indentWidth);
-    }
+    // XX this introduces irritating bugs. Commenting it out for now
+    //
+//     // relatively simplistic heuristic for determining whether or not to unindent:
+//     // if a }] has been typed and it's the sole letter on the line, unindent
+//     else if((character == '}' || character == ']') && (currentLine.sansWhiteSpace().length == 1)) {
+//         dbg('unindenting line for } or ]');
+//         return Math.max(0, document.firstVirtualColumn(line - 1) - indentWidth);
+//     }
     // finally, a raise, pass, and continue should unindent
     lastLine = lastLine.stripWhiteSpace();
     if(lastLine == 'continue' || lastLine == 'pass' || lastLine == 'raise' || lastLine.startsWith('raise ')) {
+        dbg('unindenting line for keyword');
         return Math.max(0, document.firstVirtualColumn(line - 1) - indentWidth);
     }
     return -1;

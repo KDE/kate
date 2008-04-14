06fb3f01190558b2f878cf8023098a6c0f92f434
Dominik Haumann <dhaumann@kde.org>
2008-04-14 12:21:22 +0000

    @jowenn: yes, this works, so I removed the line completely (cleaner)
    svn path=/trunk/KDE/kdelibs/kate/; revision=796851

--- part/view/kateview.cpp
+++ part/view/kateview.cpp
@@ -211,7 +211,6 @@ KateView::KateView( KateDocument *doc, QWidget *parent )
   updateConfig ();
 
   slotHlChanged();
-//   KCursor::setAutoHideCursor( this, true );
   KCursor::setAutoHideCursor( m_viewInternal, true );
   /*test texthint
   connect(this,SIGNAL(needTextHint(int, int, QString &)),

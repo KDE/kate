75e0eb5d1fc20ec01efc5b72f146d80efa1e16df
Paul Giannaros <ceruleanblaze@gmail.com>
2008-05-01 15:03:27 +0000

    Relicensed my code to GPL 2+3 instead of just GPL 2
    svn path=/trunk/KDE/kdelibs/kate/; revision=803010

--- part/script/kateindentscript.cpp
+++ part/script/kateindentscript.cpp
@@ -1,10 +1,10 @@
-
 /// This file is part of the KDE libraries
 /// Copyright (C) 2008 Paul Giannaros <paul@giannaros.org>
 ///
 /// This library is free software; you can redistribute it and/or
 /// modify it under the terms of the GNU Library General Public
-/// License version 2 as published by the Free Software Foundation.
+/// License as published by the Free Software Foundation; either
+/// version 2 of the License, or (at your option) version 3.
 ///
 /// This library is distributed in the hope that it will be useful,
 /// but WITHOUT ANY WARRANTY; without even the implied warranty of

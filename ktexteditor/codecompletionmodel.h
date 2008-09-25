96cd4789d7d5b67ba31a0702dbe1c98e20c6d318
Frederik Schwarzer <schwarzerf@gmail.com>
2008-09-25 17:51:35 +0000

    Merge branch 'typos'
    svn path=/trunk/KDE/kdelibs/interfaces/ktexteditor/; revision=864846

--- ktexteditor/codecompletionmodel.h
+++ ktexteditor/codecompletionmodel.h
@@ -413,7 +413,7 @@ class KTEXTEDITOR_EXPORT CodeCompletionModel : public QAbstractItemModel
     virtual int rowCount ( const QModelIndex & parent = QModelIndex() ) const;
 
     /**
-     * This function returns true if the model needs grouping, otherwhise false
+     * This function returns true if the model needs grouping, otherwise false
      * in KDE 4 default value is true, in KDE 5 the default will be false
      * TODO KDE 5
      */

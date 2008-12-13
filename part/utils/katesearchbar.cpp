c211e2bb917584e41a357a50e69114d1ff49af1d
Anders Lund <anders@alweb.dk>
2008-12-13 15:14:38 +0000

    BUG: 176027 Thanks to Sergio Martins
    svn path=/trunk/KDE/kdelibs/kate/; revision=896444

--- part/utils/katesearchbar.cpp
+++ part/utils/katesearchbar.cpp
@@ -193,7 +193,7 @@ KateSearchBar::KateSearchBar(bool initAsPower, KateView* kateView, QWidget* pare
 
 
 KateSearchBar::~KateSearchBar() {
-    delete m_topRange;
+//     delete m_topRange;
     delete m_layout;
     delete m_widget;
 

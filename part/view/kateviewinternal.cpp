ea8283938e1ac41ebd48a64bc92793821b8c94d3
Christoph Cullmann <cullmann@kde.org>
2009-04-22 13:55:54 +0000

    Fix the segfault, don't know, why this case was introduced, but have no idea how that should ever have worked in general displayCursor needs to be up to date... BUG: 190305
    svn path=/trunk/KDE/kdelibs/kate/; revision=957574

--- part/view/kateviewinternal.cpp
+++ part/view/kateviewinternal.cpp
@@ -3345,14 +3345,10 @@ void KateViewInternal::editEnd(int editTagLineStart, int editTagLineEnd, bool ta
 
   updateView(true);
 
-  if (editOldCursor != m_cursor)
+  if (editOldCursor != m_cursor || m_view == m_doc->activeView())
   {
     m_madeVisible = false;
     updateCursor ( m_cursor, true );
-  }
-  else if ( m_view == m_doc->activeView() )
-  {
-    makeVisible(m_displayCursor, m_displayCursor.column());
   }
 
   editIsRunning = false;

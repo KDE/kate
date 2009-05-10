4f66382f1ea856696447772c9ceadb351be396b9
Erlend Hamberg <ehamberg@gmail.com>
2009-05-10 06:57:37 +0000

    indent [count] times when in visual line mode
    BUG: 192178
    
    svn path=/trunk/KDE/kdelibs/kate/; revision=965879

--- part/vimode/katevinormalmode.cpp
+++ part/vimode/katevinormalmode.cpp
@@ -1138,7 +1138,7 @@ bool KateViNormalMode::commandIndentLines()
 
   doc()->editStart();
   for ( int i = line1; i <= line2; i++ ) {
-      doc()->indent( m_view, i, 1 );
+      doc()->indent( m_view, i, getCount() );
   }
   doc()->editEnd();
 
@@ -1156,7 +1156,7 @@ bool KateViNormalMode::commandUnindentLines()
 
   doc()->editStart();
   for ( int i = line1; i <= line2; i++ ) {
-      doc()->indent( m_view, i, -1 );
+      doc()->indent( m_view, i, -getCount() );
   }
   doc()->editEnd();
 

0e1a680a86f4d5328dc77007494552d6210868f0
Dominik Haumann <dhaumann@kde.org>
2009-07-11 22:49:26 +0000

    revert jowenn's revert. moo :)
    svn path=/trunk/KDE/kdesdk/kate/; revision=995088

--- kate/interfaces/kate/plugin.cpp
+++ kate/interfaces/kate/plugin.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
    Copyright (C) 2001 Christoph Cullmann <cullmann@kde.org>
-   Copyright (C) 2002,2005,2009 Joseph Wenninger <jowenn@kde.org>
-   
+   Copyright (C) 2001,2002,2005,2009 Joseph Wenninger <jowenn@kde.org>   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.

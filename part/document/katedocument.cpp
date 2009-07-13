5c941b6cfdcef36f30234069240b54c8f9729217
Pascal Létourneau <pascal.letourneau@gmail.com>
2009-07-13 00:52:59 +0000

    In overwrite mode, paste should overwrite!
    svn path=/trunk/KDE/kdelibs/kate/; revision=995577

--- part/document/katedocument.cpp
+++ part/document/katedocument.cpp
@@ -4041,9 +4041,22 @@ void KateDocument::paste ( KateView* view, QClipboard::Mode mode )
     view->removeSelectedText();
 
   KTextEditor::Cursor pos = view->cursorPosition();
+  bool overwrite = config()->configFlags() & KateDocumentConfig::cfOvr;
+
+  if (overwrite) {
+    QStringList pasteLines = s.split(QLatin1Char('\n'));
+    int maxi = qMin(pos.line() + pasteLines.count(), this->lines());
+
+    for (int i = pos.line(); i < maxi; ++i) {
+      int pasteLength = pasteLines[i-pos.line()].length();
+      removeText(KTextEditor::Range(i, pos.column(), 
+                                    i, qMin(pasteLength + pos.column(), lineLength(i))));
+    }
+  }
+
 
   blockRemoveTrailingSpaces(true);
-  insertText(pos, s, view->blockSelectionMode());
+  insertText(pos, s, view->blockSelectionMode() || overwrite);
   blockRemoveTrailingSpaces(false);
 
   for (int i = pos.line(); i < pos.line() + lines; ++i)

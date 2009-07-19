623024c03097ffa40da3347d5d6e433aeb1907f9
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2009-07-19 23:11:58 +0000

    restore dictionary range stuff, as it is needed in the future
    svn path=/trunk/KDE/kdelibs/kate/; revision=999499

--- part/document/katedocument.cpp
+++ part/document/katedocument.cpp
@@ -6210,6 +6210,11 @@ QString KateDocument::dictionary() const
   return m_dictionary;
 }
 
+QList<QPair<KTextEditor::SmartRange*, QString> > KateDocument::dictionaryRanges()
+{
+  return m_dictionaryRanges;
+}
+
 void KateDocument::setDictionary(const QString& dict)
 {
   if(m_dictionary == dict) {

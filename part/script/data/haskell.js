2be23972278e95dbf0ddc6c68e91e14be8d7dac7
Erlend Hamberg <ehamberg@gmail.com>
2009-08-31 07:17:28 +0000

    several improvements for the Haskell indenter
    * correctly indent '|' used in data types
    * indent 'deriving'
    * add indentation after '{' (record syntax)
    * etc
    
    svn path=/trunk/KDE/kdelibs/kate/; revision=1017670

--- part/script/data/haskell.js
+++ part/script/data/haskell.js
@@ -11,28 +11,27 @@
 
 var debugMode = false;
 
+// escapes text w.r.t. regex special chars
+function escape(text) {
+    return text.replace(/(\/|\.|,|\+|\?|\||\*|\(|\)|\[|\]|\{|\}|\\)/g, "\\$1");
+}
+
 String.prototype.startsWith = function(prefix) {
-    return this.substring(0, prefix.length) == prefix;
+    return this.search(RegExp("^"+escape(prefix)+"(\\b|\\s|$)")) != -1;
 }
 
 String.prototype.endsWith = function(suffix) {
-    var startPos = this.length - suffix.length;
-    if (startPos < 0)
-        return false;
-    return (this.lastIndexOf(suffix, startPos) == startPos);
+    return this.search(RegExp("(\\b|\\s)"+escape(suffix)+"\\s*$")) != -1;
 }
 
 String.prototype.lastCharacter = function() {
     var l = this.length;
-    if(l == 0)
+    if (l == 0)
         return '';
     else
         return this.charAt(l - 1);
 }
 
-String.prototype.sansWhiteSpace = function() {
-    return this.replace(/[ \t\n\r]/g, '');
-}
 String.prototype.stripWhiteSpace = function() {
     return this.replace(/^[ \t\n\r]+/, '').replace(/[ \t\n\r]+$/, '');
 }
@@ -45,7 +44,7 @@ function dbg(s) {
         debug("\u001B[34m" + s + "\u001B[0m");
 }
 
-var triggerCharacters = " \t";
+var triggerCharacters = "|";
 
 // General notes:
 // indent() returns the amount of characters (in spaces) to be indented.
@@ -56,56 +55,82 @@ var triggerCharacters = " \t";
 function indent(line, indentWidth, character) {
     dbg(document.attribute.toString());
     dbg("indent character: '" + character + "'");
-    dbg("line text: " + document.line(line));
     var currentLine = document.line(line);
     dbg("current line: " + currentLine);
     var lastLine = document.line(line - 1);
     var lastCharacter = lastLine.lastCharacter();
-    //
+
     // we can't really indent line 0
-    if(line == 0)
+    if (line == 0)
         return -2;
-    //
+
     // make sure the last line is code
-    if(!document.isCode(line - 1, document.lineLength(line - 1) - 1)
+    if (!document.isCode(line - 1, document.lineLength(line - 1) - 1)
             && lastCharacter != "\"" && lastCharacter != "'") {
         dbg("attributes that we don't want! Returning");
         return -1;
     }
-    // otherwise, check the line contents
 
-    dbg('line without white space: ' + currentLine.sansWhiteSpace().length);
+    // check the line contents ...
+
+    // indent lines following a line ending with '='
+    if (lastLine.endsWith("=")) {
+        dbg('indenting for =');
+        return document.firstVirtualColumn(line - 1) + indentWidth;
+    }
+
+    // indent lines following a line ending with '{'
+    if (lastLine.endsWith("{")) {
+        dbg('indenting for {');
+        return document.firstVirtualColumn(line - 1) + indentWidth;
+    }
+
+    // indent lines following a line ending with 'do'
+    if (lastLine.endsWith("do")) {
+        dbg('indenting for do');
+        return document.firstVirtualColumn(line - 1) + indentWidth;
+    }
 
     // indent line after 'where' 6 characters for alignment:
     // ... where foo = 3
     //     >>>>>>bar = 4
-    if(lastLine.stripWhiteSpace().startsWith('where')) {
+    if (lastLine.stripWhiteSpace().startsWith('where')) {
         dbg('indenting line for where');
         return document.firstVirtualColumn(line - 1) + 6;
+    }
+
+    // indent 'where' to column 0 + indentWidth
+    // fun x = y
+    // >>>>where y = x+1
+    if (currentLine.stripWhiteSpace().startsWith('where')) {
+        dbg('indenting line for where');
+        return indentWidth;
     }
 
     // indent line after 'let' 4 characters for alignment:
     // ... let foo = 3
     //     >>>>bar = 4
-    if(lastLine.stripWhiteSpace().startsWith('let')) {
+    var letCol = lastLine.search(/\blet\b/);
+    if (letCol != -1) {
         dbg('indenting line for let');
-        return document.firstVirtualColumn(line - 1) + 4;
+        return letCol + 4;
     }
 
     // deindent line starting with 'in' to the level of its corresponding 'let':
     // ... let foo = 3
     //         bar = 4
     //     in foo+bar
-    if(currentLine.search(/^\s*in/) != -1) {
+    if (currentLine.stripWhiteSpace().startsWith("in")) {
         dbg('indenting line for in');
-        var temp = line-1;
+        var t = line-1;
         var indent = -1;
-        while (temp >= 0) {
-            if (document.line(temp).sansWhiteSpace().startsWith('let') ) {
-                indent = document.line(temp).search(/\S/);
+        while (t >= 0 && line-t < 100) {
+            var letCol = document.line(t).search(/\blet\b/);
+            if (letCol != -1) {
+                indent = letCol;
                 break;
             }
-            temp = temp-1;
+            t--;
         }
         return indent;
     }
@@ -115,7 +140,7 @@ function indent(line, indentWidth, character) {
     //         bar = 4
     //     in
     //     >>>>foo+bar
-    if(lastLine.stripWhiteSpace() == 'in') {
+    if (lastLine.stripWhiteSpace() == 'in') {
         dbg('indenting line after in');
         return document.firstVirtualColumn(line - 1) + indentWidth;
     }
@@ -125,9 +150,9 @@ function indent(line, indentWidth, character) {
     // >>>>>[] -> ...
     // >>>>>(y:ys) -> ...
     var caseCol = lastLine.search(/\bcase\b/);
-    if(caseCol != -1) {
+    if (caseCol != -1) {
         dbg('indenting line for case');
-        return document.firstVirtualColumn(line - 1) + 5 + caseCol;
+        return caseCol + 5;
     }
 
     // indent line after 'if/else' 3 characters for alignment:
@@ -135,47 +160,76 @@ function indent(line, indentWidth, character) {
     // >>>then baz
     // >>>else vaff
     var ifCol = lastLine.search(/\bif\b/);
-    if(ifCol != -1) {
+    if (ifCol != -1) {
         dbg('indenting line for if');
-        return document.firstVirtualColumn(line - 1) + 3 + ifCol;
+        return ifCol + 3;
     }
 
-    // indent lines starting with '|' (assumed to be guards):
+    // indent line starting with "deriving: ":
+    // data Tree a = Node a (Tree a) (Tree a)
+    //             | Empty
+    // >>>>>>>>>>>>>>deriving (Show)
+    //
+    // - OR -
+    //
+    // data Bool = True | False
+    // >>>>>deriving (Show)
+    if (currentLine.stripWhiteSpace().startsWith('deriving')) {
+        dbg('indenting line for deriving');
+
+        var pipeCol = lastLine.search(/\|/);
+        if (lastLine.stripWhiteSpace().startsWith('data')) {
+            return indentWidth;
+        }
+        else if (pipeCol != -1) {
+            var t = 1;
+            while (lastLine[document.firstVirtualColumn(line - 1)+pipeCol+t] == ' ') {
+                t++;
+            }
+            return pipeCol + t + 1;
+        }
+        else {
+            return document.firstVirtualColumn(line - 1) + 2;
+        }
+    }
+
+    // indent lines starting with '|' (guards or alternate constructors):
     // f x
     // >>| x == 0 = 1
     // >>| x == 0 = x
-    if(currentLine.stripWhiteSpace().startsWith('|')) {
+    //
+    // OR
+    //
+    // data Bool = True
+    // >>>>>>>>>>| False
+    if (currentLine.stripWhiteSpace().startsWith("|")) {
         dbg('indenting line for |');
-        return indentWidth;
-    }
-
-    // indent lines following a line ending with '='
-    if(lastLine.stripWhiteSpace().endsWith('=')) {
-        dbg('indenting for =');
-        return document.firstVirtualColumn(line - 1) + indentWidth;
-    }
-
-    // indent lines following a line ending with 'do'
-    if(lastLine.stripWhiteSpace().endsWith('do')) {
-        dbg('indenting for do');
-        return document.firstVirtualColumn(line - 1) + indentWidth;
+        var equalsCol = lastLine.search(/=/);
+        var pipeCol = lastLine.search(/\|/);
+        if (equalsCol != -1 && lastLine.stripWhiteSpace().startsWith('data')) {
+            return equalsCol;
+        }
+        else if (pipeCol != -1) {
+            return pipeCol;
+        }
+        else {
+            return document.firstVirtualColumn(line - 1) + indentWidth;
+        }
     }
 
     // line ending with !#$%&*+./<=>?@\^|~-
-    if (lastLine.search(/[!$#%&*+.\/<=>?@\\^|~-]$/) != -1) {
-        dbg('indenting for operator');
-        return document.firstVirtualColumn(line - 1) + indentWidth;
-    }
+    //if (lastLine.search(/[!$#%&*+.\/<=>?@\\^|~-]$/) != -1) {
+    //    dbg('indenting for operator');
+    //    return document.firstVirtualColumn(line - 1) + indentWidth;
+    //}
 
     if (lastLine.search(/^\s*$/) != -1) {
         dbg('indenting for empty line');
         return 0;
     }
 
-
     dbg('continuing with regular indent');
     return -1;
 }
-
 
 // kate: space-indent on; indent-width 4; replace-tabs on;

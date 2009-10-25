4a7cfe4bee1e233e8d2bc40ecfc95ed357e7bbf2
Dominik Haumann <dhaumann@kde.org>
2009-10-25 15:36:14 +0000

    fix wrong indentation template
    svn path=/trunk/KDE/kdelibs/kate/; revision=1040097

--- part/script/data/indentation_template.js
+++ part/script/data/indentation_template.js
@@ -1,16 +1,13 @@
 /* kate-script
  * name: Indenter name (appears in the menu)
- * license: license (GPL, LGPL, Artistic, etc)
+ * license: license (BSD, GPL, LGPL, Artistic, etc)
  * author: first name last name <email-address>
  * revision: 1 (simple integer number)
  * kate-version: 3.4
  */
 
-// indent gets three arguments: line, indentwidth in spaces, typed character 
-indenter.indent = indentLine;
-
 // specifies the characters which should trigger indent, beside the default '\n'
-indenter.triggerCharacters = "{}/:;";
+triggerCharacters = "{}/:;";
 
 /**
  * Indent a line.
@@ -24,30 +21,30 @@ indenter.triggerCharacters = "{}/:;";
  * - return -2; - do nothing
  * - return -1; - keep indentation (searches for previous non-blank line)
  * - return  0; - All numbers >= 0 are the indent-width in spaces
- * 
+ *
  * Alternatively, an array of two elements can be returned:
  * return [ indent, align ];
- * 
+ *
  * The first element is the indent-width like above, with the same meaning
  * of the special values.
- * 
+ *
  * The second element is an absolute value representing a column for
  * "alignment". If this value is higher than the indent value, the
  * difference represents a number of spaces to be added after the indent.
  * Otherwise, it's ignored.
- * 
+ *
  * Example:
  * Assume using tabs to indent, and tab width is 4. Here ">" represents a
  * tab, and "." represents a space:
  * 1: >   >   foobar("hello",
  * 2: >   >   ......."world")
- * 
+ *
  * When indenting line 2, the script returns [8, 15]. Two tabs are inserted
  * to indent to column 8, and 7 spaces are added to align the second
  * parameter under the first, so that it stays aligned if the file is viewed
  * with a different tab width.
  */
-function indentLine(line, indent, ch)
+function indent(line, indentWidth, ch)
 {
     return -2;
 }

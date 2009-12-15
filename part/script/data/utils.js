43eeaea733f3f6a7314fcb1116a893676c072ab6
Milian Wolff <mail@milianw.de>
2009-12-15 00:36:39 +0000

    add uniq and natsort helper scripts. javascript natcompare implementation (c) 2005 by SCK-CEN (Belgian Nucleair Research Centre), provided 'as-is'. see embedded license for more info.
    svn path=/trunk/KDE/kdelibs/kate/; revision=1062522

--- part/script/data/utils.js
+++ part/script/data/utils.js
@@ -1,32 +1,36 @@
 /* kate-script
  * author: Dominik Haumann <dhdev@gmx.de>
  * license: LGPL
- * revision: 1
+ * revision: 2
  * kate-version: 3.4
  * type: commands
- * functions: sort, test
+ * functions: sort, natsort, uniq, test
  */
 
 function sort()
 {
-    var selection = view.selection();
-    if (selection.isValid()) {
-        selection.start.column = 0;
-        selection.end.column = document.lineLength(selection.end.line);
+    each(function(lines){return lines.sort()});
+}
 
-        var text = document.text(selection);
+function uniq()
+{
+    each(function(lines) {
+        for ( var i = 1; i < lines.length; ++i ) {
+          for ( var j = i - 1; j >= 0; --j ) {
+            if ( lines[i] == lines[j] ) {
+              lines.splice(i, 1);
+              // gets increased in the for
+              --i;
+              break;
+            }
+          }
+        }
+    });
+}
 
-        var lines = text.split("\n");
-        lines.sort();
-        text = lines.join("\n");
-
-        view.clearSelection();
-
-        document.editBegin();
-        document.removeText(selection);
-        document.insertText(selection.start, text);
-        document.editEnd();
-    }
+function natsort()
+{
+    each(function(lines){return lines.sort(natcompare);});
 }
 
 function test()
@@ -51,5 +55,202 @@ function help(cmd)
 {
     if (cmd == "sort") {
         return "Sort the selected text.";
+    } else if (cmd == "uniq") {
+        return "Remove duplicate lines from the selected text.";
+    } else if (cmd == "natsort") {
+        return "Sort the selected text in natural order.<br>"
+              +"Here's an example to show the difference to the normal sort method:<br>"
+              +"sort(a10, a1, a2) => a1, a10, a2<br>"
+              +"natsort(a10, a1, a2) => a1, a2, a10";
     }
 }
+
+/// helper code below:
+
+function each(func)
+{
+    if ( typeof(func) != "function" ) {
+        debug("parameter is not a function: " + typeof(func));
+        return;
+    }
+
+    var selection = view.selection();
+    if (!selection.isValid()) {
+        debug("invalid selection");
+        return;
+    }
+
+    selection.start.column = 0;
+    selection.end.column = document.lineLength(selection.end.line);
+
+    var text = document.text(selection);
+
+    var lines = text.split("\n");
+    lines = func(lines);
+    text = lines.join("\n");
+
+    view.clearSelection();
+
+    document.editBegin();
+    document.removeText(selection);
+    document.insertText(selection.start, text);
+    document.editEnd();
+}
+
+/*
+natcompare.js -- Perform 'natural order' comparisons of strings in JavaScript.
+Copyright (C) 2005 by SCK-CEN (Belgian Nucleair Research Centre)
+Written by Kristof Coomans <kristof[dot]coomans[at]sckcen[dot]be>
+
+Based on the Java version by Pierre-Luc Paour, of which this is more or less a straight conversion.
+Copyright (C) 2003 by Pierre-Luc Paour <natorder@paour.com>
+
+The Java version was based on the C version by Martin Pool.
+Copyright (C) 2000 by Martin Pool <mbp@humbug.org.au>
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+
+function isWhitespaceChar(a)
+{
+    var charCode;
+    charCode = a.charCodeAt(0);
+
+    if ( charCode <= 32 )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function isDigitChar(a)
+{
+    var charCode;
+    charCode = a.charCodeAt(0);
+
+    if ( charCode >= 48  && charCode <= 57 )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function compareRight(a,b)
+{
+    var bias = 0;
+    var ia = 0;
+    var ib = 0;
+
+    var ca;
+    var cb;
+
+    // The longest run of digits wins.  That aside, the greatest
+    // value wins, but we can't know that it will until we've scanned
+    // both numbers to know that they have the same magnitude, so we
+    // remember it in BIAS.
+    for (;; ia++, ib++) {
+        ca = a.charAt(ia);
+        cb = b.charAt(ib);
+
+        if (!isDigitChar(ca)
+                && !isDigitChar(cb)) {
+            return bias;
+        } else if (!isDigitChar(ca)) {
+            return -1;
+        } else if (!isDigitChar(cb)) {
+            return +1;
+        } else if (ca < cb) {
+            if (bias == 0) {
+                bias = -1;
+            }
+        } else if (ca > cb) {
+            if (bias == 0)
+                bias = +1;
+        } else if (ca == 0 && cb == 0) {
+            return bias;
+        }
+    }
+}
+
+function natcompare(a,b) {
+
+    var ia = 0, ib = 0;
+        var nza = 0, nzb = 0;
+        var ca, cb;
+        var result;
+
+    while (true)
+    {
+        // only count the number of zeroes leading the last number compared
+        nza = nzb = 0;
+
+        ca = a.charAt(ia);
+        cb = b.charAt(ib);
+
+        // skip over leading spaces or zeros
+        while ( isWhitespaceChar( ca ) || ca =='0' ) {
+            if (ca == '0') {
+                nza++;
+            } else {
+                // only count consecutive zeroes
+                nza = 0;
+            }
+
+            ca = a.charAt(++ia);
+        }
+
+        while ( isWhitespaceChar( cb ) || cb == '0') {
+            if (cb == '0') {
+                nzb++;
+            } else {
+                // only count consecutive zeroes
+                nzb = 0;
+            }
+
+            cb = b.charAt(++ib);
+        }
+
+        // process run of digits
+        if (isDigitChar(ca) && isDigitChar(cb)) {
+            if ((result = compareRight(a.substring(ia), b.substring(ib))) != 0) {
+                return result;
+            }
+        }
+
+        if (ca == 0 && cb == 0) {
+            // The strings compare the same.  Perhaps the caller
+            // will want to call strcmp to break the tie.
+            return nza - nzb;
+        }
+
+        if (ca < cb) {
+            return -1;
+        } else if (ca > cb) {
+            return +1;
+        }
+
+        ++ia; ++ib;
+    }
+}
+

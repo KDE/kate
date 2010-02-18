83885ac774a0be47b5a0721d77578843c509a6c9
Christoph Cullmann <cullmann@kde.org>
2010-02-18 12:13:27 +0000

    erlend is ok with lgpl2+, ok'd here at sprint, and I asked pdgiddie@googlemail.com via mail, he is ok too
    svn path=/trunk/KDE/kdelibs/kate/; revision=1092145

--- part/vimode/katevivisualmode.h
+++ part/vimode/katevivisualmode.h
@@ -1,21 +1,22 @@
-/* This file is part of the KDE libraries
- * Copyright (C) 2008 - 2009 Erlend Hamberg <ehamberg@gmail.com>
- * Copyright (C) 2009 Paul Gideon Dann <pdgiddie@gmail.com>
+/*  This file is part of the KDE libraries and the Kate part.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) version 3.
+ *  Copyright (C) 2008 - 2009 Erlend Hamberg <ehamberg@gmail.com>
+ *  Copyright (C) 2009 Paul Gideon Dann <pdgiddie@gmail.com>
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
  */
 
 #ifndef KATE_VI_VISUAL_MODE_INCLUDED

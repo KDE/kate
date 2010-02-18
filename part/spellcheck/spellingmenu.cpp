d0933a69cfa339d2568b10ded5e5c9e55056ca65
Christoph Cullmann <cullmann@kde.org>
2010-02-18 17:58:31 +0000

    LGPL2+, got ok from ludwig, corrected wrong date in one diff
    svn path=/trunk/KDE/kdelibs/kate/; revision=1092313

--- part/spellcheck/spellingmenu.cpp
+++ part/spellcheck/spellingmenu.cpp
@@ -1,6 +1,6 @@
 /*  This file is part of the KDE libraries and the Kate part.
  *
- *  Copyright (C) 2009 by Michel Ludwig <michel.ludwig@kdemail.net>
+ *  Copyright (C) 2009-2010 by Michel Ludwig <michel.ludwig@kdemail.net>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public

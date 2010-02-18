43ea9ac2d4c834f765b9a4c0ebec393eab262ca9
Christoph Cullmann <cullmann@kde.org>
2010-02-18 18:00:25 +0000

    LGPL2+, got ok from ludwig, dialog too, is mostly rewritten by ludwid
    svn path=/trunk/KDE/kdelibs/kate/; revision=1092314

--- part/spellcheck/spellcheckdialog.cpp
+++ part/spellcheck/spellcheckdialog.cpp
@@ -1,27 +1,28 @@
-/* This file is part of the KDE libraries
-   Copyright (C) 2009 Michel ludwig <michel.ludwig@kdemail.net>
-   Copyright (C) 2008 Mirko Stocker <me@misto.ch>
-   Copyright (C) 2004-2005 Anders Lund <anders@alweb.dk>
-   Copyright (C) 2003 Clarence Dang <dang@kde.org>
-   Copyright (C) 2002 John Firebaugh <jfirebaugh@kde.org>
-   Copyright (C) 2001-2004 Christoph Cullmann <cullmann@kde.org>
-   Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
-   Copyright (C) 1999 Jochen Wilhelmy <digisnap@cs.tu-berlin.de>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
+/*  This file is part of the KDE libraries and the Kate part.
+ *
+ *  Copyright (C) 2009 by Michel Ludwig <michel.ludwig@kdemail.net>
+ *  Copyright (C) 2008 Mirko Stocker <me@misto.ch>
+ *  Copyright (C) 2004-2005 Anders Lund <anders@alweb.dk>
+ *  Copyright (C) 2002 John Firebaugh <jfirebaugh@kde.org>
+ *  Copyright (C) 2001-2004 Christoph Cullmann <cullmann@kde.org>
+ *  Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
+ *  Copyright (C) 1999 Jochen Wilhelmy <digisnap@cs.tu-berlin.de>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
 
 #include "spellcheckdialog.h"
 #include "spellcheckdialog.moc"

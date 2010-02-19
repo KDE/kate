996ab4b88eb021b9b254a3c8e71ec4c05410273b
Joseph Wenninger <kde@jowenn.at>
2010-02-19 10:10:41 +0000

    - lgplv2+:   staniek (   2 LOC): 346274
    windows exports only -> minor
    -> LGPLv2->LGPLv2+
    
    svn path=/trunk/KDE/kdelibs/interfaces/ktexteditor/; revision=1092578

--- ktexteditor/texthintinterface.h
+++ ktexteditor/texthintinterface.h
@@ -3,7 +3,8 @@
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +16,6 @@
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
-
 #ifndef KDELIBS_KTEXTEDITOR_TEXTHINTINTERFACE_H
 #define KDELIBS_KTEXTEDITOR_TEXTHINTINTERFACE_H
 

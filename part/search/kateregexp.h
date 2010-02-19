839eaac315f042ffbfefd71684a485d31f99262f
Christoph Cullmann <cullmann@kde.org>
2010-02-19 09:43:06 +0000

    got sebastian's ok, he is in the relicensing script now, too, LGPL2+
    svn path=/trunk/KDE/kdelibs/kate/; revision=1092558

--- part/search/kateregexp.h
+++ part/search/kateregexp.h
@@ -1,21 +1,23 @@
-/* This file is part of the KDE libraries
-   Copyright (C) 2009 Bernhard Beschow <bbeschow@cs.tu-berlin.de>
-   Copyright (C) 2007 Sebastian Pipping <webmaster@hartwork.org>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02111-13020, USA.
-*/
+/*  This file is part of the KDE libraries and the Kate part.
+ *
+ *  Copyright (C) 2009 Bernhard Beschow <bbeschow@cs.tu-berlin.de>
+ *  Copyright (C) 2007 Sebastian Pipping <webmaster@hartwork.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
 
 #ifndef _KATE_REGEXP_H_
 #define _KATE_REGEXP_H_

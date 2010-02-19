7756150f4a670f3f7cc06bc4dcf1f405bbbe7049
Christoph Cullmann <cullmann@kde.org>
2010-02-19 11:32:20 +0000

    lgpl2+, script gives ok
    svn path=/trunk/KDE/kdelibs/kate/; revision=1092674

--- part/utils/katepartpluginmanager.cpp
+++ part/utils/katepartpluginmanager.cpp
@@ -1,23 +1,25 @@
-/* This file is part of the KDE project
-   Copyright 2001 Christoph Cullmann <cullmann@kde.org>
-   Copyright 2001 Joseph Wenninger <jowenn@kde.org>
-   Copyright 2001 Anders Lund <anders.lund@lund.tdcadsl.dk>
-   Copyright 2007 Dominik Haumann <dhaumann kde org>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
+/*  This file is part of the KDE libraries and the Kate part.
+ *
+ *  Copyright (C) 2001-2010 Christoph Cullmann <cullmann@kde.org>
+ *  Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
+ *  Copyright (C) 2001 Anders Lund <anders.lund@lund.tdcadsl.dk>
+ *  Copyright (C) 2007 Dominik Haumann <dhaumann kde org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
 
 #include "katepartpluginmanager.h"
 #include "katepartpluginmanager.moc"

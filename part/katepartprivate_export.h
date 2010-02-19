537e6d5597ffa4ca708b79d62a5f05b4bf0862be
Christoph Cullmann <cullmann@kde.org>
2010-02-19 10:54:00 +0000

    export header LGPL2+
    svn path=/trunk/KDE/kdelibs/kate/; revision=1092616

--- part/katepartprivate_export.h
+++ part/katepartprivate_export.h
@@ -1,27 +1,24 @@
-/*
- * This file is a part of the rekonq project
+/*  This file is part of the KDE libraries and the Kate part.
  *
- * Copyright (C) 2007 David Faure <faure@kde.org>
- * Copyright (C) 2009 by Bernhard Beschow <bbeschow@cs.tu-berlin.de>
+ *  Copyright (C) 2007 David Faure <faure@kde.org>
+ *  Copyright (C) 2009 by Bernhard Beschow <bbeschow@cs.tu-berlin.de>
  *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
  */
+
 #ifndef KATEPARTPRIVATE_EXPORT_H
 #define KATEPARTPRIVATE_EXPORT_H
 

16f65deb6c0260f20df090143769390fc18a480a
David Nolden <david.nolden.kde@art-master.de>
2010-07-13 09:14:48 +0000

    Add versions of the textChanged and textRemoved signals that also give the text that was removed. This is required in KDevelop to get a precise view of what happend in the editor.
    svn path=/trunk/KDE/kdelibs/interfaces/ktexteditor/; revision=1149352

--- ktexteditor/document.h
+++ ktexteditor/document.h
@@ -568,6 +568,16 @@ class KTEXTEDITOR_EXPORT Document : public KParts::ReadWritePart
     void textRemoved(KTextEditor::Document *document, const KTextEditor::Range& range);
 
     /**
+     * The \p document emits this signal whenever \p range was removed, i.e.
+     * text was removed.
+     * \param document document which emitted this signal
+     * \param range range that the removed text previously occupied
+     * \param oldText the text that has been removed
+     * \see removeText(), removeLine(), clear()
+     */
+    void textRemoved(KTextEditor::Document *document, const KTextEditor::Range& range, const QString& oldText);
+    
+    /**
      * The \p document emits this signal whenever the text in range
      * \p oldRange was removed and replaced with the text now in \e newRange,
      * e.g. the user selects text and pastes new text to replace the selection.
@@ -579,6 +589,19 @@ class KTEXTEDITOR_EXPORT Document : public KParts::ReadWritePart
      */
     void textChanged(KTextEditor::Document *document, const KTextEditor::Range& oldRange, const KTextEditor::Range& newRange);
 
+    /**
+     * The \p document emits this signal whenever the text in range
+     * \p oldRange was removed and replaced with the text now in \e newRange,
+     * e.g. the user selects text and pastes new text to replace the selection.
+     * \note \p oldRange.start() is guaranteed to equal \p newRange.start().
+     * \param document document which emitted this signal
+     * \param oldRange range that the text previously occupied
+     * \param oldText old text that has been replaced
+     * \param newRange range that the changed text now occupies
+     * \see insertText(), insertLine(), removeText(), removeLine(), clear()
+     */
+    void textChanged(KTextEditor::Document *document, const KTextEditor::Range& oldRange, const QString& oldText, const KTextEditor::Range& newRange);
+    
     /**
      * Warn anyone listening that the current document is about to close.
      * At this point all of the information is still accessible, such as the text,

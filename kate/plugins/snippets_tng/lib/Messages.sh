eb4c335ff4154a803961ab27998b3790be140b2e
Albert Astals Cid <tsdgeos@terra.es>
2010-08-21 19:45:44 +0000

    do not rely on extract-messages.sh doing the work for us
    svn path=/trunk/KDE/kdesdk/kate/; revision=1166432

--- kate/plugins/snippets_tng/lib/Messages.sh
+++ kate/plugins/snippets_tng/lib/Messages.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
+$EXTRACTRC *.ui >> rc.cpp
 $XGETTEXT *.cpp -o $podir/ktexteditor_codesnippets_core.pot
--- kate/plugins/tabbarextension/Messages.sh
+++ kate/plugins/tabbarextension/Messages.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
+$EXTRACTRC *.rc *.ui >> rc.cpp
 $XGETTEXT *.cpp *.h -o $podir/katetabbarextension.pot

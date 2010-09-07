f5cbda93cc2c4413e0b78dbafed35775b0722668
Christoph Cullmann <cullmann@kde.org>
2010-09-07 19:05:11 +0000

    mwolff:     make sure that the returned default style has the correct background
    svn path=/trunk/KDE/kdelibs/kate/; revision=1172670

--- part/document/katedocument.cpp
+++ part/document/katedocument.cpp
@@ -5211,8 +5211,13 @@ KTextEditor::Attribute::Ptr KateDocument::defaultStyle(const KTextEditor::Highli
     return KTextEditor::Attribute::Ptr(0);
   }
 
-  KateAttributeList list;
-  return highlight()->attributes(view->renderer()->config()->schema()).at(ds);
+  KTextEditor::Attribute::Ptr style = highlight()->attributes(view->renderer()->config()->schema()).at(ds);
+  if (!style->hasProperty(QTextFormat::BackgroundBrush)) {
+    // make sure the returned style has the default background color set
+    style.attach(new KTextEditor::Attribute(*style));
+    style->setBackground( QBrush(view->renderer()->config()->backgroundColor()) );
+  }
+  return style;
 }
 
 QList< KTextEditor::HighlightInterface::AttributeBlock > KateDocument::lineAttributes(const unsigned int line)

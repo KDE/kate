3993a04052816c64c7a9a40b71cb5204c46197b2
Pascal Létourneau <pascal.letourneau@gmail.com>
2010-09-19 00:59:46 +0000

    Revert r1099555
    BUG:247965
    
    svn path=/trunk/KDE/kdelibs/kate/; revision=1176897

--- part/search/katesearchbar.cpp
+++ part/search/katesearchbar.cpp
@@ -351,7 +351,9 @@ void KateSearchBar::indicateMatch(MatchResult matchResult) {
 
 
 void KateSearchBar::selectRange2(const KTextEditor::Range & range) {
+    disconnect(m_view, SIGNAL(selectionChanged(KTextEditor::View *)), this, SLOT(updateSelectionOnly()));
     selectRange(m_view, range);
+    connect(m_view, SIGNAL(selectionChanged(KTextEditor::View *)), this, SLOT(updateSelectionOnly()));
 }
 
 

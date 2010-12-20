KDE/kate

commit f52c93919a158decd3399d0893efa49e57547009

also add history in incremental search mode
svn path=/trunk/KDE/kdelibs/kate/; revision=1190633

part/search/katesearchbar.cpp
@@ -244,9 +244,11 @@ void KateSearchBar::setSearchMode(KateSearchBar::SearchMode mode) {
 void KateSearchBar::findNext() {
     const bool found = find();
 
-    if (found && m_powerUi != NULL) {
+    if (found) {
+        QComboBox *combo = m_powerUi != 0 ? m_powerUi->pattern : m_incUi->pattern;
+
         // Add to search history
-        addCurrentTextToHistory(m_powerUi->pattern);
+        addCurrentTextToHistory(combo);
     }
 }
 
@@ -255,9 +257,11 @@ void KateSearchBar::findNext() {
 void KateSearchBar::findPrevious() {
     const bool found = find(SearchBackward);
 
-    if (found && m_powerUi != NULL) {
+    if (found) {
+        QComboBox *combo = m_powerUi != 0 ? m_powerUi->pattern : m_incUi->pattern;
+
         // Add to search history
-        addCurrentTextToHistory(m_powerUi->pattern);
+        addCurrentTextToHistory(combo);
     }
 }
 
@@ -654,11 +658,11 @@ void KateSearchBar::givePatternFeedback() {
 void KateSearchBar::addCurrentTextToHistory(QComboBox * combo) {
     const QString text = combo->currentText();
     const int index = combo->findText(text);
-    if (index != -1) {
+
+    if (index > 0)
         combo->removeItem(index);
-    }
-    combo->insertItem(0, text);
-    combo->setCurrentIndex(0);
+    if (index != 0)
+        combo->insertItem(0, text);
 }
 
 
@@ -1255,9 +1259,11 @@ void KateSearchBar::enterPowerMode() {
         m_layout->addWidget(m_widget);
 
         // Bind to shared history models
+        m_powerUi->pattern->setDuplicatesEnabled(false);
         m_powerUi->pattern->setInsertPolicy(QComboBox::InsertAtTop);
         m_powerUi->pattern->setMaxCount(m_config->maxHistorySize());
         m_powerUi->pattern->setModel(m_config->patternHistoryModel());
+        m_powerUi->replacement->setDuplicatesEnabled(false);
         m_powerUi->replacement->setInsertPolicy(QComboBox::InsertAtTop);
         m_powerUi->replacement->setMaxCount(m_config->maxHistorySize());
         m_powerUi->replacement->setModel(m_config->replacementHistoryModel());
@@ -1418,6 +1424,7 @@ void KateSearchBar::enterIncrementalMode() {
         // Focus proxy
         centralWidget()->setFocusProxy(m_incUi->pattern);
 
+        m_incUi->pattern->setDuplicatesEnabled(false);
         m_incUi->pattern->setInsertPolicy(QComboBox::InsertAtTop);
         m_incUi->pattern->setMaxCount(m_config->maxHistorySize());
         m_incUi->pattern->setModel(m_config->patternHistoryModel());

part/tests/searchbar_test.cpp
@@ -72,6 +72,42 @@ SearchBarTest::~SearchBarTest()
 }
 
 
+void SearchBarTest::testFindNextIncremental()
+{
+  KateDocument doc(false, false, false);
+  doc.setText("a a a b b");
+
+  KateView view(&doc, 0);
+  KateViewConfig config(&view);
+
+  KateSearchBar bar(false, &view, &config);
+
+  bar.setSearchPattern("b");
+
+  QCOMPARE(view.selectionRange(), Range(0, 6, 0, 7));
+
+  bar.findNext();
+
+  QCOMPARE(view.selectionRange(), Range(0, 8, 0, 9));
+
+  bar.setSearchPattern("a");
+
+  QCOMPARE(view.selectionRange(), Range(0, 0, 0, 1));
+
+  bar.findNext();
+
+  QCOMPARE(view.selectionRange(), Range(0, 2, 0, 3));
+
+  bar.findNext();
+
+  QCOMPARE(view.selectionRange(), Range(0, 4, 0, 5));
+
+  bar.findNext();
+
+  QCOMPARE(view.selectionRange(), Range(0, 0, 0, 1));
+}
+
+
 void SearchBarTest::testSetMatchCaseIncremental()
 {
   KateDocument doc(false, false, false);

part/tests/searchbar_test.h
@@ -31,6 +31,8 @@ public:
   ~SearchBarTest();
 
 private Q_SLOTS:
+  void testFindNextIncremental();
+
   void testSetMatchCaseIncremental();
 
   void testSetMatchCasePower();

commit 9ba4d7adbe6c5350cc1f885b878ff13d5b28c383

fix regression introduced in f52c93919a158decd3399d0893efa49e57547009 properly set the current index in addCurrentTextToHistory
BUG: 248305

part/search/katesearchbar.cpp
@@ -663,8 +663,10 @@ void KateSearchBar::addCurrentTextToHistory(QComboBox * combo) {
 
     if (index > 0)
         combo->removeItem(index);
-    if (index != 0)
+    if (index != 0) {
         combo->insertItem(0, text);
+        combo->setCurrentIndex(0);
+    }
 }
 
 

part/tests/searchbar_test.cpp
@@ -564,6 +564,12 @@ void SearchBarTest::testSearchHistoryIncremental()
 
   QCOMPARE(bar2.m_incUi->pattern->findText("bar"), 0);
   QCOMPARE(bar2.m_incUi->pattern->findText("foo"), 1);
+
+  //testcase for https://bugs.kde.org/show_bug.cgi?id=248305
+  bar2.m_incUi->pattern->setCurrentIndex(1);
+  QCOMPARE(bar2.searchPattern(), QLatin1String("foo"));
+  bar2.findNext();
+  QCOMPARE(bar2.searchPattern(), QLatin1String("foo"));
 }
 
 void SearchBarTest::testSearchHistoryPower()
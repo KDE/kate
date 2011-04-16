0606ad7ed195add21335ff7d8467a1dbd1424be4
Dominik Haumann <dhdev@gmx.de>
2011-04-16 13:31:09 +0200

    do not make matches bold
    BUG: 270861

--- part/plugins/hlselection/hlselectionplugin.cpp
+++ part/plugins/hlselection/hlselectionplugin.cpp
@@ -142,7 +142,8 @@ void HighlightSelectionPluginView::createHighlights()
     qobject_cast<KTextEditor::MovingInterface*>(m_view->document());
 
   KTextEditor::Attribute::Ptr attr(new KTextEditor::Attribute());
-  attr->setFontBold(true);
+// disable bold for now: If you use non-fixed font, making it bold leads to wobbly text
+//  attr->setFontBold(true);
   attr->setBackground(Qt::yellow);
 
   KTextEditor::Cursor start(0, 0);

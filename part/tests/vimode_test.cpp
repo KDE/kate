KDE/kate

commit 82202bdedea764698a027cb39920fafb1d46e6fe

Allow "telescoping" mappings i.e. ones that are substrings of other mappings e.g.
'12
'123

etc

If the current set of keystrokes is a prefix of a mapping, then wait for the remaining keypresses that brings us up to that mapping. If that doesn't occur before the timeout occurs, choose the current mapping.  Ensure that we don't hang around waiting for timeouts if the current mapping is unambiguous.

part/vimode/katevinormalmode.cpp
@@ -82,7 +82,14 @@ void KateViNormalMode::mappingTimerTimeOut()
 {
   kDebug( 13070 ) << "timeout! key presses: " << m_mappingKeys;
   m_mappingKeyPress = true;
-  m_viInputModeManager->feedKeyPresses( m_mappingKeys );
+  if (!m_fullMappingMatch.isNull())
+  {
+    m_viInputModeManager->feedKeyPresses( getMapping(m_fullMappingMatch ));
+  }
+  else
+  {
+    m_viInputModeManager->feedKeyPresses( m_mappingKeys );
+  }
   m_mappingKeyPress = false;
   m_mappingKeys.clear();
 }
@@ -120,18 +127,34 @@ bool KateViNormalMode::handleKeypress( const QKeyEvent *e )
   if ( !m_mappingKeyPress && !m_ignoreMapping) {
     m_mappingKeys.append( key );
 
-    foreach ( const QString &str, getMappings() ) {
-      if ( str.startsWith( m_mappingKeys ) ) {
-        if ( str == m_mappingKeys ) {
-          m_viInputModeManager->feedKeyPresses( getMapping( str ) );
-          m_mappingTimer->stop();
-          return true;
+    bool isPartialMapping = false;
+    bool isFullMapping = false;
+    m_fullMappingMatch.clear();
+    foreach ( const QString &mapping, getMappings() ) {
+      if ( mapping.startsWith( m_mappingKeys ) ) {
+        if ( mapping == m_mappingKeys ) {
+          isFullMapping = true;
+          m_fullMappingMatch = mapping;
         } else {
-          m_mappingTimer->start( m_timeoutlen );
-          m_mappingTimer->setSingleShot( true );
-          return true;
+          isPartialMapping = true;
         }
       }
+    }
+    if (isFullMapping && !isPartialMapping)
+    {
+      // Great - m_mappingKeys is a mapping, and one that can't be extended to
+      // a longer one - execute it immediately.
+      m_viInputModeManager->feedKeyPresses( getMapping( m_fullMappingMatch ) );
+      m_mappingTimer->stop();
+      return true;
+    }
+    if (isPartialMapping)
+    {
+      // Need to wait for more characters (or a timeout) before we decide what to
+      // do with this.
+      m_mappingTimer->start( m_timeoutlen );
+      m_mappingTimer->setSingleShot( true );
+      return true;
     }
     m_mappingKeys.clear();
   } else {

part/tests/vimode_test.cpp
@@ -26,8 +26,10 @@
 #include <katedocument.h>
 #include <kateview.h>
 #include "kateconfig.h"
+#include <kateglobal.h>
 #include "katebuffer.h"
 #include "katevikeyparser.h"
+#include <kateviglobal.h>
 #include "kateviewhelpers.h"
 
 QTEST_KDEMAIN(ViModeTest, GUI)
@@ -769,6 +771,30 @@ void ViModeTest::CommandModeTests() {
     // Quick test that "{" and "}" motions work in visual mode
     DoTest("foo\n\n\nbar\n","v}}d","");
     DoTest("\n\nfoo\nbar\n","jjjv{d","\nar\n");
+}
+
+void ViModeTest::MappingTests()
+{
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'", "<esc>ihello<esc>^aworld<esc>");
+  DoTest("", "'", "hworldello");
+
+  {
+    // Check that '123 is mapped after the timeout, given that we also have mappings that
+    // extend it (e.g. '1234, '12345, etc) and which it itself extends ('1, '12, etc).
+    KateGlobal::self()->viInputModeGlobal()->clearMappings(NormalMode);
+    BeginTest("");
+    QString consectiveDigits;
+    for (int i = 1; i < 9; i++)
+    {
+      consectiveDigits += QString::number(i);
+      KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'" + consectiveDigits, "iMapped from " + consectiveDigits + "<esc>");
+    }
+    TestPressKey("'123");
+    QCOMPARE(kate_document->text(), QString("")); // Shouldn't add anything until after the timeout!
+    const int mappingTimeoutMS = 1000; // Chosen to match m_timeoutlen in KateViNormalMode.
+    QTest::qWait(2 * 1000);
+    FinishTest("Mapped from 123");
+  }
 }
 
 // kate: space-indent on; indent-width 2; replace-tabs on;

part/tests/vimode_test.h
@@ -45,7 +45,7 @@ private Q_SLOTS:
   void VisualModeTests();
   void CommandModeTests();
 
-
+  void MappingTests();
 private:
   void BeginTest(const QString& original_text);
   void FinishTest(const QString& expected_text);

part/vimode/katevinormalmode.h
@@ -321,6 +321,13 @@ class KATEPART_TESTS_EXPORT KateViNormalMode : public KateViModeBase
 
     // mappings
     bool m_mappingKeyPress;
+    // Will be the mapping used if we decide that no extra mapping characters will be
+    // typed, either because we have a mapping that cannot be extended to another
+    // mapping by adding additional characters, or we have a mapping and timed out waiting
+    // for it to be extended to a longer mapping.
+    // (Essentially, this allows us to have mappings that extend each other e.g. "'12" and
+    // "'123", and to choose between them.)
+    QString m_fullMappingMatch;
     // set after f/F/t/T/r so the following character isn't translated
     bool m_ignoreMapping;
     QString m_mappingKeys;

commit b76e23d6c87b60e45014f2c5814ff4c6d83bbf9b

Adding the 'telescoping' mappings in 82202bde introduced a subtle bug which occurs when a mapping shares a prefix with a built-in command.  Fix, and add tests to prevent it from happening again!

part/vimode/katevinormalmode.cpp
@@ -155,6 +155,11 @@ bool KateViNormalMode::handleKeypress( const QKeyEvent *e )
       m_mappingTimer->setSingleShot( true );
       return true;
     }
+    // We've been swallowing all the keypresses meant for m_keys for our mapping keys; now that we know
+    // this cannot be a mapping, restore them. The current key will be appended further down.
+    Q_ASSERT(!isPartialMapping && !isFullMapping);
+    if (m_keys.isEmpty())
+      m_keys = m_mappingKeys.mid(0, m_mappingKeys.length() - 1);
     m_mappingKeys.clear();
   } else {
     // FIXME:
@@ -3289,6 +3294,7 @@ KateViRange KateViNormalMode::textObjectComma(bool inner)
 
 void KateViNormalMode::executeMapping()
 {
+  m_mappingKeys.clear();
   const int numberRepeats = m_countTemp == 0 ? 1 : m_countTemp;
   m_countTemp = 1; // Ensure that the first command in the mapping is not repeated.
   m_mappingTimer->stop();

part/tests/vimode_test.cpp
@@ -794,6 +794,10 @@ void ViModeTest::MappingTests()
   KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'", "<esc>ihello<esc>^aworld<esc>");
   DoTest("", "'", "hworldello");
 
+  // Ensure that the non-mapping logged keypresses are cleared before we execute a mapping
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'a", "rO");
+  DoTest("X", "'a", "O");
+
   {
     // Check that '123 is mapped after the timeout, given that we also have mappings that
     // extend it (e.g. '1234, '12345, etc) and which it itself extends ('1, '12, etc).
@@ -826,6 +830,37 @@ void ViModeTest::MappingTests()
   QTest::qWait(2 * mappingTimeoutMS);
   FinishTest("XXXX\nXOXX\nXXOX\nXXXO");
 
+  KateGlobal::self()->viInputModeGlobal()->clearMappings(NormalMode);
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'testmapping", "ljrO");
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'testmappingdummy", "dummy");
+  BeginTest("XXXX\nXXXX\nXXXX\nXXXX");
+  TestPressKey("3'testmapping");
+  QTest::qWait(2 * mappingTimeoutMS);
+  FinishTest("XXXX\nXOXX\nXXOX\nXXXO");
+
+  // Test that telescoping mappings don't interfere with built-in commands. Assumes that gp
+  // is implemented and working.
+  KateGlobal::self()->viInputModeGlobal()->clearMappings(NormalMode);
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "gdummy", "idummy");
+  DoTest("hello", "yiwgpx", "hhellollo");
+
+  // Test that we can map a sequence of keys that extends a built-in command and use
+  // that sequence without the built-in command firing.
+  // Once again, assumes that gp is implemented and working.
+  KateGlobal::self()->viInputModeGlobal()->clearMappings(NormalMode);
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "gpa", "idummy");
+  DoTest("hello", "yiwgpa", "dummyhello");
+
+  // Test that we can map a sequence of keys that extends a built-in command and still
+  // have the original built-in command fire if we timeout after entering that command.
+  // Once again, assumes that gp is implemented and working.
+  KateGlobal::self()->viInputModeGlobal()->clearMappings(NormalMode);
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "gpa", "idummy");
+  BeginTest("hello");
+  TestPressKey("yiwgp");
+  QTest::qWait(2 * mappingTimeoutMS);
+  TestPressKey("x");
+  FinishTest("hhellollo");
 }
 
 // kate: space-indent on; indent-width 2; replace-tabs on;
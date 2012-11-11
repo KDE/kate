a968d74fd74cfdec169e499e52f184f0b56fe073
Christoph Cullmann <cullmann@kde.org>
2012-11-11 13:51:15 +0100

    add again a snippets plugin that handles the snippets toolview BUG: 309720

--- kate/app/katemainwindow.cpp
+++ kate/app/katemainwindow.cpp
@@ -260,20 +260,6 @@ void KateMainWindow::setupMainWindow ()
   
   m_bottomViewBarContainer=new QWidget(centralWidget());
   m_bottomContainerStack = new KateContainerStackedLayout(m_bottomViewBarContainer);
-
-  // use snippets widget provided by editor component, if any
-  if (QWidget *snippets = KateDocManager::self()->editor()->property("snippetWidget").value<QWidget*>()) {
-    // Toolview for snippets
-    QWidget *toolView = mainWindow()->createToolView (0,"kate_private_snippets", Kate::MainWindow::Right, SmallIcon("document-new"), i18n("Snippets"));
-    
-    // snippets toolbar
-    KToolBar *topToolbar = new KToolBar (toolView, "snippetsToolBar");
-    topToolbar->setToolButtonStyle (Qt::ToolButtonIconOnly);
-    topToolbar->addActions (snippets->actions());
-
-    // add snippets widget
-    snippets->setParent (toolView);
-  }
 }
 
 void KateMainWindow::setupActions()

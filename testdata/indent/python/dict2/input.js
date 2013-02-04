cdc845febdac578b2635571fed287bafd13c4647
Gerald Senarclens de Grancy <oss@senarclens.eu>
2013-02-04 10:14:11 +0100

    created additional tests for Python indentation
    CCBUG: 312309

---
+++ testdata/indent/python/dict2/input.js
@@ -0,0 +1,5 @@
+v.setCursorPosition(0, document.lineLength(0));
+v.enter();
+v.type("1: 'b', }");
+v.enter();
+v.type("pass");
---
+++ testdata/indent/python/tuple1/input.js
@@ -0,0 +1,7 @@
+v.setCursorPosition(0, document.lineLength(0));
+v.enter();
+v.type("1,");
+v.enter();
+v.type("2)");
+v.enter();
+v.type("pass");

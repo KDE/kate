e6b9c40f083b7f2e52ab15541a9af2993f409985
Gerald Senarclens de Grancy <oss@senarclens.eu>
2013-02-04 10:14:11 +0100

    created additional tests for Python indentation
    CCBUG: 312309

---
+++ testdata/indent/python/tuple2/input.js
@@ -0,0 +1,7 @@
+v.setCursorPosition(0, document.lineLength(0));
+v.enter();
+v.type("2,");
+v.enter();
+v.type("3)");
+v.enter();
+v.type("pass");
---
+++ testdata/indent/python/wrapped_line/input.js
@@ -0,0 +1,5 @@
+v.setCursorPosition(0, document.lineLength(0));
+v.enter();
+v.type('"b"');
+v.enter();
+v.type("pass");

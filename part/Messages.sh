4612e635cb740127eaaaf4aeae411f031a99f554
Pino Toscano <pino@kde.org>
2013-02-17 11:34:26 +0100

    i18n: do not extract .rc and .ui files from plugins
    (cherry picked from commit 416b8533f266628d3b9ca8556449b26b99856fb4)

--- part/Messages.sh
+++ part/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui | egrep -v "/tests" | egrep -v "/plugins"` >> rc.cpp || exit 11
 $EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" syntax/data/*.xml >> rc.cpp || exit 12
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o -name '*.js' | egrep -v "/tests" | egrep -v "/plugins" ` -o $podir/katepart4.pot

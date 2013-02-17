3d7405f9083158ae2e9a56e7d44e2267dc36e8dc
Kåre Särs <kare.sars@iki.fi>
2013-02-17 22:35:07 +0200

    Search&Replace: Fix removing scroll-marks if another mark exists on the line
    BUG: 315248
    FIXED-IN: 4.10.1

--- kate/plugins/search/plugin_search.cpp
+++ kate/plugins/search/plugin_search.cpp
@@ -599,8 +599,8 @@ void KatePluginSearchView::clearMarks()
             QHashIterator<int, KTextEditor::Mark*> i(marks);
             while (i.hasNext()) {
                 i.next();
-                if (i.value()->type == KTextEditor::MarkInterface::markType32) {
-                    iface->removeMark(i.value()->line, i.value()->type);
+                if (i.value()->type & KTextEditor::MarkInterface::markType32) {
+                    iface->removeMark(i.value()->line, KTextEditor::MarkInterface::markType32);
                 }
             }
         }
@@ -620,8 +620,8 @@ void KatePluginSearchView::clearDocMarks(KTextEditor::Document* doc)
         QHashIterator<int, KTextEditor::Mark*> i(marks);
         while (i.hasNext()) {
             i.next();
-            if (i.value()->type == KTextEditor::MarkInterface::markType32) {
-                iface->removeMark(i.value()->line, i.value()->type);
+            if (i.value()->type & KTextEditor::MarkInterface::markType32) {
+                iface->removeMark(i.value()->line, KTextEditor::MarkInterface::markType32);
             }
         }
     }

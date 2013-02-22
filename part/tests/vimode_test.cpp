f316dfc58c28ee061200d7018cbe0491ed0ab57c
Simon St James <kdedevel@etotheipiplusone.com>
2013-02-22 16:49:51 +0000

    Add analogous test for P instead of p (already passes)

--- part/tests/vimode_test.cpp
+++ part/tests/vimode_test.cpp
@@ -677,6 +677,7 @@ void ViModeTest::NormalModeCommandsTest() {
   DoTest("fop\ngid\nbar", "lvjyjjgpx", "fop\ngid\nbaop\ngi");
   DoTest("fop\ngid\nbar", "lvjyjjgPx", "fop\ngid\nbop\ngir");
   DoTest("fop\ngid\nbar", "lvjyjjpx", "fop\ngid\nbap\ngir");
+  DoTest("fop\ngid\nbar", "lvjyjjPx", "fop\ngid\nbp\ngiar");
   // Linewise
   DoTest("fop\ngid\nbar\nhuv", "yjjjgpx", "fop\ngid\nbar\nfop\ngid\nuv");
   DoTest("fop\ngid\nbar\nhuv", "yjjjgPx", "fop\ngid\nfop\ngid\nar\nhuv");

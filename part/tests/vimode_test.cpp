9da7e459887f3b18af4f4337f134d270fda0e7ed
Simon St James <kdedevel@etotheipiplusone.com>
2013-03-01 17:26:12 +0000

    After checking with Vim, I see I've somehow managed to misinterpret how counted repeats of last change are supposed to work.  Fix test, and mark is as expected to fail.

--- part/tests/vimode_test.cpp
+++ part/tests/vimode_test.cpp
@@ -723,7 +723,7 @@ void ViModeTest::NormalModeControlTests() {
   DoTest("-1", "l1\\ctrl-a", "0");
   DoTest("0x0000f", "\\ctrl-a","0x00010" );
   DoTest("5", "5\\ctrl-a.","15" );
-  DoTest("5", "5\\ctrl-a2.","20" );
+  DoTest("5", "5\\ctrl-a2.","12", ShouldFail, "Gah - messed up how counted repeats of last change are supposed to work");
 
   // Testing "Ctrl-r"
   DoTest("foobar", "d3lu\\ctrl-r", "bar");

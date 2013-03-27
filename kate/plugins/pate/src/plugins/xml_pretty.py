aa9f1edbbe86c268e15a84d9220f974398ae2ae2
Pablo Martín <goinnn@gmail.com>
2013-03-27 13:45:19 +0100

    Add config to xml_pretty plugin

--- kate/plugins/pate/src/plugins/xml_pretty.py
+++ kate/plugins/pate/src/plugins/xml_pretty.py
@@ -19,19 +19,31 @@
 # <https://github.com/goinnn/Kate-plugins/blob/master/kate_plugins/xhtml_plugins/xml_plugins.py>
 
 import kate
+import os
 import re
 
-from xml.dom import minidom
-from xml.parsers.expat import ExpatError
+from PyQt4 import uic
+from PyQt4.QtGui import QWidget
 
 from libkatepate import text
 from libkatepate.errors import showError
+
+from xml.dom import minidom
+from xml.parsers.expat import ExpatError
 
 
 KATE_ACTIONS = {'togglePrettyXMLFormat': {'text': 'Pretty XML',
                                           'shortcut': 'Ctrl+Alt+X',
                                           'menu': 'XML', 'icon': None},
-}
+                }
+
+KATE_CONFIG = {'name': 'xml_pretty',
+               'fullName': 'XML Pretty',
+               'icon': 'text-xml'}
+
+_CONFIG_UI = 'xml_pretty.ui'
+_INDENT_CFG = 'XMLPretty:indent'
+_NEWL_CFG = 'XMLPretty:newl'
 
 encoding_pattern = re.compile("<\?xml[\w =\"\.]*encoding=[\"']([\w-]+)[\"'][\w =\"\.]*\?>")
 
@@ -43,7 +55,7 @@ def togglePrettyXMLFormat():
     view = currentDocument.activeView()
     source = view.selectionText()
     if not source:
-        showError('Select a xml text')
+        showError('Please select a xml text and press: %s' % KATE_ACTIONS['togglePrettyXMLFormat']['shortcut'])
     else:
         try:
             encoding = 'utf-8'
@@ -52,8 +64,64 @@ def togglePrettyXMLFormat():
                 encoding = m.groups()[0]
             target = minidom.parseString(source)
             view.removeSelectionText()
-            xml_pretty = target.toprettyxml(encoding=encoding).decode(encoding)
-            xml_pretty = '\n'.join([line for line in xml_pretty.split("\n") if line.replace(' ', '').replace('\t', '')])
+            indent = kate.configuration[_INDENT_CFG].decode("string_escape")
+            newl = kate.configuration[_NEWL_CFG].decode("string_escape")
+            xml_pretty = target.toprettyxml(indent=indent, newl=newl, encoding=encoding).decode(encoding)
+            xml_pretty = newl.join([line for line in xml_pretty.split(newl) if line.replace(' ', '').replace(indent, '')])
             text.insertText(xml_pretty)
         except (ExpatError, LookupError) as e:
-            showError('This text is not a valid xml text: %s' % e.message)
+            showError('The selected text is not a valid xml text: %s' % e.message)
+
+
+class ConfigWidget(QWidget):
+    """Configuration widget for this plugin."""
+
+    def __init__(self, parent=None, name=None):
+        super(ConfigWidget, self).__init__(parent)
+
+        # Set up the user interface from Designer.
+        uic.loadUi(os.path.join(os.path.dirname(__file__), _CONFIG_UI), self)
+        self.reset()
+
+    def apply(self):
+        kate.configuration[_NEWL_CFG] = self.newl.text()
+        kate.configuration[_INDENT_CFG] = self.indent.text()
+        kate.configuration.save()
+
+    def reset(self):
+        self.defaults()
+        if _INDENT_CFG in kate.configuration:
+            self.indent.setText(kate.configuration[_INDENT_CFG])
+        if _NEWL_CFG in kate.configuration:
+            self.newl.setText(kate.configuration[_NEWL_CFG])
+
+    def defaults(self):
+        self.indent.setText('\\t')
+        self.newl.setText('\\n')
+
+
+class ConfigPage(kate.Kate.PluginConfigPage, QWidget):
+    """Kate configuration page for this plugin."""
+    def __init__(self, parent=None, name=None):
+        super(ConfigPage, self).__init__(parent, name)
+        self.widget = ConfigWidget(parent)
+        lo = parent.layout()
+        lo.addWidget(self.widget)
+
+    def apply(self):
+        self.widget.apply()
+
+    def reset(self):
+        self.widget.reset()
+
+    def defaults(self):
+        self.widget.defaults()
+        self.changed.emit()
+
+
+@kate.configPage(**KATE_CONFIG)
+def configPage(parent=None, name=None):
+    return ConfigPage(parent, name)
+
+
+# kate: space-indent on; indent-width 4;

b525ed309e87a6b32d2982649a79662b0f6db6e1
Dominik Haumann <dhaumann@kde.org>
2013-04-01 23:55:14 +0200

    fix search/replace highlight+notification
    manually removing a message called clearHighlights twice

--- part/search/katesearchbar.cpp
+++ part/search/katesearchbar.cpp
@@ -1528,8 +1528,11 @@ void KateSearchBar::enterIncrementalMode() {
 
 bool KateSearchBar::clearHighlights()
 {
-    if (m_infoMessage)
+    if (m_infoMessage) {
+        // highlightings already cleared -> remove clear connection
+        disconnect(m_infoMessage, 0, this, 0);
         delete m_infoMessage;
+    }
 
     if (m_hlRanges.isEmpty()) {
         return false;

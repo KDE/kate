ac2092902ed62d6584904924c7570ad3baae5a74
Christoph Cullmann <cullmann@kde.org>
2013-04-14 19:33:26 +0200

    try to fix templates text i18n extraction

--- kate/Messages.sh
+++ kate/Messages.sh
@@ -11,23 +11,6 @@ for dir in $msh; do
 done
 dirs=`cat dirs`
 
-for fname in `find -iname '*.katetemplate'`; do
-  for compound in template@item:inmenu \
-                  group@item:inmenu \
-                  description@info:whatsthis \
-                  author@info:credit \
-               ; do
-    field=`echo $compound | sed 's/\(.*\)@.*/\1/'`
-    ctxmark=`echo $compound | sed 's/.*\(@.*\)/\1/'`
-      grep -inH "^katetemplate:.*$field=" $fname \
-    | sed "s/\\([^:]*\\):\\([^:]*\\):.*$field= *\\([^=]*\\)\\( .*\\)\\?\$/\
-           \/\/i18n: file \1 line \2\n\
-           i18nc(\"$ctxmark\", \"\3\");\
-           /i" \
-    >> rc.cpp
-  done
-done
-
 echo "rc.cpp" > files
 find $dirs -maxdepth 1 -name "*.cpp" -print >> files
 find $dirs -maxdepth 1 -name "*.cc" -print >> files

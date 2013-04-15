215ceb9765e3db6be43799fb3c15f54fe218f1be
Pino Toscano <pino@kde.org>
2013-04-15 09:50:23 +0200

    remove the autogenerated tips.cpp when done

--- kate/Messages.sh
+++ kate/Messages.sh
@@ -2,3 +2,4 @@
 (cd data && $PREPARETIPS > ../tips.cpp)
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o -name '*.js'` -o $podir/kate.pot
+rm -f tips.cpp

103ef498db27fd38ed8a145be197be7ba584f649
Dominik Haumann <dhaumann@kde.org>
2013-04-17 13:12:34 +0200

    unbreak PHP highlighting
    The php files are auto-generated and there seems to be some magic
    about IncludeRules here. Modelines needs to be excluded here.
    
    This issue appeared two weeks ago and is in no public release.
    
    You may need to remove build/part/syntax/data/*.xml in order to
    regenerate the xml files. Otherwise, the old ones and the bug
    just persists.
    
    CCMAIL: i.zaufi@gmail.com
    BUG: 318465

--- part/syntax/data/generate-php.pl
+++ part/syntax/data/generate-php.pl
@@ -50,7 +50,7 @@ else
 
 $findphp = "<context name=\"FindPHP\">\n<RegExpr context=\"##PHP/PHP\" String=\"&lt;\\?(?:=|php)?\" lookAhead=\"true\" />\n</context>\n";
 
-$file =~ s/<IncludeRules\s([^>]*)context="##(?!Alerts|Doxygen)([^"]+)"/<IncludeRules $1context="##$2\/PHP"/g;
+$file =~ s/<IncludeRules\s([^>]*)context="##(?!Alerts|Doxygen|Modelines)([^"]+)"/<IncludeRules $1context="##$2\/PHP"/g;
 $file =~ s/(<context\s[^>]*>)/$1\n<IncludeRules context="FindPHP" \/>/g;
 $file =~ s/(?=<\/contexts\s*>)/$findphp/;
 

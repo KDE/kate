5a040575fb781e336b2bdc89ef55bb1b6a2132c6
flying sheep <flying-sheep@web.de>
2013-05-03 20:14:24 +0200

    fixed ipython console for python3

--- addons/kate/pate/src/plugins/python_console_ipython/python_console_ipython.py
+++ addons/kate/pate/src/plugins/python_console_ipython/python_console_ipython.py
@@ -8,24 +8,35 @@ May need reworking with IPython 1.0 due to changed event loop
 
 # TODO: fix help() by fixing input()
 
+from __future__ import unicode_literals
+
 import sys
 
-from PyKDE4.kdecore import i18n
+from PyKDE4.kdecore import i18n as _translate
 from libkatepate.errors import needs_packages
 
 sys.argv = [__file__]
 
-NEED_PACKAGES = {"IPython": "ipython==0.13.1",
-                 "readline": "6.2.4.1",
-                 "pygments": "Pygments==1.6"}
+NEED_PACKAGES = {
+    'IPython': 'ipython==0.13.1',
+    'readline': '6.2.4.1',
+    'pygments': 'Pygments==1.6',
+}
 
 
 if sys.version_info.major == 3:
-    NEED_PACKAGES["zmq"] = "pyzmq==13.0.0"
+    NEED_PACKAGES['zmq'] = 'pyzmq==13.0.0'
+    _u = str
 else:
-    NEED_PACKAGES["zmq"] = "pyzmq==2.0.10.1"
+    NEED_PACKAGES['zmq'] = 'pyzmq==2.0.10.1'
+    _u = unicode
 
 needs_packages(NEED_PACKAGES)
+
+def i18n(msg, *args):
+    if isinstance(msg, _u):
+        msg = msg.encode('utf-8')
+    return _translate(msg, *args) if args else _translate(msg)
 
 import atexit
 import kate
@@ -39,17 +50,17 @@ from IPython.frontend.qt.console.rich_ipython_widget import RichIPythonWidget
 from PyQt4 import uic
 from PyQt4.QtGui import QWidget
 
-from libkatepate.project_utils import (get_project_plugin,
-                                       is_version_compatible,
-                                       add_extra_path,
-                                       add_environs)
+from libkatepate.project_utils import (
+    get_project_plugin,
+    is_version_compatible,
+    add_extra_path,
+    add_environs)
 
 _SCROLLBACK_LINES_COUNT_CFG = 'ipythonConsole:scrollbackLinesCount'
 _GUI_COMPLETION_TYPE_CFG = 'ipythonConsole:guiCompletionType'
 _CONFIG_UI = 'python_console_ipython.ui'
 _CONSOLE_CSS = 'python_console_ipython.css'
-_GUI_COMPLETION_CONVERT = {0: "droplist",
-                           1: None}
+_GUI_COMPLETION_CONVERT = ['droplist', None]
 
 
 def event_loop(kernel):
@@ -75,6 +86,10 @@ def default_manager(kernel_app):
     return manager
 
 
+def print_to_shell(kernel_app, msg):
+    kernel_app.shell.run_cell('print("{}")'.format(msg))
+
+
 def django_project_filename_changed(kernel_app):
     # Uses this: https://github.com/django-extensions/django-extensions/blob/master/django_extensions/management/shells.py#L7
     from django.db.models.loading import get_models, get_apps
@@ -85,7 +100,7 @@ def django_project_filename_changed(kernel_app):
 
     dont_load = getattr(settings, 'SHELL_PLUS_DONT_LOAD', [])
     model_aliases = getattr(settings, 'SHELL_PLUS_MODEL_ALIASES', {})
-    print_imports = ""
+    imports = []
     for app_mod in get_apps():
         app_models = get_models(app_mod)
         if not app_models:
@@ -96,55 +111,52 @@ def django_project_filename_changed(kernel_app):
         app_aliases = model_aliases.get(app_name, {})
         model_labels = []
         for model in app_models:
+            model_name = model.__name__
             try:
-                imported_object = getattr(__import__(app_mod.__name__, {}, {}, model.__name__), model.__name__)
-                model_name = model.__name__
-                if "%s.%s" % (app_name, model_name) in dont_load:
+                imported_object = getattr(__import__(app_mod.__name__, {}, {}, model_name), model_name)
+                if '{}.{}'.format(app_name, model_name) in dont_load:
                     continue
                 alias = app_aliases.get(model_name, model_name)
                 imported_objects[alias] = imported_object
                 if model_name == alias:
                     model_labels.append(model_name)
                 else:
-                    model_labels.append("%s (as %s)" % (model_name, alias))
-            except AttributeError as e:
-                msg = i18n("Failed to import \'%(model)s\' from \'%(app)s\' reason: %(reason)s") % {'model': model.__name__,
-                                                                                                    'app': app_name,
-                                                                                                    'reason': str(e)}
-                kernel_app.shell.run_cell('print("%s")' % msg)
+                    model_labels.append('{} (as {})'.format(model_name, alias))
+            except AttributeError as reason:
+                msg = i18n('Failed to import “%1” from “%2” reason: %3', model_name, app_name, reason)
+                print_to_shell(kernel_app, msg)
                 continue
-        msg = i18n("From \'%(app)s\' autoload: %(models)s") % {'app': app_mod.__name__.split('.')[-2],
-                                                               'models': ", ".join(model_labels)}
-        print_imports += 'print("%s");' % msg
-    kernel_app.shell.run_cell(print_imports)
+        msg = i18n('From “%1” autoload: %2', app_mod.__name__.split('.')[-2], ', '.join(model_labels))
+        imports.append(msg)
+    for import_msg in imports:
+        print_to_shell(kernel_app, msg)
     return imported_objects
 
 
 def projectFileNameChanged(*args, **kwargs):
     projectPlugin = get_project_plugin()
-    projectMap = projectPlugin.property("projectMap")
-    if "python" in projectMap:
-        projectName = projectPlugin.property("projectName")
-        projectMapPython = projectMap["python"]
-        version = projectMapPython.get("version", None)
+    projectMap = projectPlugin.property('projectMap')
+    if 'python' in projectMap:
+        projectName = projectPlugin.property('projectName')
+        projectMapPython = projectMap['python']
+        version = projectMapPython.get('version', None)
         kernel_app = default_kernel_app()
         # Check Python version
         if not is_version_compatible(version):
-            msg = i18n("Can not load this project: %s. Python Version incompatible") % projectName
-            kernel_app.shell.run_cell('print("%s")' % msg)
+            msg = i18n('Can not load this project: %1. Python Version incompatible', projectName)
+            print_to_shell(kernel_app, msg)
             sys.stdout.flush()
             return
         kernel_app.shell.reset()
-        msg = i18n("Load project: %s") % projectName
-        kernel_app.shell.run_cell('print("%s")' % msg)
-        extraPath = projectMapPython.get("extraPath", [])
-        environs = projectMapPython.get("environs", {})
+        print_to_shell(kernel_app, i18n('Load project: %1', projectName))
+        extraPath = projectMapPython.get('extraPath', [])
+        environs = projectMapPython.get('environs', {})
         # Add Extra path
         add_extra_path(extraPath)
         # Add environs
         add_environs(environs)
         # Special treatment
-        if projectMapPython.get("projectType", "").lower() == "django":
+        if projectMapPython.get('projectType', '').lower() == 'django':
             kernel_app = default_kernel_app()
             imported_objects = django_project_filename_changed(kernel_app)
             kernel_app.shell.user_ns.update(imported_objects)
@@ -169,13 +181,16 @@ def terminal_widget(parent=None, **kwargs):
     #update namespace
     kernel_app.shell.user_ns.update(kwargs)
     kernel_app.shell.user_ns['console'] = widget
-    msg = i18n("\\nAvailable variables are everything from pylab, “{}”, and this console as “console”").format('”, “'.join(kwargs.keys()))
-    kernel_app.shell.run_cell('print("%s")' % msg)
+    kernel_app.start()
+
+    msg = i18n('\\nAvailable variables are everything from pylab, “%1”, and this console as “console”', '”, “'.join(kwargs.keys()))
+    print_to_shell(kernel_app, msg)
+
     projectPlugin = get_project_plugin()
     if projectPlugin:
         projectPlugin.projectFileNameChanged.connect(projectFileNameChanged)
         projectFileNameChanged()
-    kernel_app.start()
+
     sys.stdout.flush()
     return widget
 
@@ -236,7 +251,7 @@ class ConfigPage(kate.Kate.PluginConfigPage, QWidget):
         self.changed.emit()
 
 
-@kate.configPage("IPython Console", "IPython Console Settings", icon="text-x-python")
+@kate.configPage('IPython Console', 'IPython Console Settings', icon='text-x-python')
 def configPage(parent=None, name=None):
     return ConfigPage(parent, name)
 

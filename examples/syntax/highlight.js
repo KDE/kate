ccc33be3c30a5d0ca53c1709eaf04b030c120aa8
Milian Wolff <mail@milianw.de>
2013-07-18 16:34:44 +0200

    Add an example higihlight.js file

---
+++ examples/syntax/highlight.js
@@ -0,0 +1,20 @@
+function foo() {
+  
+}
+
+// objects
+var foo = bar.asdf;
+var foo = bar.asdf.bla;
+bar.func(adsf.bar);
+var foo = asdf. asdf . asdf ;
+
+// no regular expression
+var foo = foo.i / 0;
+var bar = bar / 1;
+var foo = bar / 1; bar / 1;
+
+// regular expressions
+foo = /asdf/i;
+foo = /asdf/ig;
+foo = /asdf/gi;
+foo = /asdf\/asdf/;

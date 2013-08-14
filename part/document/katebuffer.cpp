e5a203ad709830dc1111ee53888ad397e5ed1825
Christoph Cullmann <cullmann@kde.org>
2013-08-14 19:20:08 +0200

    fix problem with folding regions like used in objective C:
    if you don't add
    
    in front of each line, the pragma will fold the next pragma, too
    
    this is not wanted, actually.
    
    the right fix for this is: if we have a end folding at column 0,
    just move it to last column of the line in front of it
    
    this fixes the issue for which a review request was issued, too
    
    REVIEW: 111628

--- part/document/katebuffer.cpp
+++ part/document/katebuffer.cpp
@@ -703,10 +703,23 @@ KTextEditor::Range KateBuffer::computeFoldingRangeForStartLine (int startLine)
         
         /**
          * end reached?
-         * return resulting range!
+         * compute resulting range!
          */
-        if (countOfOpenRegions == 0)
-          return KTextEditor::Range (KTextEditor::Cursor (startLine, openedRegionOffset), KTextEditor::Cursor (line, lineAttributes[i].offset));
+        if (countOfOpenRegions == 0) {
+          /**
+           * special handling of end: if end is at column 0 of a line, move it to end of previous line!
+           * fixes folding for stuff like
+           * #pragma mark END_OLD_AND_START_NEW_REGION
+           */
+          KTextEditor::Cursor endCursor (line, lineAttributes[i].offset);
+          if (endCursor.column() == 0 && endCursor.line() > 0)
+            endCursor = KTextEditor::Cursor (endCursor.line()-1, plainLine (lines()-1)->length());
+          
+          /**
+           * return computed range
+           */
+          return KTextEditor::Range (KTextEditor::Cursor (startLine, openedRegionOffset), endCursor);
+        }
       }
     
       /**

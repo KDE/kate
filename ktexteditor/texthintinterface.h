9ffeceeaa65482ec0e7afd9b6c0cc3dd773e3bf8
Dominik Haumann <dhaumann@kde.org>
2013-08-15 12:00:11 +0200

    TextHintInterface: add API documentation

--- ktexteditor/texthintinterface.h
+++ ktexteditor/texthintinterface.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE libraries
    Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
+   Copyright (C) 2013 Dominik Haumann <dhaumann@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -20,7 +21,6 @@
 #define KDELIBS_KTEXTEDITOR_TEXTHINTINTERFACE_H
 
 #include <QtCore/QString>
-#include <QtCore/QStringList>
 
 #include <ktexteditor/ktexteditor_export.h>
 
@@ -30,8 +30,50 @@ namespace KTextEditor
 {
 
 /**
- * This is an interface for the KTextEditor::View class.
+ * \brief Text hint interface showing tool tips under the mouse for the View.
+ *
  * \ingroup kte_group_view_extensions
+ *
+ * \section texthint_introduction Introduction
+ *
+ * The text hint interface provides a way to show tool tips for text located
+ * under the mouse. Possible applications include showing a value of a variable
+ * when debugging an application, or showing a complete path of an include
+ * directive.
+ *
+ * By default, the text hint interface is disable for the View. To enable it,
+ * call enableTextHints() with the desired timeout. The timeout specifies the
+ * delay the user needs to hover over the text before the tool tip is shown.
+ * Therefore, the timeout should not be too large, a value of 200 milliseconds
+ * is recommended.
+ *
+ * Once text hints are enabled, the signal needTextHint() is emitted after the
+ * timeout whenever the mouse moved to a new text position in the View.
+ * Therefore, in order to show a tool tip, you need to connect to this signal
+ * and then fill the parameter \p text with the text to display.
+ *
+ * To disable all text hints, call disableTextHints(). This, however will disable
+ * the text hints entirely for the View. If there are multiple users of the
+ * TextHintInterface, this might lead to a conflict.
+ *
+ * \section texthint_access Accessing the TextHintInterface
+ *
+ * The TextHintInterface is an extension interface for a
+ * View, i.e. the View inherits the interface \e provided that the
+ * used KTextEditor library implements the interface. Use qobject_cast to
+ * access the interface:
+ * \code
+ * // view is of type KTextEditor::View*
+ * KTextEditor::TextHintInterface *iface =
+ *     qobject_cast<KTextEditor::TextHintInterface*>( view );
+ *
+ * if( iface ) {
+ *     // the implementation supports the interface
+ *     // do stuff
+ * }
+ * \endcode
+ *
+ * \since KDE 4.11
  */
 class KTEXTEDITOR_EXPORT TextHintInterface
 {
@@ -40,25 +82,45 @@ class KTEXTEDITOR_EXPORT TextHintInterface
     virtual ~TextHintInterface();
 
     /**
-     * enable Texthints. If they are enabled a signal needTextHint is emitted, if the mouse
-     * changed the position and a new character is beneath the mouse cursor. The signal is delayed
-     * for a certain time, specifiedin the timeout parameter.
+     * Enable text hints with the specified \p timeout in milliseconds.
+     * The timeout specifies the delay the user needs to hover over the text
+     * befure the tool tip is shown. Therefore, \p timeout should not be
+     * too large, a value of 200 milliseconds is recommended.
+     *     
+     * After enabling the text hints, the signal needTextHint() is emitted
+     * whenever the mouse position changed and a new character is underneath
+     * the mouse cursor. Calling the signal is delayed for the time specified
+     * by \p timeout.
+     *
+     * \param timeout tool tip delay in milliseconds
+     *
+     * \todo KDE5 add default value for timeout
      */
-    virtual void enableTextHints(int timeout)=0;
+    virtual void enableTextHints(int timeout) = 0;
 
     /**
-     * Disable texthints. Per default they are disabled.
+     * Disable all text hints for the view.
+     * By default, text hints are disabled.
      */
-    virtual void disableTextHints()=0;
+    virtual void disableTextHints() = 0;
 
-    //signals
-
+  //
+  // signals
+  //
+  public:
     /**
-     * emit this signal, if a tooltip text is needed for displaying.
-     * I you don't want a tooltip to be displayd set text to an emtpy string in a connected slot,
-     * otherwise set text to the string you want the editor to display
+     * This signal is emitted whenever the timeout for displaying a text hint
+     * is triggered. The text cursor \p position specifies the mouse position
+     * in the text. To show a text hint, fill \p text with the text to be
+     * displayed. If you do not want a tool tip to be displayed, set \p text to
+     * an empty QString() in the connected slot.
+     *
+     * \param position text cursor under the mouse position
+     * \param text tool tip to be displayed, or empty string to hide
+     *
+     * \todo KDE5: add first parameter "KTextEditor::View * view"
      */
-	virtual void needTextHint(const KTextEditor::Cursor& position, QString &text)=0;
+    virtual void needTextHint(const KTextEditor::Cursor& position, QString &text) = 0;
 
   private:
     class TextHintInterfacePrivate* const d;

7f36397b497db50ed6837aff89abb36ce7928889
Alex Turbov <i.zaufi@gmail.com>
2013-10-28 23:47:46 +0400

    add "()" after catch keyword also

--- part/script/data/indentation/cppstyle.js
+++ part/script/data/indentation/cppstyle.js
@@ -1530,6 +1530,7 @@ function tryKeywordsWithBrackets(cursor)
       || text == "while "
       || text == "for "
       || text == "switch "
+      || text == "catch "
       ;
     if (need_brackets)
     {

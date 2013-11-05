80c15965931375a7bab66918efbf1bb0fa8d881a
Alex Turbov <i.zaufi@gmail.com>
2013-11-05 04:48:58 +0400

    b ready 4 qt5

--- part/syntax/data/get-Qt-classes.sh
+++ part/syntax/data/get-Qt-classes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012 by Alex Turbov
+# Copyright (c) 2012-2013 by Alex Turbov
 #
 # Grab a documented (officially) class list from Qt project web site:
 # http://qt-project.org/doc/qt-${version}/classes.html
@@ -9,11 +9,23 @@
 version=$1
 shift
 
+case "$version" in
+5*)
+    url="http://qt-project.org/doc/qt-${version}/qtdoc/classes.html"
+    ;;
+4*)
+    url="http://qt-project.org/doc/qt-${version}/classes.html"
+    ;;
+*)
+    echo "*** Error: Only Qt4 and Qt5 supported!"
+esac
+
 if [ -n "$version" ]; then
   tmp=`mktemp`
-  wget -O $tmp http://qt-project.org/doc/qt-${version}/classes.html
+  wget -O $tmp "$url"
   cat $tmp | egrep '^<dd><a href=".*\.html">.*</a></dd>$' \
-    | sed -e 's,<dd><a href=".*\.html">\(.*\)</a></dd>,<item> \1 </item>,'
+    | sed -e 's,<dd><a href=".*\.html">\(.*\)</a></dd>,<item> \1 </item>,' \
+    | grep -v 'qoutputrange'
   rm $tmp
 else
   cat <<EOF

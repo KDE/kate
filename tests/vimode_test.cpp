KDE/kate

commit 1dd5ed84c1b401a60e0669f48807de258b695802

Fix for an issue with recursive mappings where the executed mapping looks like it might have been part of another mapping, but turns out not to have been: we need to replay *all* the keys we'd been saving.
This actually makes the code infinitescimally more comprehensible, actually :)

tests/vimode_test.cpp
@@ -1910,6 +1910,14 @@ void ViModeTest::MappingTests()
     DoTest("", "\\:nn h<space> i<space>a<space>b<esc>\\h ", " a b");
   }
 
+  // More recursion tests - don't lose characters from a Recursive mapping if it looks like they might
+  // be part of a different mapping (but end up not being so).
+  // (Here, the leading "i" in "irecursive<c-c>" could be part of the mapping "ihello<c-c>").
+  clearAllMappings();
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "'", "ihello<c-c>", KateViGlobal::Recursive);
+  KateGlobal::self()->viInputModeGlobal()->addMapping(NormalMode, "ihello<c-c>", "irecursive<c-c>", KateViGlobal::Recursive);
+  DoTest("", "'", "recursive");
+
   // Clear mappings for subsequent tests.
   clearAllMappings();
 }

part/vimode/katevikeymapper.cpp
@@ -107,13 +107,14 @@ bool KateViKeyMapper::handleKeypress(QChar key)
       return true;
     }
     // We've been swallowing all the keypresses meant for m_keys for our mapping keys; now that we know
-    // this cannot be a mapping, restore them. The current key will be appended further down.
+    // this cannot be a mapping, restore them.
     Q_ASSERT(!isPartialMapping && !isFullMapping);
      //currentKeys += m_mappingKeys.mid(0, m_mappingKeys.length() - 1);
     m_doNotExpandFurtherMappings = true;
-    m_viInputModeManager->feedKeyPresses(m_mappingKeys.mid(0, m_mappingKeys.length() - 1));
+    m_viInputModeManager->feedKeyPresses(m_mappingKeys.mid(0, m_mappingKeys.length()));
     m_doNotExpandFurtherMappings = false;
     m_mappingKeys.clear();
+    return true;
   } else {
     // FIXME:
     //m_mappingKeyPress = false; // key press ignored wrt mappings, re-set m_mappingKeyPress

commit dd259d3ce452b0b4a3fe25e5b354d16eec9862f6

Fix Alt Gr regression that was introduced in 1dd5ed84c1b401a60e0669f48807de258b695802 (results of Alt Gr+keypress won't survive encoding/ decoding of keypress, so make sure they don't go through the encode/ decode - prior to 1dd5ed84c1b401a60e0669f48807de258b695802 this was already the case, but 1dd5ed forced all keypresses to go through the keymapping system which implicitly encodes/decodes them).
Thanks to Micheal W for reporting and testing!

BUG:325794

part/vimode/katevikeyparser.cpp
@@ -788,7 +788,7 @@ const QChar KateViKeyParser::KeyEventToQChar(const QKeyEvent& keyEvent)
   }
 
   if ( text.isEmpty() || ( text.length() == 1 && text.at(0) < 0x20 )
-      || ( mods != Qt::NoModifier && mods != Qt::ShiftModifier && mods != Qt::KeypadModifier ) ) {
+      || ( mods != Qt::NoModifier && mods != Qt::ShiftModifier && mods != Qt::KeypadModifier && mods != Qt::GroupSwitchModifier) ) {
     QString keyPress;
 
     keyPress.append( '<' );

tests/vimode_test.cpp
@@ -819,6 +819,25 @@ void ViModeTest::InsertModeTests() {
 
   // Test that our test driver can handle newlines during insert mode :)
   DoTest("", "ia\\returnb", "a\nb");
+
+  // Test Alt-gr still works - this isn't quite how things work in "real-life": in real-life, something like
+  // Alt-gr+7 would be a "{", but I don't think this can be reproduced without sending raw X11
+  // keypresses to Qt, so just duplicate the keypress events we would receive if we pressed
+  // Alt-gr+7 (that is: Alt-gr down; "{"; Alt-gr up).
+  BeginTest("");
+  TestPressKey("i");
+  QKeyEvent *altGrDown = new QKeyEvent(QEvent::KeyPress, Qt::Key_AltGr, Qt::NoModifier);
+  QApplication::postEvent(kate_view->focusProxy(), altGrDown);
+  QApplication::sendPostedEvents();
+  // Not really Alt-gr and 7, but this is the key event that is reported by Qt if we press that.
+  QKeyEvent *altGrAnd7 = new QKeyEvent(QEvent::KeyPress, Qt::Key_BraceLeft, Qt::GroupSwitchModifier, "{" );
+  QApplication::postEvent(kate_view->focusProxy(), altGrAnd7);
+  QApplication::sendPostedEvents();
+  QKeyEvent *altGrUp = new QKeyEvent(QEvent::KeyRelease, Qt::Key_AltGr, Qt::NoModifier);
+  QApplication::postEvent(kate_view->focusProxy(), altGrUp);
+  QApplication::sendPostedEvents();
+  TestPressKey("\\ctrl-c");
+  FinishTest("{");
 }
 
 void ViModeTest::NormalModeMotionsTest() {
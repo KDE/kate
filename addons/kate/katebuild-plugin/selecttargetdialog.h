3d03a9813cdccbeea0bd9a14ecabd7b8663a93a2
Alex Neundorf <neundorf@kde.org>
2013-11-16 16:37:48 +0100

    build plugin: add LGPL2 license header to new files
    Alex

--- addons/kate/katebuild-plugin/selecttargetdialog.h
+++ addons/kate/katebuild-plugin/selecttargetdialog.h
@@ -1,6 +1,25 @@
 #ifndef SELECT_TARGET_DIALOG_H
 #define SELECT_TARGET_DIALOG_H
 
+// Description: dialog for selecting a target to build
+//
+// Copyright (c) 2013 Alexander Neundorf <neundorf@kde.org>
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Library General Public
+//  License version 2 as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Library General Public License for more details.
+//
+//  You should have received a copy of the GNU Library General Public License
+//  along with this library; see the file COPYING.LIB.  If not, write to
+//  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+//  Boston, MA 02110-1301, USA.
+
+
 #include <kdialog.h>
 
 #include "plugin_katebuild.h"

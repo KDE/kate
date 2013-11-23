3a3cde5d39bd386d8eeb633a8029ec52fd47bf82
Kevin Funk <kevin@kfunk.org>
2013-11-23 08:28:19 +0100

    Update copyright years
    It's 2013 after all

--- kate/app/katemain.cpp
+++ kate/app/katemain.cpp
@@ -80,7 +80,7 @@ extern "C" KDE_EXPORT int kdemain( int argc, char **argv )
 
   KAboutData aboutData ("kate", 0, ki18n("Kate"), kateVersion,
                         ki18n( "Kate - Advanced Text Editor" ), KAboutData::License_LGPL_V2,
-                        ki18n( "(c) 2000-2005 The Kate Authors" ), KLocalizedString(), "http://www.kate-editor.org");
+                        ki18n( "(c) 2000-2013 The Kate Authors" ), KLocalizedString(), "http://www.kate-editor.org");
   aboutData.setOrganizationDomain("kde.org");
   aboutData.addAuthor (ki18n("Christoph Cullmann"), ki18n("Maintainer"), "cullmann@kde.org", "http://www.cullmann.io");
   aboutData.addAuthor (ki18n("Anders Lund"), ki18n("Core Developer"), "anders@alweb.dk", "http://www.alweb.dk");
--- part/utils/kateglobal.cpp
+++ part/utils/kateglobal.cpp
@@ -65,7 +65,7 @@ KateGlobal::KateGlobal ()
  : KTextEditor::Editor (0)
  , m_aboutData ("katepart", 0, ki18n("Kate Part"), katePartVersion().toLatin1(),
              ki18n( "Embeddable editor component" ), KAboutData::License_LGPL_V2,
-             ki18n( "(c) 2000-2009 The Kate Authors" ), KLocalizedString(), "http://www.kate-editor.org")
+             ki18n( "(c) 2000-2013 The Kate Authors" ), KLocalizedString(), "http://www.kate-editor.org")
  , m_componentData (&m_aboutData)
  , m_snippetGlobal (0) // lazy constructed
  , m_sessionConfig (KGlobal::config())

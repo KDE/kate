eead2d6ae4f04894a38367bc378c7bb84add7133
Shaheed Haque <srhaque@theiet.org>
2013-12-07 18:07:56 +0000

    Fix licensing to be KDE compliant.

--- addons/kate/pate/src/plugins/gid/__init__.py
+++ addons/kate/pate/src/plugins/gid/__init__.py
@@ -5,22 +5,24 @@ for token completion, when looking up the usage of a token, or jumping to the
 definition. etags(1) is used to locate the definition.
 """
 #
-# Copyright (C) 2012 Shaheed Haque <srhaque@theiet.org>
+# Copyright 2012, 2013, Shaheed Haque <srhaque@theiet.org>.
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) version 3.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
 #
-# This library is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Library General Public License
-# along with this library; see the file COPYING.LIB.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import kate
 

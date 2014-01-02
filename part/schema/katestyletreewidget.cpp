1b4461662e9c61e311f550f76bfe8461f838556e
Christoph Cullmann <cullmann@kde.org>
2014-01-02 22:12:42 +0100

    relicense LGPLv2+
    cullmann@altair:/local/cullmann/kf5/src/kate> ../kde-dev-scripts/relicensecheck.pl part/schema/katestyletreewidget.cpp
    Need permission for licensing:
    
    - gplv23: jtamate michalhumpula vegardoye
      jtamate (   2 LOC): 2af427f
    michalhumpula (  72 LOC): 51256c9,8d92a2b,cf78324,b0e864a,1a2e27e,b67dd4e,816bcd0
    vegardoye (   2 LOC): bfe33fe
    
    - gplv2+: jtamate michalhumpula vegardoye
      jtamate (   2 LOC): 2af427f
    michalhumpula (  72 LOC): 51256c9,8d92a2b,cf78324,b0e864a,1a2e27e,b67dd4e,816bcd0
    vegardoye (   2 LOC): bfe33fe
    
    - +eV: vegardoye jtamate michalhumpula
    vegardoye (   2 LOC): bfe33fe
      jtamate (   2 LOC): 2af427f
    michalhumpula (  72 LOC): 51256c9,8d92a2b,cf78324,b0e864a,1a2e27e,b67dd4e,816bcd0
    
    - lgplv2+: michalhumpula jtamate vegardoye
    michalhumpula (  72 LOC): 51256c9,8d92a2b,cf78324,b0e864a,1a2e27e,b67dd4e,816bcd0
      jtamate (   2 LOC): 2af427f
    vegardoye (   2 LOC): bfe33fe
    
    => Michal OK, as in commits before, ok'd on kwrite-devel
    
    jtamate, only C++ cleanup:  2af427f
        more C++ casts vs C casts
    
        2/3 of C casts now are C++ casts. There was somewhere
        C and C++ casts mixed in one line.
    
    vegardoye, only typos: bfe33fe
    
       Comments cleanup
    
       Correct typos throughout the codebase.
    
    - lgplv23: michalhumpula jtamate vegardoye
    michalhumpula (  72 LOC): 51256c9,8d92a2b,cf78324,b0e864a,1a2e27e,b67dd4e,816bcd0
      jtamate (   2 LOC): 2af427f
    vegardoye (   2 LOC): bfe33fe
    
    Summary:
        9 commits possibly violating lgplv2+
        9 commits possibly violating +eV
        9 commits possibly violating gplv2+
        9 commits possibly violating gplv23
        9 commits possibly violating lgplv23
    
    Do not forget to check copyright headers and for patches committed in the name of others!

--- part/schema/katestyletreewidget.cpp
+++ part/schema/katestyletreewidget.cpp
@@ -3,21 +3,22 @@
    Copyright (C) 2002, 2003 Anders Lund <anders.lund@lund.tdcadsl.dk>
    Copyright (C) 2005-2006 Hamish Rodda <rodda@kde.org>
    Copyright (C) 2007 Mirko Stocker <me@misto.ch>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
 
 #include "katestyletreewidget.h"
 #include "kateconfig.h"

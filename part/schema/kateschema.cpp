18b1a233a90b3dd3262c5f398d608da2c9417dc8
Christoph Cullmann <cullmann@kde.org>
2014-01-02 22:52:16 +0100

    relicense LGPLv2+
    cullmann@altair:/local/cullmann/kf5/src/kate> ../kde-dev-scripts/relicensecheck.pl part/schema/kateschema.cpp
    Need permission for licensing:
    
    - lgplv2+: michalhumpula
    michalhumpula (   3 LOC): 51256c9,cf78324,816bcd0,5016261
    
    cullmann@altair:/local/cullmann/kf5/src/kate> ../kde-dev-scripts/relicensecheck.pl part/schema/kateschema.h
    Need permission for licensing:
    
    - lgplv2+: michalhumpula
    michalhumpula (   5 LOC): 51256c9,cf78324
    
    => Michal OKd

--- part/schema/kateschema.cpp
+++ part/schema/kateschema.cpp
@@ -2,21 +2,22 @@
    Copyright (C) 2007, 2008 Matthew Woehlke <mw_triad@users.sourceforge.net>
    Copyright (C) 2001-2003 Christoph Cullmann <cullmann@kde.org>
    Copyright (C) 2002, 2003 Anders Lund <anders.lund@lund.tdcadsl.dk>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
 
 //BEGIN Includes
 #include "kateschema.h"

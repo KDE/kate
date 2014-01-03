4cf8dad60e92c29aa2f2079cbb0bfc5fcc83bcb2
Christoph Cullmann <cullmann@kde.org>
2014-01-03 20:31:59 +0100

    relicense LGPLv2+
    OK, as only:
    
    katemdi.h
    
    - lgplv2+: michalhumpula jowenn@kde.org cfeck
    michalhumpula (  14 LOC): 51256c9,0a7f36a,4a5a610,a0e9c94
    jowenn@kde.org (  13 LOC): 118ba41
    
    katemwmodonhddialog.h
    
    - lgplv2+: michalhumpula
    michalhumpula (  13 LOC): 51256c9,0a7f36a,c9b468e,791e112
    
    katepluginmanager.h
    
    - lgplv2+: michalhumpula
    michalhumpula (   2 LOC): 51256c9,0a7f36a,01a42d7
    
    katequickopen.h
    
    - lgplv2+: michalhumpula
    michalhumpula (   2 LOC): af3ce2d,51256c9,cd0933e
    
    katerunninginstanceinfo.h
    
    - lgplv2+: michalhumpula
    michalhumpula (   2 LOC): ac24a48
    
    and they OKd (like before)

--- kate/app/katepluginmanager.h
+++ kate/app/katepluginmanager.h
@@ -2,10 +2,11 @@
    Copyright (C) 2001 Christoph Cullmann <cullmann@kde.org>
    Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
    Copyright (C) 2001 Anders Lund <anders.lund@lund.tdcadsl.dk>
-
+    
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of

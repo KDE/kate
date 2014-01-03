fe2e807bf910c5177a2ccc50e02b521caaa7c1ae
Christoph Cullmann <cullmann@kde.org>
2014-01-03 19:30:25 +0100

    relicense LGPLv2+
    cullmann@altair:~/local/kate/kate/tests> ../../kde-dev-scripts/relicensecheck.pl wordcompletiontest.h
    Need permission for licensing:
    
    - lgplv2+: brauch
       brauch (  47 LOC): af7860d
    
    got OK

--- tests/wordcompletiontest.h
+++ tests/wordcompletiontest.h
@@ -1,24 +1,22 @@
 /*
  * <one line to give the library's name and an idea of what it does.>
  * Copyright 2013  Sven Brauch <svenbrauch@gmail.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
 
 #ifndef KATE_WORDCOMPLETIONTEST_H
 #define KATE_WORDCOMPLETIONTEST_H

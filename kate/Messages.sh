7bbc7320d3f77ee301b244dec43d5182bcd2b56e
Christoph Cullmann <cullmann@kde.org>
2014-01-20 17:51:34 +0100

    fix i18n, no js, no tips

--- kate/Messages.sh
+++ kate/Messages.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
-(cd data && $PREPARETIPS > ../tips.cpp)
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o -name '*.js'` -o $podir/kate.pot
-rm -f tips.cpp
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o $podir/kate.pot

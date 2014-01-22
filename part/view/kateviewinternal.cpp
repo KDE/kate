586538228be61e96091d0cbd16ee314feb9b4688
Albert Astals Cid <aacid@kde.org>
2014-01-23 00:00:58 +0100

    we need a second fix for the scrollbar problem, else we crash, as we have no lines
    Backport from frameworks

--- part/view/kateviewinternal.cpp
+++ part/view/kateviewinternal.cpp
@@ -295,8 +295,7 @@ KTextEditor::Cursor KateViewInternal::endPos() const
     return KTextEditor::Cursor(thisLine.virtualLine(), thisLine.wrap() ? thisLine.endCol() - 1 : thisLine.endCol());
   }
 
-  kDebug(13030) << "WARNING: could not find a lineRange at all";
-  return KTextEditor::Cursor(-1, -1);
+  return KTextEditor::Cursor();
 }
 
 int KateViewInternal::endLine() const

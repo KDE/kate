875715704c2078bd0230d53d6b7cd3b8fe88c806
Sven Brauch <svenbrauch@googlemail.com>
2014-02-04 20:43:49 +0100

    Change license of keyword completion from LGPL2 to LGPL2+
    I'm the only copyright holder.

--- part/completion/katekeywordcompletion.h
+++ part/completion/katekeywordcompletion.h
@@ -3,7 +3,8 @@
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   License version 2, or any later version,
+   as published by the Free Software Foundation.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of

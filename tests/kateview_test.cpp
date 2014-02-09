050d17e0745a6274f08ac4a7a20fdb87034b2125
Sven Brauch <svenbrauch@googlemail.com>
2014-02-09 20:24:30 +0100

    use coordinatesToCursor instead of custom font metrics calculation
    I don't know why my font metrics stuff doesn't work on CI, here it works.

--- tests/kateview_test.cpp
+++ tests/kateview_test.cpp
@@ -58,15 +58,16 @@ void KateViewTest::testCoordinatesToCursor()
     KateDocument doc(false, false, false);
     doc.setText("Hi World!\nHi\n");
 
-    KateView* view1 = new KateView(&doc, 0);
-    int charWidth = view1->renderer()->currentFontMetrics().averageCharWidth();
-    int charHeight = view1->renderer()->lineHeight();
-    // keep a few pixels away from the borders between chars or lines (that's what the .2 and .7 is for)
-    // to avoid placing the pointer exactly on the border between two lines or characters
-    QVERIFY(view1->coordinatesToCursor(QPoint(2.7 * charWidth, 0.2 * charHeight)) == KTextEditor::Cursor(0, 2));
-    QVERIFY(view1->coordinatesToCursor(QPoint(1.7 * charWidth, 1.2 * charHeight)) == KTextEditor::Cursor(1, 1));
+    KateView* view1 = static_cast<KateView*>(doc.createView(0));
+    view1->show();
+
+    QCOMPARE(view1->coordinatesToCursor(view1->cursorToCoordinate(KTextEditor::Cursor(0, 2))),
+             KTextEditor::Cursor(0, 2));
+    QCOMPARE(view1->coordinatesToCursor(view1->cursorToCoordinate(KTextEditor::Cursor(1, 1))),
+             KTextEditor::Cursor(1, 1));
     // behind end of line should give an invalid cursor
-    QVERIFY(view1->coordinatesToCursor(QPoint(2.7 * charWidth, 1.2 * charHeight)) == KTextEditor::Cursor::invalid());
+    QCOMPARE(view1->coordinatesToCursor(view1->cursorToCoordinate(KTextEditor::Cursor(1, 5))),
+             KTextEditor::Cursor::invalid());
 }
 
 void KateViewTest::testReloadMultipleViews()

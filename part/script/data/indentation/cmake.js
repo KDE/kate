a052db14c07a01d88c2cc6004faa5f9fce882d78
Alex Turbov <i.zaufi@gmail.com>
2014-03-05 23:00:37 +0400

    set license to LGPL2+. PS: actually we need some (etalon) skeleton file to copy header from, cuz lot of files has different header text... and it is hard sometimes to quick find smth suitable to copy-n-paste

--- part/script/data/indentation/cmake.js
+++ part/script/data/indentation/cmake.js
@@ -7,9 +7,12 @@
  * required-syntax-style: CMake
  * indent-languages: CMake
  *
+ * This file is part of the Kate Project.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
- * License version 2 as published by the Free Software Foundation.
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of

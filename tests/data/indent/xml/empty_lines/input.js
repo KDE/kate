23d32b3ed77202a0067d587dd7a41618e226bc1d
Kåre Särs <kare.sars@iki.fi>
2014-03-10 00:05:36 +0200

    XML indenter test: Workaround (not) added spaces at end of line

--- tests/data/indent/xml/empty_lines/input.js
+++ tests/data/indent/xml/empty_lines/input.js
@@ -1,8 +1,20 @@
 v.setCursorPosition(0,0);
-v.selectAll();
+d.align(new Range(new Cursor(0, 0), new Cursor(0, 0)));
+v.setCursorPosition(1,0);
+d.align(new Range(new Cursor(1, 0), new Cursor(1, 0)));
+v.setCursorPosition(2,0);
+d.align(new Range(new Cursor(2, 0), new Cursor(2, 0)));
+v.setCursorPosition(3,0);
+d.align(new Range(new Cursor(3, 0), new Cursor(3, 0)));
+v.setCursorPosition(4,0);
+d.align(new Range(new Cursor(4, 0), new Cursor(4, 0)));
 
-var r = v.selection();
-if( r.isValid() ) {
-  d.align( r );
-}
+v.setCursorPosition(7,0);
+d.align(new Range(new Cursor(7, 0), new Cursor(7, 0)));
+v.setCursorPosition(8,0);
+d.align(new Range(new Cursor(8, 0), new Cursor(8, 0)));
+v.setCursorPosition(9,0);
+d.align(new Range(new Cursor(9, 0), new Cursor(9, 0)));
+v.setCursorPosition(12,0);
+d.align(new Range(new Cursor(12, 0), new Cursor(12, 0)));
 

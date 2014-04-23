3020b4b65053d5366f8b7408ef30be9a566f8aa5
Alex Turbov <i.zaufi@gmail.com>
2014-04-23 18:52:16 +0400

    Do not extent '..' --> '...' for other than "NormalText" code BUG: 333696

---
+++ tests/data/indent/cppstyle/dot3/input.js
@@ -0,0 +1,17 @@
+# BUG: #333696
+
+v.setCursorPosition(0,10);
+v.type(".");
+v.type(".");
+v.type("/");
+v.type("foo.hh");
+
+v.setCursorPosition(1,21);
+v.type(".");
+v.type(".");
+v.type(".");
+
+v.setCursorPosition(2,10);
+v.type(".");
+v.type(".");
+v.type(".");

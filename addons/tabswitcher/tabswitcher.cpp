76bffedb197618e8e8009d3c1d8a99a070fd9661
Dominik Haumann <dhaumann@kde.org>
2014-09-14 00:48:13 +0200

    fix position of document switcher

--- addons/tabswitcher/tabswitcher.cpp
+++ addons/tabswitcher/tabswitcher.cpp
@@ -236,7 +236,7 @@ void TabSwitcherPluginView::updateViewGeometry()
 
     // Position should be central over the editor area, so map to global from
     // parent of central widget since the view is positioned in global coords
-    QPoint centralWidgetPos = window->mapToGlobal(window->pos());
+    const QPoint centralWidgetPos = window->parentWidget() ? window->mapToGlobal(window->pos()) : window->pos();
     const int xPos = std::max(0, centralWidgetPos.x() + (centralSize.width()  - viewSize.width()  ) / 2);
     const int yPos = std::max(0, centralWidgetPos.y() + (centralSize.height() - viewSize.height() ) / 2);
 

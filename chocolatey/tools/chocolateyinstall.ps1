1a243ddbc791b7aab7e5c2ef30f6958262a87ea5
Hannah von Reth <vonreth@kde.org>
2016-06-16 14:14:40 +0200

    Updated nuspec for the latest release

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,3 +1,1 @@
-Install-ChocolateyPackage 'kate' 'exe' '/S' 'http://download.kde.org/unstable/kate/Kate-setup-16.04.1-32bit.exe' 'http://download.kde.org/unstable/kate/Kate-setup-16.04.1-64bit.exe'
-$uninstallInfo = Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Kate\'
-Generate-BinFile "kate" $uninstallInfo.DisplayIcon
+Install-ChocolateyPackage 'kate' 'exe' '/S' 'http://download.kde.org/unstable/kate/Kate-setup-16.04.2-KF5.23-32bit.exe' 'http://download.kde.org/unstable/kate/Kate-setup-16.04.2-KF5.23-64bit.exe'

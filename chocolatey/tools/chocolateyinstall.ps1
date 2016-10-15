bbd921df999af95be793d919764d09d3300cd0e3
Dominik Haumann <dhaumann@kde.org>
2016-10-15 15:50:29 +0200

    Update to Applications 16.08.2 release with KDE Frameworks 5.27

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'kate' 'exe' '/S' 'http://download.kde.org/unstable/kate/Kate-setup-16.04.2-KF5.23-32bit.exe' 'http://download.kde.org/unstable/kate/Kate-setup-16.04.2-KF5.23-64bit.exe'
+Install-ChocolateyPackage 'kate' 'exe' '/S' 'http://download.kde.org/stable/kate/Kate-setup-16.08.2-KF5.27-32bit.exe' 'http://download.kde.org/stable/kate/Kate-setup-16.08.2-KF5.27-64bit.exe'

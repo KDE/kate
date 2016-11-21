973b304e057a379f1fdccafc507a0a3ad850cce0
Hannah von Reth <vonreth@kde.org>
2016-11-21 11:39:44 +0100

    The chocolatey script was moved ...
    to http://commits.kde.org/chocolatey-packaging

--- chocolatey/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-[array]$key = Get-UninstallRegistryKey -SoftwareName "Kate*"
-if ($key -ne $null) {
-	Uninstall-ChocolateyPackage "kate" "EXE" "/S" -File $key.UninstallString
-	$instDir = Split-Path -Parent $key.UninstallString
-	Uninstall-BinFile -Name "kate" -Path "$instDir/bin/kate.exe"
-}

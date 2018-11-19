KDE/kate

commit a7ba6eb00095df86ae6bc4829327a41e521f1c1c

Quick Open: Remove "file:///" prefix from file paths
Summary:
In the second column, remove all "file:///" prefixes from the file paths.

Screenshot BEFORE:

{F6313000}

(Note, that the screenshot was taken at a time where the file paths in the upper section where not bold (before d6e38c0cbd3d6d7f7658862757b8c33db4322d7a). Now, the items in the first and second column are both bold)

AFTER:

{F6329707}

Reviewers: #kate, cullmann

Reviewed By: #kate, cullmann

Subscribers: cullmann, kfunk, gregormi, broulik, dhaumann, tcanabrava, kwrite-devel

Tags: #kate

Differential Revision: https://phabricator.kde.org/D16204

kate/katequickopenmodel.cpp
@@ -84,23 +84,24 @@ void KateQuickOpenModel::refresh()
 
     for (auto *view : qAsConst(sortedViews)) {
         auto doc = view->document();
-        allDocuments.push_back({ doc->documentName(), doc->url().toString(QUrl::NormalizePathSegments), false });
+        allDocuments.push_back({ doc->documentName(), doc->url().toDisplayString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile), false });
     }
 
     QStringList openedUrls;
     openedUrls.reserve(openDocs.size());
     for (auto *doc : qAsConst(openDocs)) {
-        const auto normalizedUrl = doc->url().toString(QUrl::NormalizePathSegments);
+        const auto normalizedUrl = doc->url().toString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile);
         allDocuments.push_back({ doc->documentName(), normalizedUrl, false });
         openedUrls.push_back(normalizedUrl);
     }
 
     for (const auto& file : qAsConst(projectDocs)) {
         QFileInfo fi(file);
-        allDocuments.push_back({ fi.fileName(), QUrl::fromLocalFile(file).toString(QUrl::NormalizePathSegments), false });
+        // example of file: "/home/user/projects/myfile.txt" which is consistent with QUrl::toDisplayString(QUrl::PreferLocalFile)
+        allDocuments.push_back({ fi.fileName(), file, false });
     }
 
-    /** Sort the arrays via Url. */
+    /** Sort the arrays by filePath. */
     std::sort(std::begin(allDocuments), std::end(allDocuments),
         [](const ModelEntry& a, const ModelEntry& b) {
         return a.filePath < b.filePath;
@@ -114,7 +115,7 @@ void KateQuickOpenModel::refresh()
         }),
         std::end(allDocuments));
 
-    for(auto& doc : allDocuments) {
+    for (auto& doc : allDocuments) {
         if (Q_UNLIKELY(openedUrls.indexOf(doc.filePath) != -1)) {
             doc.bold = true;
         }

commit 5850022cd0e2e1a56b376a62709a0be467fdb3e1

Fix crash introduced with a7ba6eb00095df86ae6bc482932
Summary:
Reason for the crash: with "Quick Open: Remove "file:///" prefix from file paths"
(a7ba6eb00095df86ae6bc4829327a41e521f1c1c) the Url prefix file:///
was removed and therefore we hit an assertion (missing protocol) when
trying to open a file from the Quick Open view.

Reviewers: #kate, cullmann

Reviewed By: #kate, cullmann

Subscribers: cullmann, kwrite-devel

Tags: #kate

Differential Revision: https://phabricator.kde.org/D16831

kate/katequickopen.cpp
@@ -159,7 +159,7 @@ void KateQuickOpen::update()
 void KateQuickOpen::slotReturnPressed()
 {
     const auto index = m_listView->model()->index(m_listView->currentIndex().row(), KateQuickOpenModel::Columns::FilePath);
-    auto url = QUrl(index.data(Qt::DisplayRole).toString());
+    auto url = index.data(Qt::UserRole).toUrl();
     m_mainWindow->wrapper()->openUrl(url);
     m_mainWindow->slotWindowActivated();
     m_inputLine->clear();

kate/katequickopenmodel.cpp
@@ -51,7 +51,7 @@ QVariant KateQuickOpenModel::data(const QModelIndex& idx, int role) const
         return {};
     }
 
-    if (role != Qt::DisplayRole && role != Qt::FontRole) {
+    if (role != Qt::DisplayRole && role != Qt::FontRole && role != Qt::UserRole) {
         return {};
     }
 
@@ -67,6 +67,8 @@ QVariant KateQuickOpenModel::data(const QModelIndex& idx, int role) const
             font.setBold(true);
             return font;
         }
+    } else if (role == Qt::UserRole) {
+        return entry.url;
     }
 
     return {};
@@ -84,21 +86,21 @@ void KateQuickOpenModel::refresh()
 
     for (auto *view : qAsConst(sortedViews)) {
         auto doc = view->document();
-        allDocuments.push_back({ doc->documentName(), doc->url().toDisplayString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile), false });
+        allDocuments.push_back({ doc->url(), doc->documentName(), doc->url().toDisplayString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile), false });
     }
 
     QStringList openedUrls;
     openedUrls.reserve(openDocs.size());
     for (auto *doc : qAsConst(openDocs)) {
         const auto normalizedUrl = doc->url().toString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile);
-        allDocuments.push_back({ doc->documentName(), normalizedUrl, false });
+        allDocuments.push_back({ doc->url(), doc->documentName(), normalizedUrl, false });
         openedUrls.push_back(normalizedUrl);
     }
 
     for (const auto& file : qAsConst(projectDocs)) {
         QFileInfo fi(file);
         // example of file: "/home/user/projects/myfile.txt" which is consistent with QUrl::toDisplayString(QUrl::PreferLocalFile)
-        allDocuments.push_back({ fi.fileName(), file, false });
+        allDocuments.push_back({ QUrl::fromLocalFile(fi.absoluteFilePath()), fi.fileName(), file, false });
     }
 
     /** Sort the arrays by filePath. */

kate/katequickopenmodel.h
@@ -28,9 +28,10 @@
 #include "katemainwindow.h"
 
 struct ModelEntry {
-    QString fileName;
-    QString filePath;
-    bool bold;
+    QUrl url; // used for actually opening a selected file (local or remote)
+    QString fileName; // display string for left column
+    QString filePath; // display string for right column
+    bool bold; // format line in bold text or not
 };
 
 class KateQuickOpenModel : public QAbstractTableModel {
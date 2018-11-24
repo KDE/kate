KDE/kate

commit a7ba6eb00095df86ae6bc4829327a41e521f1c1c

Quick Open: Remove "file:///" prefix from file paths
Summary:
In the second column, remove all "file:///" prefixes from the file paths.

Screenshot BEFORE:

{F6313000}

(Note, that the screenshot was taken at a time where the file paths in the upper section where not bold (before d6e38c0cbd3d6d7f7658862757b8c33db4322d7a). Now, the items in the first and second column are both bold)

AFTER:

{F6329707}

Reviewers: #kate, cullmann

Reviewed By: #kate, cullmann

Subscribers: cullmann, kfunk, gregormi, broulik, dhaumann, tcanabrava, kwrite-devel

Tags: #kate

Differential Revision: https://phabricator.kde.org/D16204

kate/katequickopenmodel.cpp
@@ -84,23 +84,24 @@ void KateQuickOpenModel::refresh()
 
     for (auto *view : qAsConst(sortedViews)) {
         auto doc = view->document();
-        allDocuments.push_back({ doc->documentName(), doc->url().toString(QUrl::NormalizePathSegments), false });
+        allDocuments.push_back({ doc->documentName(), doc->url().toDisplayString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile), false });
     }
 
     QStringList openedUrls;
     openedUrls.reserve(openDocs.size());
     for (auto *doc : qAsConst(openDocs)) {
-        const auto normalizedUrl = doc->url().toString(QUrl::NormalizePathSegments);
+        const auto normalizedUrl = doc->url().toString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile);
         allDocuments.push_back({ doc->documentName(), normalizedUrl, false });
         openedUrls.push_back(normalizedUrl);
     }
 
     for (const auto& file : qAsConst(projectDocs)) {
         QFileInfo fi(file);
-        allDocuments.push_back({ fi.fileName(), QUrl::fromLocalFile(file).toString(QUrl::NormalizePathSegments), false });
+        // example of file: "/home/user/projects/myfile.txt" which is consistent with QUrl::toDisplayString(QUrl::PreferLocalFile)
+        allDocuments.push_back({ fi.fileName(), file, false });
     }
 
-    /** Sort the arrays via Url. */
+    /** Sort the arrays by filePath. */
     std::sort(std::begin(allDocuments), std::end(allDocuments),
         [](const ModelEntry& a, const ModelEntry& b) {
         return a.filePath < b.filePath;
@@ -114,7 +115,7 @@ void KateQuickOpenModel::refresh()
         }),
         std::end(allDocuments));
 
-    for(auto& doc : allDocuments) {
+    for (auto& doc : allDocuments) {
         if (Q_UNLIKELY(openedUrls.indexOf(doc.filePath) != -1)) {
             doc.bold = true;
         }

commit 74eda4bc5874395d33c3a6242fcdcfb33f811e0b

Fix regression: duplicates were shown again
Summary:
commit a7ba6eb000 regressed duplicates showing in the
Quick Open view.

Reviewers: gregormi, #kate, cullmann

Reviewed By: #kate, cullmann

Subscribers: cullmann, kwrite-devel

Tags: #kate

Differential Revision: https://phabricator.kde.org/D17121

kate/katequickopenmodel.cpp
@@ -99,8 +99,11 @@ void KateQuickOpenModel::refresh()
 
     for (const auto& file : qAsConst(projectDocs)) {
         QFileInfo fi(file);
-        // example of file: "/home/user/projects/myfile.txt" which is consistent with QUrl::toDisplayString(QUrl::PreferLocalFile)
-        allDocuments.push_back({ QUrl::fromLocalFile(fi.absoluteFilePath()), fi.fileName(), file, false });
+        const auto localFile = QUrl::fromLocalFile(fi.absoluteFilePath());
+        allDocuments.push_back({
+            localFile,
+            fi.fileName(),
+            localFile.toString(QUrl::NormalizePathSegments | QUrl::PreferLocalFile), false });
     }
 
     /** Sort the arrays by filePath. */
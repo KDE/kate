a9b612688dc7bbd820bfef9fca14ff4ba6fbafb1
Dominik Haumann <dhaumann@kde.org>
2019-01-18 21:04:38 +0100

    Factor out KateToolRunner for better unit testing

---
+++ addons/externaltools/katetoolrunner.cpp
@@ -0,0 +1,36 @@
+/* This file is part of the KDE project
+ *
+ *  Copyright 2019 Dominik Haumann <dhaumann@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "katetoolrunner.h"
+
+KateToolRunner::KateToolRunner(KateExternalTool * tool)
+    : m_tool(tool)
+{
+}
+
+KateToolRunner::~KateToolRunner()
+{
+}
+
+void KateToolRunner::run()
+{
+}
+
+// kate: space-indent on; indent-width 4; replace-tabs on;

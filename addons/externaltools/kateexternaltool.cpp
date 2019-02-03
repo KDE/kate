2c970af9755416660ed9990f0633e15725ece6da
Dominik Haumann <dhaumann@kde.org>
2019-02-03 15:18:54 +0100

    Save & load enums manually instead of casting to int. This is more flexible once we change them

--- addons/externaltools/kateexternaltool.cpp
+++ addons/externaltools/kateexternaltool.cpp
@@ -22,6 +22,54 @@
 #include <KConfigGroup>
 #include <QStandardPaths>
 
+namespace {
+    QString toString(KateExternalTool::SaveMode saveMode)
+    {
+        switch (saveMode) {
+            case KateExternalTool::SaveMode::None: return QStringLiteral("None");
+            case KateExternalTool::SaveMode::CurrentDocument: return QStringLiteral("CurrentDocument");
+            case KateExternalTool::SaveMode::AllDocuments: return QStringLiteral("AllDocuments");
+        }
+        Q_ASSERT(false); // yout forgot a case above
+        return QStringLiteral("None");
+    }
+
+    KateExternalTool::SaveMode toSaveMode(const QString & mode)
+    {
+        if (mode == QStringLiteral("None")) return KateExternalTool::SaveMode::None;
+        if (mode == QStringLiteral("CurrentDocument")) return KateExternalTool::SaveMode::CurrentDocument;
+        if (mode == QStringLiteral("AllDocuments")) return KateExternalTool::SaveMode::AllDocuments;
+        return KateExternalTool::SaveMode::None;
+    }
+
+    QString toString(KateExternalTool::OutputMode outputMode)
+    {
+        switch (outputMode) {
+            case KateExternalTool::OutputMode::Ignore: return QStringLiteral("Ignore");
+            case KateExternalTool::OutputMode::InsertAtCursor: return QStringLiteral("InsertAtCursor");
+            case KateExternalTool::OutputMode::ReplaceSelectedText: return QStringLiteral("ReplaceSelectedText");
+            case KateExternalTool::OutputMode::ReplaceCurrentDocument: return QStringLiteral("ReplaceCurrentDocument");
+            case KateExternalTool::OutputMode::AppendToCurrentDocument: return QStringLiteral("AppendToCurrentDocument");
+            case KateExternalTool::OutputMode::InsertInNewDocument: return QStringLiteral("InsertInNewDocument");
+            case KateExternalTool::OutputMode::DisplayInPane: return QStringLiteral("DisplayInPane");
+        }
+        Q_ASSERT(false); // yout forgot a case above
+        return QStringLiteral("Ignore");
+    }
+
+    KateExternalTool::OutputMode toOutputMode(const QString & mode)
+    {
+        if (mode == QStringLiteral("Ignore")) return KateExternalTool::OutputMode::Ignore;
+        if (mode == QStringLiteral("InsertAtCursor")) return KateExternalTool::OutputMode::InsertAtCursor;
+        if (mode == QStringLiteral("ReplaceSelectedText")) return KateExternalTool::OutputMode::ReplaceSelectedText;
+        if (mode == QStringLiteral("ReplaceCurrentDocument")) return KateExternalTool::OutputMode::ReplaceCurrentDocument;
+        if (mode == QStringLiteral("AppendToCurrentDocument")) return KateExternalTool::OutputMode::AppendToCurrentDocument;
+        if (mode == QStringLiteral("InsertInNewDocument")) return KateExternalTool::OutputMode::InsertInNewDocument;
+        if (mode == QStringLiteral("DisplayInPane")) return KateExternalTool::OutputMode::DisplayInPane;
+        return KateExternalTool::OutputMode::Ignore;
+    }
+}
+
 bool KateExternalTool::checkExec() const
 {
     return !QStandardPaths::findExecutable(executable).isEmpty();
@@ -44,9 +92,9 @@ void KateExternalTool::load(const KConfigGroup& cg)
     mimetypes = cg.readEntry("mimetypes", QStringList());
     actionName = cg.readEntry("actionName");
     cmdname = cg.readEntry("cmdname");
-    saveMode = static_cast<KateExternalTool::SaveMode>(cg.readEntry("save", 0));
+    saveMode = toSaveMode(cg.readEntry("save", "None"));
     reload = cg.readEntry("reload", false);
-    outputMode = static_cast<KateExternalTool::OutputMode>(cg.readEntry("output", 0));
+    outputMode = toOutputMode(cg.readEntry("output", "Ignore"));
     includeStderr = cg.readEntry("includeStderr", false);
 
     hasexec = checkExec();
@@ -64,9 +112,9 @@ void KateExternalTool::save(KConfigGroup& cg) const
     cg.writeEntry("mimetypes", mimetypes);
     cg.writeEntry("actionName", actionName);
     cg.writeEntry("cmdname", cmdname);
-    cg.writeEntry("save", static_cast<int>(saveMode));
+    cg.writeEntry("save", toString(saveMode));
     cg.writeEntry("reload", reload);
-    cg.writeEntry("output", static_cast<int>(outputMode));
+    cg.writeEntry("output", toString(outputMode));
     cg.writeEntry("includeStderr", includeStderr);
 }
 

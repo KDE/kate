158ef31ed916f24dd1e8df93d3597ef9d1fd313e
Christoph Cullmann <cullmann@kde.org>
2019-09-10 15:12:29 +0200

    auto commit

--- reformat.sh
+++ reformat.sh
@@ -1,1 +1,5 @@
-exec find . -name "*.cpp" -or -name "*.h" -exec clang-format -i {} \;
+# reformat the code
+find . -name "*.cpp" -or -name "*.h" -exec clang-format -i {} \;
+
+# check in the result
+git commit -a -m "GIT_SILENT: application of coding style"

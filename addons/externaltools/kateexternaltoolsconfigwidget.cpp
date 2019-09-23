bd8d3e61173a73141ef74508db6ff55f52fabcb7
Dominik Haumann <dhaumann@kde.org>
2019-09-23 12:02:41 +0200

    External tools: Fix uniqueness of action names
    Required for persistent shortcuts.

--- addons/externaltools/kateexternaltoolsconfigwidget.cpp
+++ addons/externaltools/kateexternaltoolsconfigwidget.cpp
@@ -81,7 +81,9 @@ static void makeActionNameUnique(KateExternalTool *tool, const std::vector<KateE
     QString name = tool->actionName;
     int i = 1;
     while (true) {
-        auto it = std::find_if(tools.cbegin(), tools.cend(), [&name](const KateExternalTool *tool) { return tool->actionName == name; });
+        auto it = std::find_if(tools.cbegin(), tools.cend(), [tool](const KateExternalTool *t) {
+            return (t != tool) && (t->actionName == tool->actionName);
+        });
         if (it == tools.cend()) {
             break;
         }
@@ -320,7 +322,10 @@ bool KateExternalToolsConfigWidget::editTool(KateExternalTool *tool)
         tool->cmdname = editor.ui->edtCommand->text();
 
         // sticky action collection name, never changes again, so that shortcuts stay
-        tool->actionName = QStringLiteral("externaltool_") + QString(tool->name).remove(QRegularExpression(QStringLiteral("\\W+")));
+        if (tool->actionName.isEmpty()) {
+            tool->actionName = QStringLiteral("externaltool_") + QString(tool->name).remove(QRegularExpression(QStringLiteral("\\W+")));
+        }
+        makeActionNameUnique(tool, collectTools(m_toolsModel));
 
         changed = true;
     }

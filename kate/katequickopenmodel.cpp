1dec9c372201b2c34310b81d2cf8bf82e37eaa83
Waqar Ahmed <waqar.17a@gmail.com>
2021-01-07 03:15:25 +0500

    Also remove the leading slash

--- kate/katequickopenmodel.cpp
+++ kate/katequickopenmodel.cpp
@@ -73,7 +73,7 @@ void KateQuickOpenModel::refresh()
     const QList<KTextEditor::View *> sortedViews = m_mainWindow->viewManager()->sortedViews();
     const QList<KTextEditor::Document *> openDocs = KateApp::self()->documentManager()->documentList();
     const QStringList projectDocs = projectView ? (m_listMode == CurrentProject ? projectView->property("projectFiles") : projectView->property("allProjectsFiles")).toStringList() : QStringList();
-    const QString projectBase = projectView ? (m_listMode == CurrentProject ? projectView->property("projectBaseDir") : projectView->property("allProjectsCommonBaseDir")).toString() : QString();
+    const QString projectBase = projectView ? (m_listMode == CurrentProject ? projectView->property("projectBaseDir") : projectView->property("allProjectsCommonBaseDir")).toString() + QStringLiteral("/") : QString();
 
     QVector<ModelEntry> allDocuments;
     allDocuments.reserve(sortedViews.size() + openDocs.size() + projectDocs.size());

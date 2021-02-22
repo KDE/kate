718d59f1426a6da9870bc593546b05778e0f924f
Waqar Ahmed <waqar.17a@gmail.com>
2021-02-23 03:57:53 +0500

    Make sure we can highlight more than 2 parameters

--- addons/lspclient/lspclientserver.cpp
+++ addons/lspclient/lspclientserver.cpp
@@ -612,7 +612,10 @@ static LSPSignatureHelp parseSignatureHelp(const QJsonValue &result)
     ret.activeSignature = sig.value(QStringLiteral("activeSignature")).toInt(0);
     ret.activeParameter = sig.value(QStringLiteral("activeParameter")).toInt(0);
     ret.activeSignature = qMin(qMax(ret.activeSignature, 0), ret.signatures.size());
-    ret.activeParameter = qMin(qMax(ret.activeParameter, 0), ret.signatures.size());
+    ret.activeParameter = qMax(ret.activeParameter, 0);
+    if (!ret.signatures.isEmpty()) {
+        ret.activeParameter = qMin(ret.activeParameter, ret.signatures.at(ret.activeSignature).parameters.size());
+    }
     return ret;
 }
 

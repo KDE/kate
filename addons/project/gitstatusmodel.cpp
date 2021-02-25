f608f65b6cf4863763a5fc87bc6bc02a7ed04fc5
Waqar Ahmed <waqar.17a@gmail.com>
2021-02-25 13:07:56 +0500

    Use Breeze fg-negative/positive in git-status-view
    Signed-off-by: Waqar Ahmed <waqar.17a@gmail.com>

--- addons/project/gitstatusmodel.cpp
+++ addons/project/gitstatusmodel.cpp
@@ -139,9 +139,9 @@ QVariant GitStatusModel::data(const QModelIndex &index, int role) const
             }
         } else if (role == Qt::ForegroundRole) {
             if (index.column() == 1 && rootIndex > 0) {
-                return QColor(Qt::red);
+                return QColor(218, 68, 83); // Breeze Fg Negative
             } else if (index.column() == 1 && rootIndex == 0) {
-                return QColor(Qt::green);
+                return QColor(39, 174, 96); // Breeze Fg Positive
             }
         }
     }

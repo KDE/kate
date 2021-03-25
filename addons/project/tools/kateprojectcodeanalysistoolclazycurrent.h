7a4d6952290e921d352595068fe05c165f0db49d
Waqar Ahmed <waqar.17a@gmail.com>
2021-03-25 18:45:09 +0500

    Add SPDX-License headers
    Signed-off-by: Waqar Ahmed <waqar.17a@gmail.com>

--- addons/project/tools/kateprojectcodeanalysistoolclazycurrent.h
+++ addons/project/tools/kateprojectcodeanalysistoolclazycurrent.h
@@ -1,3 +1,9 @@
+/*  This file is part of the Kate project.
+ *
+ *  SPDX-FileCopyrightText: 2021 Waqar Ahmed <waqar.17a@gmail.com>
+ *
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
+ */
 #ifndef KATEPROJECTCODEANALYSISTOOLCLANGTIDY_H
 #define KATEPROJECTCODEANALYSISTOOLCLANGTIDY_H
 

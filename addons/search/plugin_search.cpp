fce870476ef824d1ddaa021bc9e057dd8b8a5aa4
Kåre Särs <kare.sars@iki.fi>
2021-04-14 12:42:40 +0300

    Fix S&R not recovering from canceled folder search
    BUG: 435689

--- addons/search/plugin_search.cpp
+++ addons/search/plugin_search.cpp
@@ -878,7 +878,7 @@ void KatePluginSearchView::matchesFound(const QUrl &url, const QVector<KateSearc
 
 void KatePluginSearchView::stopClicked()
 {
-    m_folderFilesList.cancelSearch();
+    m_folderFilesList.terminateSearch();
     m_searchOpenFiles.cancelSearch();
     cancelDiskFileSearch();
     Results *res = qobject_cast<Results *>(m_ui.resultTabWidget->currentWidget());
@@ -1935,7 +1935,7 @@ void KatePluginSearchView::tabCloseRequested(int index)
     if (m_curResults == tmp) {
         m_searchOpenFiles.cancelSearch();
         cancelDiskFileSearch();
-        m_folderFilesList.cancelSearch();
+        m_folderFilesList.terminateSearch();
     }
     if (m_ui.resultTabWidget->count() > 1) {
         delete tmp; // remove the tab

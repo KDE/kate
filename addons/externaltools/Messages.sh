9dcc6910681a3fe0757999aa8ab4043c64efc60a
Ahmad Samir <a.samirh78@gmail.com>
2021-09-29 00:27:04 +0200

    ExternalToolsPlugin: split defaultexternaltoolsrc into one file per tool

--- addons/externaltools/Messages.sh
+++ addons/externaltools/Messages.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 $EXTRACTRC *.rc *.ui >> rc.cpp
-grep -n -e '^ *name=' defaultexternaltoolsrc | sed 's!^\(.*\):.*name= *\(.*\) *$!// i18n: file: \1\ni18nc("External tool name", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
-grep -n -e '^ *category=' defaultexternaltoolsrc | sed 's!^\(.*\):.*category= *\(.*\) *$!// i18n: file: \1\ni18nc("External tool category", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
+for i in externaltools-config/*; do
+    grep -n -e '^ *name=' $i | sed 's!^\(.*\):.*name= *\(.*\) *$!// i18n: file: \1\ni18nc("External tool name", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
+    grep -n -e '^ *category=' $i | sed 's!^\(.*\):.*category= *\(.*\) *$!// i18n: file: \1\ni18nc("External tool category", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
+;done
 $XGETTEXT *.cpp  -o $podir/kateexternaltoolsplugin.pot

9f185afc2d7a4f2047f04f9cbfc46648c3436907
Yuri Chornoivan <yurchor@ukr.net>
2021-10-10 08:52:17 +0300

    Remove extra semicolon

--- addons/externaltools/Messages.sh
+++ addons/externaltools/Messages.sh
@@ -3,5 +3,5 @@ $EXTRACTRC *.rc *.ui >> rc.cpp
 for i in externaltools-config/*; do
     grep -n -e '^ *name=' $i | sed 's!^\(.*\):.*name= *\(.*\) *$!// i18n: file: \1\ni18nc("External tool name", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
     grep -n -e '^ *category=' $i | sed 's!^\(.*\):.*category= *\(.*\) *$!// i18n: file: \1\ni18nc("External tool category", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
-;done
+done
 $XGETTEXT *.cpp  -o $podir/kateexternaltoolsplugin.pot

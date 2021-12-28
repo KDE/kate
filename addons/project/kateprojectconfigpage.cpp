78acc8ffa726a748dfdeea2624c15fcbff2a7ea7
Christoph Cullmann <cullmann@kde.org>
2021-12-28 19:57:35 +0100

    update project preference icon

--- addons/project/kateprojectconfigpage.cpp
+++ addons/project/kateprojectconfigpage.cpp
@@ -132,7 +132,7 @@ QString KateProjectConfigPage::fullName() const
 
 QIcon KateProjectConfigPage::icon() const
 {
-    return QIcon::fromTheme(QLatin1String("view-list-tree"));
+    return QIcon::fromTheme(QLatin1String("preferences-git"), QIcon::fromTheme(QLatin1String("view-list-tree")));
 }
 
 void KateProjectConfigPage::apply()

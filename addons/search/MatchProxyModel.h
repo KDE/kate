6b4a72932da3ef55f0912f101a9650576247149c
Waqar Ahmed <waqar.17a@gmail.com>
2022-02-04 21:14:17 +0500

    Missing license headers

--- addons/search/MatchProxyModel.h
+++ addons/search/MatchProxyModel.h
@@ -1,3 +1,8 @@
+/*
+    SPDX-FileCopyrightText: 2022 Waqar Ahmed <waqar.17a@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 #ifndef KATE_SEARCH_MATCH_PROXY_MODEL_H
 #define KATE_SEARCH_MATCH_PROXY_MODEL_H
 
--- addons/search/plugin_search.cpp
+++ addons/search/plugin_search.cpp
@@ -1,5 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2011-21 Kåre Särs <kare.sars@iki.fi>
+    SPDX-FileCopyrightText: 2022 Waqar Ahmed <waqar.17a@gmail.com>
 
     SPDX-License-Identifier: LGPL-2.0-or-later
 */

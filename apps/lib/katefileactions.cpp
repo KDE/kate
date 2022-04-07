fe5271e62a0ca9112b5a0c33f52db1425e8979c3
Christoph Cullmann <cullmann@kde.org>
2022-04-07 23:13:22 +0200

    asked author Gregor Mi <codestruct@posteo.org> for re-licensing, is ok

--- apps/lib/katefileactions.cpp
+++ apps/lib/katefileactions.cpp
@@ -2,7 +2,7 @@
  *
  * SPDX-FileCopyrightText: 2018 Gregor Mi <codestruct@posteo.org>
  *
- * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  **/
 
 #include "katefileactions.h"

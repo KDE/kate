5ea5c2dee505e0d3fb83a63bac8b67ef7e816474
Milian Wolff <mail@milianw.de>
2022-04-09 15:14:11 +0200

    Relicense urlinfo.h to LGPL v2+
    This unifies the license with other files in this repo. Previously,
    only 2.1 and 3.0 were accepted, but I don't remember that being a
    deliberate choice of mine all these years ago.

--- apps/lib/urlinfo.h
+++ apps/lib/urlinfo.h
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
    SPDX-FileCopyrightText: 2015 Milian Wolff <mail@milianw.de>
 
-   SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+   SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #ifndef URLINFO_H

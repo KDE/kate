8bb619005a41823aa2feecb8bd01178cdef1d12b
Alain Laporte <alaporte@anevia.com>
2022-06-02 23:28:17 +0200

    Add sample for Python
    Currently, some cases are not well handled

---
+++ addons/symbolviewer/samples/testfile.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+
+def foo1():
+    pass
+
+if toto == True:
+    def foo2():
+        pass
+else:
+    def foo2():
+        pass
+
+class class1:
+    pass
+
+def foo3(param):
+    pass
+
+def foo4(param1, param2="default"):
+    pass
+
+class class2(class1):
+    pass
+
+class class3(class1, toto=1):
+    pass
+
+class class4():
+    def foo1():
+        pass

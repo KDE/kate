69b5ca3a9495ee0b05425fb1d992fe676ab6b45c
Eric Armbruster <eric1@armbruster-online.de>
2022-07-04 00:47:50 +0200

    Add icon to configure button

--- apps/lib/katemdi.cpp
+++ apps/lib/katemdi.cpp
@@ -757,7 +757,7 @@ bool Sidebar::eventFilter(QObject *obj, QEvent *ev)
 
                 if (!w->plugin.isNull()) {
                     if (w->plugin.data()->configPages() > 0) {
-                        menu.addAction(i18n("Configure ..."))->setData(20);
+                        menu.addAction(QIcon::fromTheme(QStringLiteral("configure")), i18n("Configure..."))->setData(20);
                     }
                 }
 

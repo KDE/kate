76b948ad695077b4a688c8ffeb2bc230199b8a04
Pablo Rauzy <r@uzy.me>
2022-08-03 14:19:49 +0200

    fix licensing to allow potential future merge in KTextEditor

--- addons/keyboardmacros/macro.h
+++ addons/keyboardmacros/macro.h
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2022 Pablo Rauzy <r .at. uzy .dot. me>
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
 #ifndef KEYBOARDMACROSPLUGIN_MACRO_H

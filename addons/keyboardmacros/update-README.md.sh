f470de4cfb4bad966b211d4583490b50c160a947
Pablo Rauzy <r@uzy.me>
2022-08-07 15:06:24 +0200

    adding example of pre-commit hook that updates the README automatically

--- addons/keyboardmacros/update-README.md.sh
+++ addons/keyboardmacros/update-README.md.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script uses pandoc to update the local README.md file with the content from Kate's docbook documentation.
 
 cd $(dirname "$(readlink -f "$BASH_SOURCE")")
@@ -10,3 +10,26 @@ sed -n '/^<sect1 id="kate-application-plugin-keyboardmacros">$/,/^<\/sect1>$/p' 
     cat > README.md
 
 cd - >/dev/null
+
+##### BEGIN git pre-commit hook script
+###
+### #!/usr/bin/env bash
+###
+### UPDATE_SCRIPT="$(dirname $(git rev-parse --git-common-dir))"/addons/keyboardmacros/update-README.md.sh
+###
+### test -f "$UPDATE_SCRIPT" || exit 0
+###
+### TMP=$(mktemp)
+### README="$(dirname $(git rev-parse --git-common-dir))"/addons/keyboardmacros/README.md
+### cp "$README" "$TMP"
+### source "$UPDATE_SCRIPT"
+###
+### if ! diff -q "$README" "$TMP" &>/dev/null; then
+###     echo "Warning: README.md has been updated!"
+###     rm "$TMP"
+###     exit 1
+### fi
+### rm "$TMP"
+### exit 0
+###
+##### END

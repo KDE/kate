214702dd6958a887706108439ae988060a951a0e
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-10-15 14:23:04 +0200

    Remove Kate Session Applet
    With Plasma 5.26, this is shipped applet is shipped as part of kdeplasma-addons under a different id.
    Otherwise, Plasma 5.26 and Kate < 22.12 would not have been co-installable.
    Consequently we have to adjust the plugin id in the plasma config file that keeps track of the applets.
    
    BUG: 460275

---
+++ addons/sessionapplet/migrate_kate_sessions_applet_to_kdeplasma-addons.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2022 Alexander Lohnau <alexander.lohnau@gmx.de>
+# SPDX-License-Identifier: MIT
+
+sed -i 's/plugin=org\.kde\.plasma\.katesessions/plugin=org.kde.plasma.addons.katesessions/g' ~/.config/plasma-org.kde.plasma.desktop-appletsrc

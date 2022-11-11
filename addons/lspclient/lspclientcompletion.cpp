e2d1f1b7cc076f1342f9f0eb743b4ccecca20e59
Waqar Ahmed <waqar.17a@gmail.com>
2022-11-11 22:58:32 +0500

    Fix lsp completion not triggered on user invocation
    For user invocation it should always be triggered.
    
    (cherry picked from commit a96d0337c370dd8234e9f1f706007c7f1c43adae)

--- addons/lspclient/lspclientcompletion.cpp
+++ addons/lspclient/lspclientcompletion.cpp
@@ -325,7 +325,8 @@ public:
 
         qCInfo(LSPCLIENT) << "completion invoked" << m_server;
 
-        if (it == UserInvocation && range.isEmpty() && m_signatureHelp) {
+        const bool userInocation = it == UserInvocation;
+        if (userInocation && range.isEmpty() && m_signatureHelp) {
             // If this is a user invocation (ctrl-space), check the last non-space char for sig help trigger
             QChar c;
             int i = range.start().column() - 1;
@@ -396,7 +397,7 @@ public:
             auto cursor = qMax(range.start(), qMin(range.end(), position));
             m_manager->update(document, false);
 
-            if (m_triggerCompletion) {
+            if (m_triggerCompletion || userInocation) {
                 m_handle = m_server->documentCompletion(document->url(), {cursor.line(), cursor.column()}, this, handler);
             }
 

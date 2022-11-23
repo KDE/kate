cdbb75729d1473ee676bd63e3e89605c18198d9b
Waqar Ahmed <waqar.17a@gmail.com>
2022-11-23 17:02:07 +0500

    lsp: specify codeActionKind valueSet
    Otherwise some servers refuse to provide any actions
    
    (cherry picked from commit 1f5bd3eae89ac869a4c82002651729164a61f722)

--- addons/lspclient/lspclientserver.cpp
+++ addons/lspclient/lspclientserver.cpp
@@ -1330,7 +1330,16 @@ private:
     {
         // clang-format off
         QJsonObject codeAction{{QStringLiteral("codeActionLiteralSupport"),
-                                QJsonObject{{QStringLiteral("codeActionKind"), QJsonObject{{QStringLiteral("valueSet"), QJsonArray()}}}}}};
+                                    QJsonObject{{
+                                        QStringLiteral("codeActionKind"), QJsonObject{{
+                                            QStringLiteral("valueSet"), QJsonArray({
+                                                QStringLiteral("quickfix"),
+                                                QStringLiteral("refactor"),
+                                                QStringLiteral("source")
+                                            })
+                                        }}
+                                    }}
+                              }};
 
         QJsonObject semanticTokens{{QStringLiteral("requests"),
                                         QJsonObject{

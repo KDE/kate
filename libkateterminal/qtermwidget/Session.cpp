94a4f4261609ef3c0b7b343976d4903f43ecc10d
Waqar Ahmed <waqar.17a@gmail.com>
2023-02-13 15:25:31 +0500

    Close session on destruction

--- libkateterminal/qtermwidget/Session.cpp
+++ libkateterminal/qtermwidget/Session.cpp
@@ -592,6 +592,10 @@ void Session::sendKeyEvent(QKeyEvent *e) const
 
 Session::~Session()
 {
+    if (_shellProcess && _shellProcess->isRunning()) {
+        close();
+    }
+
     delete _emulation;
     delete _shellProcess;
     //  delete _zmodemProc;

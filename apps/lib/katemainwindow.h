aec3a2225fe12483ab76d88f978322e24e4a73fc
Waqar Ahmed <waqar.17a@gmail.com>
2023-03-24 17:26:29 +0500

    Fix ModOnHDDialog not shown after doc externally modified
    - Use the right api i.e., applicationState provides the info on
    whether we are active or not.
    - Connect to the applicationStateChanged signal to show the dialog
    
    (cherry picked from commit 8952759457dbef8fdfb99e41a0e363b4dc0b244d)

--- apps/lib/katemainwindow.h
+++ apps/lib/katemainwindow.h
@@ -223,6 +223,8 @@ private Q_SLOTS:
 
     void slotListRecursiveEntries(KIO::Job *job, const KIO::UDSEntryList &list);
 
+    void onApplicationStateChanged(Qt::ApplicationState);
+
 private Q_SLOTS:
     void toggleShowMenuBar(bool showMessage = true);
     void toggleShowStatusBar();

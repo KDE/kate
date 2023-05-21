0b9996dfe02f5d79a777e08645555bcfe374e3ab
Cezar Tigaret <cezar.tigaret@gmail.com>
2023-05-21 19:03:58 +0100

    Signed-off-by: Cezar Tigaret <cezar.tigaret@gmail.com>

--- addons/symbolviewer/julia_parser.cpp
+++ addons/symbolviewer/julia_parser.cpp
@@ -8,7 +8,8 @@
  ***************************************************************************/
 /***************************************************************************
  *                                                                         *
- *   SPDX-License-Identifier: GPL-2.0-or-later
+ *   SPDX-FileCopyrightText: 2023 Cezar M. Tigaret <cezar.tigaret@gmail.com>
+ *   SPDX-License-Identifier: LGPL-2.0-or-later
  *                                                                         *
  ***************************************************************************/
 #include "plugin_katesymbolviewer.h"

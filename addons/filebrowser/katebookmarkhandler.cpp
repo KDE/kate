8cadc671ba81348d6a32abad24b3e7ee4706342c
Nicolas Fella <nicolas.fella@gmx.de>
2023-08-20 11:35:46 +0200

    [addons/filebrowser] Fix KF6 build

--- addons/filebrowser/katebookmarkhandler.cpp
+++ addons/filebrowser/katebookmarkhandler.cpp
@@ -32,7 +32,11 @@ KateBookmarkHandler::KateBookmarkHandler(KateFileBrowser *parent, QMenu *kpopupm
         file = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QStringLiteral("/kate/fsbookmarks.xml");
     }
 
+#if QT_VERSION_MAJOR == 6
+    KBookmarkManager *manager = KBookmarkManager::managerForFile(file);
+#else
     KBookmarkManager *manager = KBookmarkManager::managerForFile(file, QStringLiteral("kate"));
+#endif
     manager->setUpdate(true);
 
     m_bookmarkMenu = new KBookmarkMenu(manager, this, m_menu);

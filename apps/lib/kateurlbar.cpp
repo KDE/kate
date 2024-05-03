96b061d5c9587e3400a8859686aa5c9a65d18b54
Waqar Ahmed <waqar.17a@gmail.com>
2024-05-03 23:24:59 +0500

    UrlBar: Fix filtering in treeview
    Filter recursive + expand afterwards
    
    (cherry picked from commit 4a4dc5ede212bc0bd0de6413dd34ff153209fa61)

--- apps/lib/kateurlbar.cpp
+++ apps/lib/kateurlbar.cpp
@@ -113,12 +113,16 @@ public:
 
         m_proxyModel.setFilterKeyColumn(0);
         m_proxyModel.setFilterRole(Qt::DisplayRole);
+        m_proxyModel.setRecursiveFilteringEnabled(true);
         m_itemView.setModel(&m_proxyModel);
         connect(&m_filterText, &QLineEdit::textChanged, &m_proxyModel, &FuzzyFilterModel::setFilterString);
         connect(&m_filterText, &QLineEdit::textChanged, this, [this] {
             auto index = m_proxyModel.index(0, 0);
             if (index.isValid()) {
                 m_itemView.setCurrentIndex(index);
+                if constexpr (std::is_same_v<ItemView, QTreeView>) {
+                    m_itemView.expandAll();
+                }
             }
         });
     }

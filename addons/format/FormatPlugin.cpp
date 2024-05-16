dc0a6474830b4ca6c46f763a266adfa645ab2f30
Waqar Ahmed <waqar.17a@gmail.com>
2024-05-16 22:47:13 +0500

    Move formatting plugin actions to "Edit" menu

--- addons/format/FormatPlugin.cpp
+++ addons/format/FormatPlugin.cpp
@@ -118,15 +118,16 @@ FormatPluginView::FormatPluginView(FormatPlugin *plugin, KTextEditor::MainWindow
     a->setText(i18n("Format Document"));
     connect(mainWin, &KTextEditor::MainWindow::viewChanged, this, &FormatPluginView::onActiveViewChanged);
 
-    const QString guiDescription = QStringLiteral(
-        ""
-        "<!DOCTYPE gui><gui name=\"formatplugin\">"
-        "<MenuBar>"
-        "    <Menu name=\"tools\">"
-        "        <Action name=\"format_on_save\"/>"
-        "    </Menu>"
-        "</MenuBar>"
-        "</gui>");
+    const QString guiDescription = QStringLiteral(R"xml(
+<!DOCTYPE gui><gui name="formatplugin">
+<MenuBar>
+    <Menu name="edit">
+        <Action name="format_on_save"/>
+        <Action name="format_document"/>
+    </Menu>
+</MenuBar></gui>
+)xml");
+
     setXML(guiDescription);
     a = actionCollection()->addAction(QStringLiteral("format_on_save"), this, [this](bool b) {
         m_plugin->formatOnSave = b;

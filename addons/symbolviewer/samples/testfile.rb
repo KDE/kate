8008e395c314f2bdf6b07b26f060a1d81920c5dc
Waqar Ahmed <waqar.17a@gmail.com>
2024-05-16 23:07:44 +0500

    Fix ruby method parsing and nested classes parsing
    support
     - self.function
     - nested classes
     - functions with parens
    
    BUG: 480756

--- addons/symbolviewer/samples/testfile.rb
+++ addons/symbolviewer/samples/testfile.rb
@@ -2,8 +2,17 @@ class Class1
     def foo1_in_Class1()
         print "foo1_in_Class1"
     end
+
+    class Class2
+        def self.name
+        end
+    end
 end
 
 def foo1(param1)
     print "foo1"
+end
+
+def no_paren_function
+    puts 'aaaaa'
 end

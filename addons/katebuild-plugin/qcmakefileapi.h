4e1a824a81c318663b0524e3a0bf1d9b2dc85a0f
Alex Neundorf <neundorf@kde.org>
2024-06-04 23:00:00 +0200

    build plugin: add a bit of docs for the cmake file API class

--- addons/katebuild-plugin/qcmakefileapi.h
+++ addons/katebuild-plugin/qcmakefileapi.h
@@ -14,32 +14,42 @@
 #include <set>
 #include <vector>
 
+/** QCMakeFileApi can be used to query a cmake build tree via the CMake file API
+ * to extract information like build targets, their sources etc. from it.
+ * It runs exactly the CMake which was used to create the build tree. */
 class QCMakeFileApi : public QObject
 {
     Q_OBJECT
 public:
-    //* \a buildDir can be either the cmake build dir or the full path of a CMakeCache.txt
+    /// \a buildDir can be either the cmake build dir or the full path of a CMakeCache.txt
     QCMakeFileApi(const QString &buildDir, bool withSourceFiles = true);
-
-    bool runCMake();
 
     const QString &getCMakeExecutable() const;
     QString getCMakeGuiExecutable() const;
 
+    /// Write the CMake file API query files. Once they have been written
+    /// CMake will generate reply files when it runs the next time.
     bool writeQueryFiles();
 
-    bool readReplyFiles();
+    /// Run CMake. You probably want to writeQueryFiles() before.
+    bool runCMake();
 
+    /// Check whether the reply files have been written.
     bool haveKateReplyFiles() const;
+
+    /// Read the reply files (hopefully) written by CMake.
+    bool readReplyFiles();
 
     //! Returns the command line which will be run. Needed only for asking the user for permission.
     QStringList getCMakeRequestCommandLine() const;
 
+    /// Get the list of build configurations for this build tree. Contains "NONE" if there is none.
     const std::vector<QString> &getConfigurations() const;
     const QString &getProjectName() const;
     const QString &getBuildDir() const;
     const QString &getSourceDir() const;
 
+    /// Return a list of all source files of all targets in the build tree.
     const std::set<QString> &getSourceFiles() const;
 
     enum class TargetType { Executable = 0, Library = 1, Utility = 2, Unknown = 3 };
@@ -49,6 +59,7 @@ public:
         TargetType type = TargetType::Unknown;
     };
 
+    /// Return the vector of build Targets in this build tree.
     const std::vector<Target> &getTargets(const QString &config) const;
 
 private Q_SLOTS:

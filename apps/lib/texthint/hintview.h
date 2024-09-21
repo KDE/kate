c461ef83c278c0b82f62a9187c333d3a3e5af0e8
Karthik Nishanth <nishanthkarthik@live.com>
2024-09-21 15:40:11 -0700

    Add copyright headers to new files from #1557

--- apps/lib/texthint/hintview.h
+++ apps/lib/texthint/hintview.h
@@ -1,3 +1,10 @@
+/*
+    SPDX-FileCopyrightText: 2024 Karthik Nishanth <kndevl@outlook.com>
+    SPDX-FileCopyrightText: 2023 Waqar Ahmed <waqar.17a@gmail.com
+    SPDX-FileCopyrightText: 2019 Mark Nauwelaerts <mark.nauwelaerts@gmail.com>
+    SPDX-FileCopyrightText: 2019 Christoph Cullmann <cullmann@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 #pragma once
 
 #include "KateTextHintManager.h"

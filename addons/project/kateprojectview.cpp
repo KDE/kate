d46855e14bd4e2d7abcd358be1b0e48671e4e5f6
Waqar Ahmed <waqar.17a@gmail.com>
2024-12-16 12:26:09 +0500

    Fix clang-tidy misc-redundant-expression

--- addons/project/kateprojectview.cpp
+++ addons/project/kateprojectview.cpp
@@ -58,7 +58,7 @@ KateProjectView::KateProjectView(KateProjectPluginView *pluginView, KateProject 
      */
     m_filter->setPlaceholderText(i18n("Filter…"));
     m_filter->setClearButtonEnabled(true);
-    m_filter->setProperty("_breeze_borders_sides", QVariant::fromValue(Qt::TopEdge | Qt::TopEdge));
+    m_filter->setProperty("_breeze_borders_sides", QVariant::fromValue(Qt::TopEdge));
     connect(m_filter, &KLineEdit::textChanged, this, [this] {
         m_filterStartTimer.start();
     });

139e4410d2e6e8da15339bbdcf4799e3f9641c19
Kåre Särs <kare.sars@iki.fi>
2025-06-09 14:53:53 +0300

    Build: Fix creating a new set of targets
    BUG: 505346

--- addons/katebuild-plugin/targets.cpp
+++ addons/katebuild-plugin/targets.cpp
@@ -246,8 +246,13 @@ void TargetsUi::targetSetNew()
     targetFilterEdit->setText(QString());
     QModelIndex currentIndex = proxyModel.mapToSource(targetsView->currentIndex());
     QString workingDir = QDir::homePath();
+    QString projDir;
+    if (currentIndex.data(TargetModel::IsProjectTargetRole).toBool()) {
+        workingDir = currentProjectBaseDir + u"/build"_s;
+        projDir = currentProjectBaseDir;
+    }
 
-    QModelIndex index = targetsModel.insertTargetSetAfter(currentIndex, i18n("Target Set"), workingDir);
+    QModelIndex index = targetsModel.insertTargetSetAfter(currentIndex, i18n("Target Set"), workingDir, false, QString(), projDir);
     index = targetsModel.addCommandAfter(index, i18nc("Name/Label for a command to configure a build", "Configure"), configCmd, QString());
     index = targetsModel.addCommandAfter(index, i18nc("Name/Label for a compilation or build command", "Build Command"), buildCmd, runCmd);
     index = proxyModel.mapFromSource(index);

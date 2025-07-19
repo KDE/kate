c2aa4f788764d6755076d6ba950a014bc5c8b3cd
Mark Nauwelaerts <mark.nauwelaerts@gmail.com>
2025-07-19 12:29:59 +0200

    lib: add kio kateexec scheme for internal use

---
+++ apps/lib/autotests/io_tests.cpp
@@ -0,0 +1,159 @@
+/*
+ * This file is part of the Kate project.
+ *
+ *  SPDX-FileCopyrightText: 2025 Mark Nauwelaerts <mark.nauwelaerts@gmail.com>
+ *
+ *  SPDX-License-Identifier: MIT
+ */
+
+#include <QCoreApplication>
+#include <QString>
+#include <QTemporaryFile>
+#include <QTest>
+
+#include <KIO/FileCopyJob>
+#include <KIO/ListJob>
+#include <KIO/StatJob>
+
+#include "../exec_io_utils.h"
+
+class ExecIOTest : public QObject
+{
+    Q_OBJECT
+
+    QTemporaryFile m_tempFile;
+
+public:
+    ExecIOTest()
+    {
+        setup();
+    }
+
+    void setup()
+    {
+        Utils::ExecPrefixManager::instance().update(QLatin1String("identity"), {QLatin1String("env")});
+    }
+
+    void makeRemote(QUrl &url)
+    {
+        url.setScheme(Utils::ExecPrefixManager::scheme());
+        url.setHost(QLatin1String("identity"));
+    }
+
+    void doCopy(bool toFile)
+    {
+        QByteArray data{"foobar"};
+
+        // remote
+        QTemporaryFile remote;
+        remote.open();
+        auto remoteUrl = QUrl::fromLocalFile(remote.fileName());
+        makeRemote(remoteUrl);
+
+        // local
+        QTemporaryFile local;
+        local.open();
+        auto localUrl = QUrl::fromLocalFile(local.fileName());
+
+        auto &srcUrl = toFile ? remoteUrl : localUrl;
+        auto &destUrl = toFile ? localUrl : remoteUrl;
+        auto &dest = toFile ? local : remote;
+        auto &src = toFile ? remote : local;
+
+        src.write(data);
+        src.flush();
+        src.close();
+
+        QEventLoop loop;
+
+        // copy
+        auto job = QScopedPointer(KIO::file_copy(srcUrl, destUrl, 0600, KIO::Overwrite));
+
+        QObject::connect(job.get(), &KJob::result, this, [&](KJob *job) {
+            loop.quit();
+            QVERIFY(!job->error());
+            dest.seek(0);
+            QCOMPARE(dest.readAll(), data);
+        });
+
+        loop.exec();
+
+        // should reject overwrite
+        job.reset(KIO::file_copy(srcUrl, destUrl, 0600, KIO::DefaultFlags));
+
+        QObject::connect(job.get(), &KJob::result, this, [&](KJob *job) {
+            loop.quit();
+            QVERIFY(job->error());
+            dest.seek(0);
+            QCOMPARE(dest.readAll(), data);
+        });
+
+        loop.exec();
+    }
+
+private Q_SLOTS:
+
+    void testCopyToFile()
+    {
+        doCopy(true);
+    }
+
+    void testCopyFromFile()
+    {
+        doCopy(false);
+    }
+
+    void testListDir()
+    {
+        QTemporaryDir tempdir;
+
+        QVERIFY(tempdir.isValid());
+
+        auto url = QUrl::fromLocalFile(tempdir.path());
+        makeRemote(url);
+
+        QDir topdir(tempdir.path());
+        // add some content
+        topdir.mkdir(topdir.path() + QLatin1String("/subdir"));
+        QFile file(topdir.path() + QLatin1String("/subfile"));
+        file.open(QFile::WriteOnly);
+        file.close();
+
+        auto job = QScopedPointer(KIO::listDir(url));
+
+        QEventLoop loop;
+
+        KIO::UDSEntryList list;
+        QObject::connect(job.get(), &KIO::ListJob::entries, this, [&](KIO::Job *job, const KIO::UDSEntryList &entries) {
+            (void)job;
+            list.append(entries);
+        });
+
+        QObject::connect(job.get(), &KJob::result, this, [&](KJob *job) {
+            loop.quit();
+            QVERIFY(!job->error());
+        });
+
+        loop.exec();
+
+        // check contents
+        QCOMPARE(list.size(), 4);
+
+        for (const auto &e : list) {
+            auto name = e.stringValue(KIO::UDSEntry::UDS_NAME);
+            auto isDir = e.isDir();
+            if (name == QLatin1String(".") || name == QLatin1String("..") || name == QLatin1String("subdir")) {
+                QVERIFY(isDir);
+            } else {
+                QVERIFY(!isDir);
+                QCOMPARE(name, QFileInfo(file.fileName()).baseName());
+            }
+        }
+    }
+};
+
+QTEST_MAIN(ExecIOTest)
+
+#include "io_tests.moc"
+
+// kate: space-indent on; indent-width 4; replace-tabs on;
---
+++ apps/lib/exec_io_utils.cpp
@@ -0,0 +1,103 @@
+#include "exec_io_utils.h"
+
+#include <QMap>
+#include <QSaveFile>
+#include <QTemporaryFile>
+
+#include "kate_exec_debug.h"
+
+namespace Utils
+{
+static constexpr char ENV_KATE_EXEC_DATA[] = "KATE_EXEC_DATA";
+
+class ExecPrefixManager::ExecPrefixManagerPrivate
+{
+    QMap<QString, QStringList> m_args;
+    QTemporaryFile m_workerConfig;
+    QString m_workerConfigPath;
+
+public:
+    ExecPrefixManagerPrivate()
+    {
+        // prime temp file
+        m_workerConfig.open();
+        m_workerConfigPath = m_workerConfig.fileName();
+        m_workerConfig.close();
+
+        // provide forked workers with info where to find config
+        qputenv(ENV_KATE_EXEC_DATA, m_workerConfigPath.toLocal8Bit());
+
+        // NOTE alternatively, WorkerConfig might be an option
+        // but looks like that is not exposed by KIO
+        // so we have to come up with our own
+        // another option might be to update the KConfig rc file
+        // but not yet for now
+    }
+
+    void update_config()
+    {
+        QSaveFile save(m_workerConfigPath);
+        // QTemporaryFile tmp;
+        // tmp.open();
+        save.open(QFile::WriteOnly);
+        QDataStream stream(&save);
+        QMap<QString, QStringList> data;
+        stream << m_args;
+        auto ret = save.commit();
+        qCDebug(LibKateExec) << "save exec config" << data << ret;
+    }
+
+    void update(QString name, QStringList args)
+    {
+        // FIXME check for different args for same name ??
+        // it depends on whether from same or different origin
+        // -> undefined behaviour, no diagnostic required
+        m_args[name] = args;
+        update_config();
+    }
+};
+
+ExecPrefixManager &ExecPrefixManager::instance()
+{
+    static auto instance = new ExecPrefixManager();
+    return *instance;
+}
+
+ExecPrefixManager::ExecPrefixManager()
+    : d(new ExecPrefixManagerPrivate())
+{
+}
+
+ExecPrefixManager::~ExecPrefixManager()
+{
+    delete d;
+}
+
+void ExecPrefixManager::update(QString name, QStringList args)
+{
+    return d->update(name, args);
+}
+
+QStringList ExecPrefixManager::load(QString name)
+{
+    auto fname = qEnvironmentVariable(ENV_KATE_EXEC_DATA);
+    qCDebug(LibKateExec) << "in worker" << fname;
+    QFile fdata(fname);
+    fdata.open(QFile::ReadOnly);
+    QDataStream stream(&fdata);
+    QMap<QString, QStringList> data;
+    stream >> data;
+
+    auto it = data.find(name);
+    if (it != data.end()) {
+        auto args = *it;
+        if (args.size()) {
+            qCDebug(LibKateExec) << "found" << args;
+            return args;
+        }
+    }
+
+    return {};
+}
+
+}

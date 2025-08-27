9063464544348ac8e41167e9134dc5438a88eea6
Waqar Ahmed <waqar.17a@gmail.com>
2025-08-27 23:34:21 +0500

    Fix incorrect error handling
    we can't read readAllStandardError() twice

--- apps/lib/diff/diffwidget.cpp
+++ apps/lib/diff/diffwidget.cpp
@@ -547,7 +547,7 @@ void DiffWidget::runGitDiff()
         const QByteArray out = git->readAllStandardOutput();
         const QByteArray err = git->readAllStandardError();
         if (!err.isEmpty()) {
-            onError(git->readAllStandardError(), git->exitCode());
+            onError(err, git->exitCode());
         }
 
         if (!out.isEmpty()) {

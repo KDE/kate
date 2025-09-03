2e89b5ac65debb22653dd9924cdb09f922dbbd6b
Aleix Pol <aleixpol@kde.org>
2025-09-04 01:02:06 +0200

    Support running our processes from within a container
    Be it flatpak, docker or ssh, at times we'll want to run processes that
    need specific tooling. This could be something including
    `flatpak-builder --run`, `docker run` or any similar command.
    
    The intent here is to make kate more flexible and able to run such
    processes.

--- apps/lib/hostprocess.cpp
+++ apps/lib/hostprocess.cpp
@@ -6,7 +6,10 @@
 #include <QProcess>
 #include <QStandardPaths>
 
+#include "katedebug.h"
+#include <KConfigGroup>
 #include <KSandbox>
+#include <KSharedConfig>
 
 QString safeExecutableName(const QString &executableName, const QStringList &paths)
 {
@@ -21,6 +24,30 @@ QString safeExecutableName(const QString &executableName, const QStringList &pat
 
 void startHostProcess(QProcess &proc, QProcess::OpenMode mode)
 {
+    KConfigGroup cg(KSharedConfig::openConfig(), QStringLiteral("kate_execution"));
+    QStringList command = cg.readEntry("BaseCommand", QStringList());
+
+    // This allows us to run the command under certain tooling like within a docker or flatpak container
+    if (!command.isEmpty()) {
+        auto args = proc.arguments();
+        args.prepend(proc.program());
+        proc.setProgram(command.takeFirst());
+        proc.setArguments(command + args);
+    }
+
+    const QStringList envs = cg.readEntry("Environment", QStringList());
+    if (!envs.isEmpty()) {
+        auto environment = proc.processEnvironment();
+        for (const QString &env : envs) {
+            auto splitEnv = env.indexOf(QLatin1Char('='));
+            if (splitEnv <= 0) {
+                qCWarning(LOG_KATE) << "entry wrongly formatted" << env;
+                continue;
+            }
+            environment.insert(env.left(splitEnv), env.mid(splitEnv + 1));
+        }
+        proc.setProcessEnvironment(environment);
+    }
     KSandbox::startHostProcess(proc, mode);
 }
 

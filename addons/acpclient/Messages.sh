ab6d5229e5e0c026c3878f396105c5ab284d65b2
Christoph Cullmann <christoph@cullmann.dev>
2026-08-08 20:36:16 +0200

    simplify message extraction
    the model gave there all it had

--- addons/acpclient/Messages.sh
+++ addons/acpclient/Messages.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: 2026 Christoph Cullmann <cullmann@kde.org>
-#
-# SPDX-License-Identifier: MIT
-# Extract messages for ACP Client plugin
-
-exrc="acpclientplugin.cpp acpclientpluginview.cpp acpclientconfigpage.cpp acpserverdialog.cpp acpclientserver.cpp acpclientservermanager.cpp acpclientprotocol.cpp"
-
-extractrc "ui.rc" >> rc.cpp
-extractrc "acpconfigwidget.ui" >> rc.cpp
-extractrc "acpserverdialog.ui" >> rc.cpp
-
-xgettext --from-code=UTF-8 -kde -ki18n -ktr2i18n -kI18N_NOOP -kI18N_NOOP2 -kalreadyTranslatedText \
-    -kalreadyTranslatedTextPlural -kki18n -kki18n: -kki18nc -kki18nc: \
-    -kki18np -kki18np: -kki18nq -kki18nq: -ktranslate -o $podir/acpclient.pot \
-    rc.cpp $exrc
+#! /bin/sh
+$EXTRACTRC *.ui *.rc >> rc.cpp
+$XGETTEXT *.cpp -o $podir/acpclient.pot
